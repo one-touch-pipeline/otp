6cfa0800cf60a67814cc36f64ed7b8870fa4f13a
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2011-10-06 11:20:55 +0200

    Include Spring Beans with @Component annotation
    Allows our Jobs and Scheduler to be configured as Spring Bean
    without the need for custom code in resources.groovy.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,6 @@
 // Place your Spring DSL code here
 beans = {
+    // include Spring Beans with @Component annotation
+    xmlns context:"http://www.springframework.org/schema/context"
+    context.'component-scan'('base-package' :"de.dkfz.tbi.otp" )
 }
