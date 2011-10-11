8df544c17c83bef3db2f17247499375fab07f8a2
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2011-10-11 11:41:34 +0200

    Event script to compile AST jar first
    Hooks into the compile phase and ensures that the ast jar is
    compiled first and added to the compile dependency.
    
    By that the AST transformation is available at compile time
    without the need to use an ant script first.

---
+++ scripts/_Events.groovy
@@ -0,0 +1,18 @@
+includeTargets << grailsScript("_GrailsCompile")
+
+eventSetClasspath = { rootLoader ->
+    classpathSet = false
+    println "Set Classpath handler"
+    String buildDir = "./ast-build"
+    String astLib = "./ast-build/ast.jar"
+    ant.delete(dir: buildDir)
+    ant.mkdir(dir: buildDir)
+    ant.groovyc(srcdir: "./ast", destdir: buildDir)
+    ant.jar(destfile: astLib, basedir: buildDir) {
+        ant.service(type: "org.codehaus.groovy.transform.ASTTransformation", provider: "de.dkfz.tbi.otp.job.ast.JobTransformation")
+    }
+    grailsSettings.compileDependencies << new File(astLib)
+    rootLoader.addURL(new File(astLib).toURI().toURL())
+
+    classpath()
+}
