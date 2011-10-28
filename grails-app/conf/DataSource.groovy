2d7c6021bb1bef7be583898a3e7987df58813afa
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2011-10-28 16:52:19 +0200

    updates

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,9 +1,12 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
+    driverClassName = "org.h2.Driver" 
+	//driverClassName = "com.mysql.jdbc.Driver"
+    //dialect = org.hibernate.dialect.MySQL5InnoDBDialect
     username = "sa"
     password = ""
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
@@ -14,8 +17,10 @@ environments {
     development {
         dataSource {
 			//loggingSql = true
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:h2:mem:devDb" 
+			url = "jdbc:h2:~/h2/ngsdata"
+			//url = "jdbc:mysql://localhost/test"
         }
     }
     test {
