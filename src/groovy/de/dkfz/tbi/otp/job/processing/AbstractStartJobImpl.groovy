9808f6611e3d3021669c998dcd3c29bf519e2fcf
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2011-11-14 12:36:24 +0100

    Extend the AbstractStartJobImpl class
    Add useful methods for StartJobs.

--- src/groovy/de/dkfz/tbi/otp/job/processing/AbstractStartJobImpl.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/processing/AbstractStartJobImpl.groovy
@@ -1,6 +1,9 @@
 package de.dkfz.tbi.otp.job.processing
 
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
+import de.dkfz.tbi.otp.job.plan.StartJobDefinition
+import de.dkfz.tbi.otp.job.scheduler.SchedulerService
+import org.springframework.beans.factory.annotation.Autowired
 
 /**
  * Abstract base class for {@link StartJob}s.
@@ -10,10 +13,24 @@ import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
  * @see StartJob
  */
 abstract class AbstractStartJobImpl implements StartJob {
+    @Autowired
+    SchedulerService schedulerService
     private JobExecutionPlan plan
 
     protected AbstractStartJobImpl() {}
     protected AbstractStartJobImpl(JobExecutionPlan plan) {
+        this.plan = plan
+    }
+
+    /**
+     * Sets the JobExecutionPlan of this StartJob if not already set.
+     * @param plan The JobExecutionPlan this Job belongs to
+     * @throws InvalidStateException If the plan has already been set
+     */
+    public final void setJobExecutionPlan(JobExecutionPlan plan) throws InvalidStateException {
+        if (this.plan) {
+            throw new InvalidStateException("Execution plan has already been set")
+        }
         this.plan = plan
     }
 
@@ -25,5 +42,17 @@ abstract class AbstractStartJobImpl implements StartJob {
     @Override
     public String getVersion() {
         return ""
+    }
+
+    /**
+     * Convenient method to access the JobExecutionPlan's StartJobDefinition.
+     * @return The StartJobDefiniton
+     * @throws InvalidStateException in case the JobExecutionPlan is not yet set.
+     */
+    protected final StartJobDefinition getStartJobDefinition() throws InvalidStateException {
+        if (!plan) {
+            throw new InvalidStateException("Execution plan has not been set")
+        }
+        return plan.startJob
     }
 }
