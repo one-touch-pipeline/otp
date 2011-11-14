3d081906b83ebc6613ddf4365c7bba89a318536b
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2011-11-14 13:10:20 +0100

    Start the StartJobs on Bootstrap
    All active JobExecutionPlans are found and their StartJob is
    started and configured with the plan.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import grails.util.Environment
+import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
+import de.dkfz.tbi.otp.job.plan.StartJobDefinition
+import de.dkfz.tbi.otp.job.processing.AbstractStartJobImpl
+
 class BootStrap {
+    def grailsApplication
 
     def init = { servletContext ->
+        if (Environment.current != Environment.TEST) {
+            List<JobExecutionPlan> plans = JobExecutionPlan.findAllByEnabledAndObsoleted(true, false)
+            plans.each { JobExecutionPlan plan ->
+                AbstractStartJobImpl startJob = grailsApplication.mainContext.getBean(plan.startJob.bean) as AbstractStartJobImpl
+                startJob.setJobExecutionPlan(plan)
+            }
+        }
     }
     def destroy = {
     }
