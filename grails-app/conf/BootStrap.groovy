127ae658f2a9cf435c0377b8e5c369450ed44287
Jochen Schramm <j.schramm@dkfz-heidelberg.de>
2011-11-15 15:49:16 +0100

    Remove private method safeSave()
    A private method in the Bootstrap is not senseful.
    As the method is located in the MetaDataService class
    it can safely be removed at this point.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -18,25 +18,4 @@ class BootStrap {
     }
     def destroy = {
     }
-
-	///////////////////////////////////////////////////////////////////////////
-	
-	private void safeSave(def obj) {
-		//
-		// probably will go to separate static class
-		// no formal exception, information only
-		//
-		
-		obj.validate()
-		if (obj.hasErrors()) {
-			println obj.errors
-			return
-		}
-
-		if (!obj.save(flush: true))
-			println "can not save ${obj}"
-	}
-		
-	///////////////////////////////////////////////////////////////////////////
-	
 }
