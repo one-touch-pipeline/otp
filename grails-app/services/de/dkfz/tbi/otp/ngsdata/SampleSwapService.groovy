568cace7b0a9496b0331e739ce864757c6c3cbf7
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2011-11-18 10:30:26 +0100

    introducing a service to manage sample swaps

---
+++ grails-app/services/de/dkfz/tbi/otp/ngsdata/SampleSwapService.groovy
@@ -0,0 +1,59 @@
+package de.dkfz.tbi.otp.ngsdata
+
+class SampleSwapService {
+
+    /**
+     * 
+     * This function disconnect data from a given sample
+     * from tha sample. This functions shall be used when 
+     * physical sample was discovered to be swapped.
+     * 
+     * New new individual is created "PooledErrors"
+     * 
+     * @param sample
+     */
+    void wrongSample(Sample sample) {
+
+        // take real individual
+        Individual oldIndividual = sample.individual
+
+        // take or create and PooledError Individual
+
+        Individual ind = Individual.findByPid("errors")
+        if (!ind) {
+            ind = new Individual (
+                    pid: "errors",
+                    mockPid: "errors",
+                    mockFullName: "PooledErrors",
+                    type = Individual.Type.POOL
+                )
+            ind.save(flush: true)
+        }
+
+        // move Sample to different this individual
+        sample.individual = ind;
+        ind.save()
+        sample.save();
+
+        // create new Sample 
+        Sample newSample = new Sample(
+            type = sample.type,
+            subType = sample.subType
+        )
+
+        oldIndividual.addToSamples(newSample);
+        oldIndividual.save()
+        newSample.save()
+
+        // move Sample identifiers
+        sample.sampleIdentifiers.each {SampleIdentifier identifier ->
+            newSample.addToSampleIdentifiers(identifier)
+        }
+        newSample.save()
+
+        // invalidate meta-data entries
+        
+
+        // deal with the files 
+    }
+}
