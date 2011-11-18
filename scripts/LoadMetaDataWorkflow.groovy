60623c4af8ecc62d82109dbb8275ef945c7f5abd
Jochen Schramm <j.schramm@dkfz-heidelberg.de>
2011-11-18 17:22:50 +0100

    Initial commit

---
+++ scripts/LoadMetaDataWorkflow.groovy
@@ -0,0 +1,46 @@
+import de.dkfz.tbi.otp.job.plan.JobDefinition
+import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
+import de.dkfz.tbi.otp.job.plan.StartJobDefinition
+import de.dkfz.tbi.otp.job.processing.Parameter
+import de.dkfz.tbi.otp.job.processing.ParameterMapping
+import de.dkfz.tbi.otp.job.processing.ParameterType
+import de.dkfz.tbi.otp.job.processing.ParameterUsage
+
+JobExecutionPlan jep = new JobExecutionPlan(name: "loadMetaData", planVersion: 0, enabled: true)
+assert(jep.save())
+StartJobDefinition definition = new StartJobDefinition(name: "start", bean: "loadMetaDataStartJob", plan: jep)
+assert(definition.save())
+jep.startJob = definition
+
+JobDefinition registerInputFilesJob = new JobDefinition(name: "registerInputFiles", bean: "registerInputFilesJob", plan: jep)
+assert(registerInputFilesJob.save())
+JobDefinition loadMetaDataJob = new JobDefinition(name: "loadMetaData", bean: "loadMetaDataJob", plan: jep, previous: registerInputFilesJob)
+assert(loadMetaDataJob.save())
+registerInputFilesJob.next = loadMetaDataJob
+assert(registerInputFilesJob.save())
+JobDefinition validateMetadataJob = new JobDefinition(name: "validateMetadata", bean: "validateMetadataJob", plan: jep, previous: loadMetaDataJob)
+assert(validateMetadataJob.save())
+loadMetaDataJob.next = validateMetadataJob
+assert(loadMetaDataJob.save())
+JobDefinition buildExecutionDateJob = new JobDefinition(name: "buildExecutionDate", bean: "buildExecutionDateJob", plan: jep, previous: validateMetadataJob)
+assert(buildExecutionDateJob.save())
+validateMetadataJob.next = buildExecutionDateJob
+assert(validateMetadataJob.save())
+JobDefinition buildSequenceTracksJob = new JobDefinition(name: "buildSequenceTracks", bean: "buildSequenceTracksJob", plan: jep, previous: buildExecutionDateJob)
+assert(buildSequenceTracksJob.save())
+buildExecutionDateJob.next = buildSequenceTracksJob
+assert(buildExecutionDateJob.save())
+JobDefinition checkSequenceTracksJob = new JobDefinition(name: "checkSequenceTracks", bean: "checkSequenceTracksJob", plan: jep, previous: buildSequenceTracksJob)
+assert(checkSequenceTracksJob.save())
+buildSequenceTracksJob.next = checkSequenceTracksJob
+assert(buildSequenceTracksJob.save())
+// set the first job
+jep.firstJob = registerInputFilesJob
+assert(jep.save(flush: true))
+// Parameters for Start Job
+ParameterType type = new ParameterType(name: "run", jobDefinition: definition, usage: ParameterUsage.OUTPUT)
+assert(type.save())
+Parameter parameter = new Parameter(type: type)
+assert(parameter.save())
+definition.addToConstantParameters(parameter)
+assert(definition.save(flush: true))
