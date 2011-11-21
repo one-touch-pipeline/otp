aa27b049fd0725199f58380f26b1b1421dc2bd43
Jochen Schramm <j.schramm@dkfz-heidelberg.de>
2011-11-21 15:26:55 +0100

    Add new classes for having process parameters
    The ProcessParameter is a new parameter functionality
    enabling to have parameters unchanged valid for a
    complete process. These new parameters are this way
    some kind of static for a particular process
    simplifying parameters to be set by the start job,
    used by several jobs, but not changed at all.
    The to the ProcessParameter associated
    ProcessParameterType is needed as it maps to the
    JobExecutionPlan instead of a JobDefinition
    (as ParameterType does) and as it is simpler.

---
+++ grails-app/domain/de/dkfz/tbi/otp/job/processing/ProcessParameter.groovy
@@ -0,0 +1,9 @@
+package de.dkfz.tbi.otp.job.processing
+
+class ProcessParameter {
+    ProcessParameterType type
+    String value
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/de/dkfz/tbi/otp/job/processing/ProcessParameterType.groovy
@@ -0,0 +1,34 @@
+package de.dkfz.tbi.otp.job.processing
+
+import de.dkfz.tbi.otp.job.plan.JobDefinition
+import de.dkfz.tbi.otp.job.plan.JobExecutionPlan;
+
+/**
+ * The ProcessParameterType is the generic description for {@link ProcessParameter}s of a {@link Process}.
+ *
+ * It contains a name which is unique for a Process, a human readable description
+ * and whether it references a domain class or not. The actual values are ProcessParameter
+ * which combines a ProcessParameterType with a value.
+ *
+ * @see ProcessParameter
+ * @see Process
+ */
+class ProcessParameterType {
+    /**
+     * The name for this processParameter
+     */
+    String name
+    /**
+     * A description for the processParameter
+     */
+    String description
+    /**
+     * The JobExecutionPlan for which this process parameter type has been created
+     */
+    JobExecutionPlan plan
+
+    static constraints = {
+        name(nullable: false, blank: false, unique: 'process')
+        description(nullable: true)
+    }
+}
