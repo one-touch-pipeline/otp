e0f6c7f1a5bb18ece051026a6881553a46cadced
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2011-11-28 14:10:30 +0100

    Extend ProcessService for information about ProcessingSteps
    Unit tests for service still missing.

--- grails-app/services/de/dkfz/tbi/otp/job/processing/ProcessService.groovy
+++ grails-app/services/de/dkfz/tbi/otp/job/processing/ProcessService.groovy
@@ -2,6 +2,7 @@ package de.dkfz.tbi.otp.job.processing
 
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.access.prepost.PostAuthorize
 
 /**
  * Service providing methods to access information about Processes.
@@ -9,6 +10,40 @@ import org.springframework.security.access.prepost.PreAuthorize
  */
 class ProcessService {
     static transactional = true
+
+    /**
+     * Security aware way to access a Process.
+     * @param id The Process's id
+     * @return
+     */
+    @PostAuthorize("hasPermission(returnObject.jobExecutionPlan, read) or hasRole('ROLE_ADMIN')")
+    public Process getProcess(long id) {
+        return Process.get(id)
+    }
+
+    /**
+    * Retrieves all ProcessingSteps for the given Process.
+    * @param process The Process whose ProcessingSteps should be retrieved
+    * @param max The number of elements to retrieve, default 10
+    * @param offset The offset in the list, default -
+    * @param column The column to search for, default "id"
+    * @param order {@code true} for ascending ordering, {@code false} for descending, default {@code false}
+    * @return List of all ProcessingSteps run for the Process filtered as requested
+    */
+    @PreAuthorize("hasPermission(#process.jobExecutionPlan, read) or hasRole('ROLE_ADMIN')")
+    public List<ProcessingStep> getAllProcessingSteps(Process process, int max = 10, int offset = 0, String column = "id", boolean order = false) {
+        return ProcessingStep.findAllByProcess(process, [max: max, offset: offset, sort: column, order: order ? "asc" : "desc"])
+    }
+
+    /**
+    * Returns the number of ProcessesingSteps for the given Process.
+    * @param plan The Process for which the number of ProcessingSteps should be returned
+    * @return The number of ProcessingSteps for the given Process
+    */
+    @PreAuthorize("hasPermission(#process.jobExecutionPlan, read) or hasRole('ROLE_ADMIN')")
+    public int getNumberOfProcessessingSteps(Process process) {
+        return ProcessingStep.countByProcess(process)
+    }
 
     /**
      * Retrieves the date when the given Process finished.
@@ -112,6 +147,56 @@ class ProcessService {
     @PreAuthorize("hasPermission(#step.process.jobExecutionPlan, read) or hasRole('ROLE_ADMIN')")
     public Date getLastUpdate(ProcessingStep step) {
         return lastUpdate(step).date
+    }
+
+    /**
+     * Retrieves the first update date for the ProcessingStep
+     * @param step
+     * @return
+     */
+    @PreAuthorize("hasPermission(#step.process.jobExecutionPlan, read) or hasRole('ROLE_ADMIN')")
+    public Date getFirstUpdate(ProcessingStep step) {
+        List<ProcessingStepUpdate> updates = ProcessingStepUpdate.findAllByProcessingStep(step)
+        if (updates.isEmpty()) {
+            throw new IllegalArgumentException("ProcessingStep has no updates")
+        }
+        return updates.sort { it.id }.first().date
+    }
+
+    /**
+     * Calculates the duration between the start and finish event. This means it ignores the
+     * created event as well as finished and success.
+     * @param step The processing step for which the duration has to be calculated
+     * @return The duration between the started and finished event for this ProcessingStep
+     */
+    @PreAuthorize("hasPermission(#step.process.jobExecutionPlan, read) or hasRole('ROLE_ADMIN')")
+    public Long getProcessingStepDuration(ProcessingStep step) {
+        List<ProcessingStepUpdate> updates = ProcessingStepUpdate.findAllByProcessingStep(step)
+        if (updates.isEmpty()) {
+            throw new IllegalArgumentException("ProcessingStep has no updates")
+        }
+        updates = updates.sort { it.id }
+        Date startDate = null
+        Date finishDate = null
+        // find the finished
+        for (ProcessingStepUpdate update in updates) {
+            if (update.state == ExecutionState.STARTED) {
+                startDate = update.date
+                continue
+            }
+            if (update.state == ExecutionState.FINISHED) {
+                finishDate = update.date
+                break
+            }
+            if (update.state == ExecutionState.FAILURE) {
+                finishDate = update.date
+                // no break as there could be another finish afterwards due to manual restart
+            }
+        }
+        if (finishDate) {
+            return finishDate.time - startDate.time
+        }
+        return null
     }
 
     /**
