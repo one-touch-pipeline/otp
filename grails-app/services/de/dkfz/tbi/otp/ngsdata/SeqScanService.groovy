6799f500f4254dc22436e8be5305c6940ab7f023
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2011-12-05 09:56:19 +0100

    creating SeqScan object based on the information in discovered bam file

--- grails-app/services/de/dkfz/tbi/otp/ngsdata/SeqScanService.groovy
+++ grails-app/services/de/dkfz/tbi/otp/ngsdata/SeqScanService.groovy
@@ -66,7 +66,7 @@ class SeqScanService {
             }
         }
     
-            println "found lanes: ${seqTracksToMerge}"
+        println "found lanes: ${seqTracksToMerge}"
     
             /*
             // check if all have bam file
@@ -79,71 +79,113 @@ class SeqScanService {
             */
     
     
-            // find old seqScan and invalidate it
-            def criteria = SeqScan.createCriteria()
-            def oldSeqScans = criteria.list {
-                and {
-                    eq("sample", sample)
-                    eq("seqTech", seqTech)
-                    eq("seqType", seqType)
-                }
+        // find old seqScan and invalidate it
+        def criteria = SeqScan.createCriteria()
+        def oldSeqScans = criteria.list {
+            and {
+                eq("sample", sample)
+                eq("seqTech", seqTech)
+                eq("seqType", seqType)
             }
+        }
     
-            oldSeqScans.each {SeqScan old ->
-                old.state = SeqScan.State.OBSOLETE
-                safeSave(old)
-            }
-    
-    
-            println "invalidating ${oldSeqScans.size()} seq scans"
+        oldSeqScans.each {SeqScan old ->
+            old.state = SeqScan.State.OBSOLETE
+            safeSave(old)
+        }
     
     
-            // create new seqScan
+        println "invalidating ${oldSeqScans.size()} seq scans"
     
-            SeqScan seqScan = new SeqScan(
-                alignmentParams : null,
-                sample: sample,
-                seqTech: seqTech,
-                seqType: seqType
-            )
+    
+        // create new seqScan
+    
+        SeqScan seqScan = new SeqScan(
+            alignmentParams : null,
+            sample: sample,
+            seqTech: seqTech,
+            seqType: seqType
+        )
     
             //sample.addToSeqScans(seqScan)
             //seqTech.addToSeqScans(seqScan)
             //seqType.addToSeqScans(seqScan)
     
-            seqTracksToMerge.each { SeqTrack iTrack ->
-                seqScan.addToSeqTracks(iTrack)
-                safeSave(iTrack)
-            }
-    
-            fillSeqScan(seqScan)
-            fillSeqCenters(seqScan)
-    
-            safeSave(seqScan)
-            safeSave(sample)
-            safeSave(seqTech)
-            safeSave(seqType)
+        seqTracksToMerge.each { SeqTrack iTrack ->
+            seqScan.addToSeqTracks(iTrack)
+            safeSave(iTrack)
         }
+
+        fillSeqScan(seqScan)
+        fillSeqCenters(seqScan)
+        fillInsertSize(seqScan)
+
+        safeSave(seqScan)
+        safeSave(sample)
+        safeSave(seqTech)
+        safeSave(seqType)
+    }
     
+
+    /**
+     * 
+     * @param tracks
+     * @param alignParams
+     * @return
+     */
+    SeqScan buildSeqScan(List<SeqTrack> tracks, AlignmentParams alignParams) {
+
+        // take parameters
+        SeqTrack seqTrack = tracks.get(0)
+        Sample sample = seqTrack.sample
+        SeqTech seqTech = seqTrack.seqTech
+        SeqType seqType = seqTrack.seqType
+
+        // create new seqScan
+
+        SeqScan seqScan = new SeqScan(
+            alignmentParams : alignParams,
+            sample: sample,
+            seqTech: seqTech,
+            seqType: seqType
+        )
+
+        tracks.each { SeqTrack iTrack ->
+            seqScan.addToSeqTracks(iTrack)
+            safeSave(iTrack)
+        }
+
+       fillSeqScan(seqScan)
+       fillSeqCenters(seqScan)
+       fillInsertSize(seqScan)
+
+       safeSave(seqScan)
+       safeSave(sample)
+       safeSave(seqTech)
+       safeSave(seqType)
+
+       //seqScan.refresh()
+       return seqScan
+    }
+
+
+    /**
+     *
+     * Fills SeqScan object with numbers derived from
+     * its SeqTrack objects. Coverage is calculated as
+     * number of base pairs divided by genome size (3e9).
+     *
+     * Coverage number will be replaced be a full calculation
+     * from coverage analysis
+     *
+     * @param seqScan - SeqScan object
+     */
     
+    private void fillSeqScan(SeqScan seqScan) {
     
-        /**
-         *
-         * Fills SeqScan object with numbers derived from
-         * its SeqTrack objects. Coverage is calculated as
-         * number of base pairs divided by genome size (3e9).
-         *
-         * Coverage number will be replaced be a full calculation
-         * from coverage analysis
-         *
-         * @param seqScan - SeqScan object
-         */
+        seqScan.nLanes = seqScan.seqTracks.size()
     
-        private void fillSeqScan(SeqScan seqScan) {
-    
-            seqScan.nLanes = seqScan.seqTracks.size()
-    
-            long nbp = 0
+        long nbp = 0
             seqScan.seqTracks.each {SeqTrack seqTrack ->
                 nbp += seqTrack.nBasePairs
             }
@@ -154,34 +196,67 @@ class SeqScanService {
     
     
     
-        /**
-         *
-         *  Most of samples are sequenced in one sequence center
-         *  if this is the case the string seqCenter for a given
-         *  SeqScan is filed.
-         *
-         * @param seqScan
-         */
-    
-        private void fillSeqCenters(SeqScan seqScan) {
-    
-            SeqCenter seqCenter = null
-            String name = ""
-    
-            seqScan.seqTracks.each {SeqTrack seqTrack ->
-    
-                if (seqCenter == null) {
-                    seqCenter = seqTrack.run.seqCenter
-                    name = seqCenter.name
-                }
-    
-                if (seqCenter != null) {
-                    if (seqTrack.run.seqCenter != seqCenter)
-                        name += "*"
-                }
+    /**
+    *
+    *  Most of samples are sequenced in one sequence center
+    *  if this is the case the string seqCenter for a given
+    *  SeqScan is filed.
+    *
+    * @param seqScan
+    */
+
+    private void fillSeqCenters(SeqScan seqScan) {
+
+        SeqCenter seqCenter = null
+        String name = ""
+
+        seqScan.seqTracks.each {SeqTrack seqTrack ->
+
+            if (seqCenter == null) {
+                seqCenter = seqTrack.run.seqCenter
+                name = seqCenter.name
             }
-            seqScan.seqCenters = name
+
+            if (seqCenter != null) {
+                if (seqTrack.run.seqCenter != seqCenter)
+                    name += "*"
+            }
         }
+        seqScan.seqCenters = name
+    }
+
+
+
+    /**
+     * 
+     * fills insert size as string
+     * in case of mized library "!" signs
+     * are used
+     * 
+     * @param SeqScan
+     */
+    private void fillInsertSize(SeqScan seqScan) {
+
+        boolean defined = false
+        int iSize = 0
+        String insertSize = ""
+
+       seqScan.seqTracks.each {SeqTrack seqTrack ->
+
+           if (!defined) {
+               iSize = seqTrack.insertSize
+               defined = true
+               insertSize += iSize
+           } else {
+               if (seqTrack.insertSize != iSize) {
+                   insertSize += "!"
+               }
+           }
+       }
+
+       seqScan.insertSize = insertSize
+    }
+
 
 
     /**
