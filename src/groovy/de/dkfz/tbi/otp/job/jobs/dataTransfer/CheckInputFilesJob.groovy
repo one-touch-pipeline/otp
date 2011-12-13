d058c36859f1200e9e20a524d6cf974e514d9b11
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2011-12-13 09:13:56 +0100

    Initial comit of the job to check if input files exists in the initial directory

---
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/dataTransfer/CheckInputFilesJob.groovy
@@ -0,0 +1,43 @@
+package de.dkfz.tbi.otp.job.jobs.dataTransfer
+
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.job.processing.*
+import org.springframework.beans.factory.annotation.Autowired
+
+@Component("checkInputFiles")
+@Scope("prototype")
+class CheckInputFilesJob extends AbstractEndStateAwareJobImpl {
+
+   /**
+    * dependency injection of meta data service
+    */
+    @Autowired
+    LsdfFilesService lsdfFilesService
+
+    /**
+     * Loop over all sequence and alignment files and check
+     * if they exists in the initial location.
+     * 
+     * @throws Exception
+     */
+    @Override
+    public void execute() throws Exception {
+
+        long runId = Long.parseLong(getProcessParameterValue("run"))
+        Run run = Run.get(runId)
+        String path = run.dataPath
+        succeed()
+
+        Set<DataFile> dataFiles = run.dataFiles
+        dataFiles.each {DataFile dataFile ->
+            if (dataFile.fileType == FileType.Type.SEQUENCE ||
+                dataFile.fileType == FileType.Type.ALIGNMENT) {
+                String fullPath = path + "/" + run.name + "/" + dataFile.pathName + 
+                    "/" + dataFile.fileName
+                if (!lsdfFilesService.fileExists(fullPath)) {
+                    fail()
+                }
+            }
+        }
+    }
+}
