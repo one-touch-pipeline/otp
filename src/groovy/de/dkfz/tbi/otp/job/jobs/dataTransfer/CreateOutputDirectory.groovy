bc51b5db3581f47f0389a7eb3114377a12c0e995
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2011-12-14 10:59:26 +0100

    initial commit

---
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/dataTransfer/CreateOutputDirectory.groovy
@@ -0,0 +1,32 @@
+package de.dkfz.tbi.otp.job.jobs.dataTransfer
+
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.job.processing.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+@Component("checkInputFiles")
+@Scope("prototype")
+class CreateOutputDirectoryJob extends AbstractJobImpl {
+
+    @Autowired
+    LsdfFilesService lsdfFilesService
+
+    final private String projectName = "PROJECT_NAME"
+
+    @Override
+    public void execute() throws Exception {
+
+        long runId = Long.parseLong(getProcessParameterValue("run"))
+        Run run = Run.get(runId)
+
+        String[] dirs = lsdfFilesService.getListOfRunDirecotries(run, projectName)
+
+        String cmd = ""
+        dirs.each {String line ->
+            cmd += "mkdir " + line + ";"
+        }
+        println cmd
+    }
+}
