33eeeb402b1db60d185ad537ffb3d7c9460b5c9a
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2011-12-14 12:40:29 +0100

    initial commit

---
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/dataTransfer/CheckViewByPidJob.groovy
@@ -0,0 +1,34 @@
+package de.dkfz.tbi.otp.job.jobs.dataTransfer
+
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.job.processing.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+@Component("checkFinalLocation")
+@Scope("prototype")
+class CheckViewByPidJob extends AbstractEndStateAwareJobImpl {
+
+    /**
+     * dependency injection of meta data service
+     */
+    @Autowired
+    FilesCompletenessService filesCompletenessService
+
+    /**
+     * Check if all files are linked in the view by pid structure
+     *
+     * @throws Exception
+     */
+    @Override
+    public void execute() throws Exception {
+        long runId = Long.parseLong(getProcessParameterValue("run"))
+        Run run = Run.get(runId)
+        if (filesCompletenessService.checkViewByPid(run)) {
+            succeed()
+        } else {
+            fail()
+        }
+    }
+}
