f2eec1aceebdf5ed6988904c1e794a6e7f4acf96
Jochen Schramm <j.schramm@dkfz-heidelberg.de>
2011-12-21 11:55:15 +0100

    Add missing parameter

--- src/groovy/de/dkfz/tbi/otp/job/processing/AbstractPbsJobImpl.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/processing/AbstractPbsJobImpl.groovy
@@ -24,8 +24,8 @@ abstract public class AbstractPbsJobImpl extends AbstractJobImpl implements PbsJ
      * To connect parameters set in properties file are used.
      * @return List of String with PBS ids
      */
-    protected List<String> sendPbsJob() {
-        File fileWithPbsId = pbsService.sendPbsJob()
+    protected List<String> sendPbsJob(String cmd = null) {
+        File fileWithPbsId = pbsService.sendPbsJob(cmd)
         if (!fileWithPbsId.isFile || fileWithPbsId.size() == 0) {
             throw new ProcessingException("File for PBS ids is not existing or empty.")
         }
