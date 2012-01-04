e37e306b335fdc8cb3892ad50ee536a526336683
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2012-01-04 12:42:16 +0100

    Support for constantParameter in DSL

--- src/groovy/de/dkfz/tbi/otp/utils/JobExecutionPlanDSL.groovy
+++ src/groovy/de/dkfz/tbi/otp/utils/JobExecutionPlanDSL.groovy
@@ -3,6 +3,9 @@ package de.dkfz.tbi.otp.utils
 import de.dkfz.tbi.otp.job.plan.JobDefinition
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
 import de.dkfz.tbi.otp.job.plan.StartJobDefinition
+import de.dkfz.tbi.otp.job.processing.Parameter
+import de.dkfz.tbi.otp.job.processing.ParameterType
+import de.dkfz.tbi.otp.job.processing.ParameterUsage
 
 class JobExecutionPlanDSL {
     public static def plan = { String name, c ->
@@ -17,7 +20,7 @@ class JobExecutionPlanDSL {
                 jep.startJob = startJobDefinition
                 assert(jep.save())
             }
-            c.job = { String jobName, String bean ->
+            c.job = { String jobName, String bean, closure = null ->
                 JobDefinition jobDefinition = new JobDefinition(name: jobName, bean: bean, plan: jep, previous: previous)
                 assert(jobDefinition.save())
                 if (!firstJob) {
@@ -28,6 +31,17 @@ class JobExecutionPlanDSL {
                     assert(previous.save())
                 }
                 previous = jobDefinition
+                if (closure) {
+                    closure.constantParameter = { String typeName, String value ->
+                        ParameterType type = new ParameterType(name: typeName, jobDefinition: jobDefinition, parameterUsage: ParameterUsage.INPUT)
+                        assert(type.save())
+                        Parameter parameter = new Parameter(type: type, value: value)
+                        assert(parameter.save())
+                        jobDefinition.addToConstantParameters(parameter)
+                        assert(jobDefinition.save())
+                    }
+                    closure()
+                }
             }
             c()
             jep.firstJob = firstJob
