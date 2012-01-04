6235d3b5a45736abab91b24bd20315d61396dd15
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2012-01-04 11:26:43 +0100

    Use new DSL for MetaDataWorkflow script

--- scripts/MetaDataWorkflow.groovy
+++ scripts/MetaDataWorkflow.groovy
@@ -1,39 +1,11 @@
-import de.dkfz.tbi.otp.job.plan.JobDefinition
-import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
-import de.dkfz.tbi.otp.job.plan.StartJobDefinition
-import de.dkfz.tbi.otp.job.processing.Parameter
-import de.dkfz.tbi.otp.job.processing.ParameterMapping
-import de.dkfz.tbi.otp.job.processing.ParameterType
-import de.dkfz.tbi.otp.job.processing.ParameterUsage
+import static de.dkfz.tbi.otp.utils.JobExecutionPlanDSL.*
 
-JobExecutionPlan jep = new JobExecutionPlan(name: "loadMetaData", planVersion: 0, enabled: true)
-assert(jep.save())
-StartJobDefinition definition = new StartJobDefinition(name: "start", bean: "metaDataStartJob", plan: jep)
-assert(definition.save())
-jep.startJob = definition
-
-JobDefinition registerInputFilesJob = new JobDefinition(name: "registerInputFiles", bean: "registerInputFilesJob", plan: jep)
-assert(registerInputFilesJob.save())
-JobDefinition loadMetaDataJob = new JobDefinition(name: "loadMetaData", bean: "metaDataJob", plan: jep, previous: registerInputFilesJob)
-assert(loadMetaDataJob.save())
-registerInputFilesJob.next = loadMetaDataJob
-assert(registerInputFilesJob.save())
-JobDefinition validateMetadataJob = new JobDefinition(name: "validateMetadata", bean: "validateMetadataJob", plan: jep, previous: loadMetaDataJob)
-assert(validateMetadataJob.save())
-loadMetaDataJob.next = validateMetadataJob
-assert(loadMetaDataJob.save())
-JobDefinition buildExecutionDateJob = new JobDefinition(name: "buildExecutionDate", bean: "buildExecutionDateJob", plan: jep, previous: validateMetadataJob)
-assert(buildExecutionDateJob.save())
-validateMetadataJob.next = buildExecutionDateJob
-assert(validateMetadataJob.save())
-JobDefinition buildSequenceTracksJob = new JobDefinition(name: "buildSequenceTracks", bean: "buildSequenceTracksJob", plan: jep, previous: buildExecutionDateJob)
-assert(buildSequenceTracksJob.save())
-buildExecutionDateJob.next = buildSequenceTracksJob
-assert(buildExecutionDateJob.save())
-JobDefinition checkSequenceTracksJob = new JobDefinition(name: "checkSequenceTracks", bean: "checkSequenceTracksJob", plan: jep, previous: buildSequenceTracksJob)
-assert(checkSequenceTracksJob.save())
-buildSequenceTracksJob.next = checkSequenceTracksJob
-assert(buildSequenceTracksJob.save())
-// set the first job
-jep.firstJob = registerInputFilesJob
-assert(jep.save(flush: true))
+plan("loadMetaData") {
+    start("start", "metaDataStartJob")
+    job("registerInputFiles", "registerInputFilesJob")
+    job("loadMetaData", "metaDataJob")
+    job("validateMetadata", "validateMetadataJob")
+    job("buildExecutionDate", "buildExecutionDateJob")
+    job("buildSequenceTracks", "buildSequenceTracksJob")
+    job("checkSequenceTracks", "checkSequenceTracksJob")
+}
