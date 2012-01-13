e208bb72a5b284016f22afb541f7bedd822e1bd7
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-01-13 13:08:21 +0100

    Start job check if given run was already processed. Only new runs are started.

--- src/groovy/de/dkfz/tbi/otp/job/jobs/metaData/MetaDataStartJob.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/metaData/MetaDataStartJob.groovy
@@ -11,26 +11,39 @@ import org.springframework.stereotype.Component
 @Component("metaDataStartJob")
 @Scope("singleton")
 class MetaDataStartJob extends AbstractStartJobImpl {
-    boolean performed = false
 
     @Scheduled(fixedRate=10000l)
     void execute() {
-        if (performed) {
-            println "Jobs already started ..."
-            return
-        }
+        int n = 0
         if (!getExecutionPlan() || !getExecutionPlan().enabled) {
-            println("Execution plan not set or not active")
+            println("meta data Execution plan not set or not active")
             return
         }
-        //return
-        println("Load Meta Data Start Job called")
-        // TODO Assure that the runs are processed only once. Verify via Process?
         List<Run> runs = Run.list()
-        runs.each { Run run ->
+        for(Run run in runs) {
+            if (processed(run)) {
+                continue
+            }
+            // new run to be processed
             createProcess(new ProcessParameter(value: run.id.toString(), className: run.class.name))
             println run.toString()
+            n++
         }
-        performed = true
+        println "MetaDataWorkflow: ${n} jobs started"
+    }
+
+    /**
+     * Checks if given run was already processed by MetaData execution plan
+     * @param run
+     * @return
+     */
+    boolean processed(Run run) {
+        List<ProcessParameter> processParameters =
+            ProcessParameter.findAllByValue(run.id.toString(), run.class.name)
+        for(ProcessParameter parameter in processParameters) {
+            if (parameter.process.jobExecutionPlan.id == getExecutionPlan().id) {
+                return true
+            }
+        }
     }
 }
