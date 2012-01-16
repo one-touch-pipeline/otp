36480b33c64fbc5689c366151c2f7ef1f93799bb
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-01-16 12:54:15 +0100

    adding calculateChecksum job and its watchdog

--- scripts/DataTransferWorkflow.groovy
+++ scripts/DataTransferWorkflow.groovy
@@ -6,10 +6,20 @@ plan("DataTransferWorkflow") {
     job("createOutputDirectory", "createOutputDirectoryJob") {
         constantParameter("project", "PROJECT_NAME")
     }
-    job("copyFiles", "copyFilesJob")
-    job("copyFilesWatchdog", "myPbsWatchdog")
+    job("copyFiles", "copyFilesJob") {
+        outputParameter("pbsIds")
+    }
+    job("copyFilesWatchdog", "myPBSWatchdogJob") {
+        inputParameter("pbsIds", "copyFiles", "pbsIds")
+    }
     // TODO: milestone
     job("checkFinalLocation", "checkFinalLocationJob")
+    job("calculateChecksum", "calculateChecksumJob") {
+        outputParameter("pbsIds")
+    }
+    job("calculateChecksumWatchdog", "myPBSWatchdogJob") {
+        inputParameter("pbsIds", "calculateChecksum", "pbsIds")
+    }
     job("createViewByPid", "createViewByPidJob")
     job("checkViewByPid", "checkViewByPidJob")
 }
