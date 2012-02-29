b797c593702eee87ad2a65fb994930dcf0c6387a
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-02-29 16:19:28 +0100

    DSL for bootstraping seq platforms

---
+++ src/groovy/de/dkfz/tbi/otp/ngsdata/SeqPlatformDSL.groovy
@@ -0,0 +1,14 @@
+package de.dkfz.tbi.otp.ngsdata
+
+class SeqPlatformDSL {
+
+    public static def seqPlatformDef = {String vendor, String model, c ->
+        SeqPlatform platform = new SeqPlatform(name: vendor, model: model)
+        assert(platform.save())
+        c.name = {String name ->
+         assert((new SeqPlatformModelIdentifier(seqPlatform: platform, name: name)).save())
+        }
+        c()
+        assert(platform.save(flush: true))
+    }
+}
