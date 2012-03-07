c17889351141728a6cc9769e7d7b9f1247917054
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-03-07 10:11:53 +0100

    start job for data location

---
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/dataLocation/DataLocationStartJob.groovy
@@ -0,0 +1,73 @@
+package de.dkfz.tbi.otp.job.jobs.dataLocation
+
+import de.dkfz.tbi.otp.job.processing.AbstractStartJobImpl
+import de.dkfz.tbi.otp.job.processing.Parameter
+import de.dkfz.tbi.otp.job.processing.Process
+import de.dkfz.tbi.otp.job.processing.ProcessParameter
+import de.dkfz.tbi.otp.ngsdata.Run
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Component
+
+@Component("dataLocationStartJob")
+@Scope("singleton")
+class DataLocationStartJob extends AbstractStartJobImpl {
+
+    final int MAX_RUNNING = 1
+
+    @Scheduled(fixedRate=5000l)
+    void execute() {
+        if (!hasOpenSlots()) {
+            return
+        }
+        int n = 0;
+        List<Run> runs = Run.findAllByComplete(true)
+        for(Run run in runs) {
+            if (!hasOpenSlots()) {
+                break
+            }
+            if (processed(run)) {
+                continue
+            }
+            // new run to be processed
+            createProcess(new ProcessParameter(value: run.id.toString(), className: run.class.name))
+            println run.toString()
+            n++
+        }
+        println "DataLocationWorkflow: ${n} jobs started"
+    }
+
+    /**
+    * Checks if given run was already processed by DataLocation execution plan
+    * @param run
+    * @return
+    */
+    boolean processed(Run run) {
+        List<ProcessParameter> processParameters =
+            ProcessParameter.findAllByValue(run.id.toString(), run.class.name)
+        for(ProcessParameter parameter in processParameters) {
+            if (parameter.process.jobExecutionPlan.id == getExecutionPlan().id) {
+                return true
+            }
+        }
+    }
+
+    boolean hasOpenSlots() {
+        if (!getExecutionPlan() || !getExecutionPlan().enabled) {
+            return false
+        }
+        int numberOfRunning = numberOfRunningProcesses()
+        if (numberOfRunning >= MAX_RUNNING) {
+            return false
+        }
+        return true
+    }
+
+    /**
+     * returns number of running processes for this execution plan
+     * @return
+     */
+    private int numberOfRunningProcesses() {
+        return Process.countByFinishedAndJobExecutionPlan(false, getExecutionPlan())
+    }
+}
