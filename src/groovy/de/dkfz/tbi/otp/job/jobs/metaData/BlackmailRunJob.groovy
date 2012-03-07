0b426ac63ad75178ca30a1b05998e722d39c5080
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-03-07 10:10:43 +0100

    job to blacklist known problematic  runs

---
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/metaData/BlackmailRunJob.groovy
@@ -0,0 +1,37 @@
+package de.dkfz.tbi.otp.job.jobs.metaData
+
+import org.springframework.beans.factory.annotation.Autowired
+import de.dkfz.tbi.otp.job.processing.AbstractEndStateAwareJobImpl
+import de.dkfz.tbi.otp.ngsdata.Run
+
+class BlackmailRunJob extends AbstractEndStateAwareJobImpl {
+
+    @Override
+    public void execute() throws Exception {
+        long runId = Long.parseLong(getProcessParameterValue())
+        Run run = Run.get(runId)
+        if (checkRun(run)) {
+            run.blacklisted = false
+            succeed()
+        } else {
+            run.blacklisted = true
+            fail()
+        }
+        run.save(flush: true)
+    }
+
+    boolean checkRun(Run run) {
+        List<String> blackList = [
+            "110218_HWUSI-EAS451_00020_FC_62YJKAAXX",
+            "110325_SN750_0056_BB0244ABXX",
+            "110325_SN750_0055_AB022NABXX",
+            "110616_SN750_0075_BC018AACXX",
+            "110826_SN169_0223_BD07H6ACXX",
+            "110714_SN750_0086_BB07Y9ABXX"
+        ]
+        if (blackList.contains(run.name)) {
+            return false
+        }
+        return true
+    }
+}
