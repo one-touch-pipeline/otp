f1b642cdb6d691bc3deb9c048d46e77636ac29f2
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-03-21 11:31:17 +0100

    project specific overview page

---
+++ grails-app/controllers/de/dkfz/tbi/otp/ngsdata/OverviewMBController.groovy
@@ -0,0 +1,65 @@
+package de.dkfz.tbi.otp.ngsdata
+import de.dkfz.tbi.otp.ngsdata.WorkPackagesDSL
+
+class OverviewMBController {
+
+    def index() {
+
+        Calendar cal = Calendar.getInstance()
+        cal.set(2011, 11, 1)
+        Date date = cal.getTime()
+
+        List stringCenters = new ArrayList()
+
+
+        List<SeqCenter> centers = SeqCenter.findAll()
+        for(SeqCenter center in centers) {
+            List<String> text = new ArrayList<String>()
+            int nRuns = Run.countBySeqCenter(center)
+            int nNew = Run.countBySeqCenterAndDateExecutedGreaterThan(center, date)
+            println "${center} ${nRuns} ${nNew}"
+            text << center
+            text << nRuns
+            text << nNew
+            stringCenters << text
+        }
+
+        List<SeqType> wp = WorkPackagesDSL.make {
+            seqType("WHOLE_GENOME", "PAIRED")
+            seqType("WHOLE_GENOME", "SINGLE")
+            seqType("WHOLE_GENOME_BISULFITE", "PAIRED")
+            seqType("WHOLE_GENOME", "MATE_PAIR")
+            seqType("RNA", "PAIRED")
+            seqType("MI_RNA", "SINGLE")
+            seqType("mi_rna_sequencing", "SINGLE")
+        }
+
+        List stringTypes = new ArrayList()
+        for(SeqType type in wp) {
+            List<String> text = new ArrayList<String>()
+            int nTracks = SeqTrack.countBySeqType(type)
+            int nTracksNew = countNewSeqTracks(type)
+            println "${type} ${nTracks} ${nTracksNew}"
+            text << type
+            text << nTracks
+            text << nTracksNew
+            stringTypes << text
+        }
+        [centers: stringCenters, types: stringTypes]
+    }
+
+    private int countNewSeqTracks(SeqType type) {
+        Calendar cal = Calendar.getInstance()
+        cal.set(2011, 11, 1)
+        Date date = cal.getTime()
+
+        int N = 0;
+        List<SeqTrack> tracks = SeqTrack.findAllBySeqType(type)
+        for(SeqTrack track in tracks) {
+            if (track.run.dateExecuted > date) {
+                N++
+            }
+        }
+        return N
+    }
+}
---
+++ grails-app/controllers/de/dkfz/tbi/otp/ngsdata/OverviewPAController.groovy
@@ -0,0 +1,46 @@
+package de.dkfz.tbi.otp.ngsdata
+
+import de.dkfz.tbi.otp.ngsdata.WorkPackagesDSL
+
+class OverviewPAController {
+
+    def index() {
+        Project project = Project.findByName("astrocytome")
+        String pattern = "PA%"
+        List<Individual> inds = Individual.findAllByProjectAndMockPidLike(project, pattern)
+
+        //println project
+        //println inds
+
+        List<SeqType> wp = WorkPackagesDSL.make {
+            seqType("WHOLE_GENOME", "PAIRED")
+            seqType("WHOLE_GENOME", "MATE_PAIR")
+            seqType("RNA", "PAIRED")
+            seqType("MI_RNA", "SINGLE")
+        }
+
+        List table = new ArrayList()
+        for(Individual ind in inds) {
+            List person = new ArrayList()
+            person << ind.mockPid
+            Sample sample = Sample.findByIndividualAndType(ind, Sample.Type.TUMOR)
+            for(SeqType type in wp) {
+                SeqScan scan = SeqScan.findBySampleAndSeqType(sample, type)
+                person << statusSeqScan(scan)
+            }
+            table << person
+        }
+        [table: table, scans: wp]
+    }
+
+    private String statusSeqScan(SeqScan scan) {
+        String str = ""
+        if (!scan) {
+            return ""
+        }
+        for(int i=0; i<scan.nLanes; i++) {
+            str += "o"
+        }
+        return str
+    }
+}
