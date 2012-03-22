9a20eab27dfa5e7a17f5274afa4b9eccd523f245
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-03-22 16:00:32 +0100

    DSL to define work packages

---
+++ src/groovy/de/dkfz/tbi/otp/ngsdata/WorkPackagesDSL.groovy
@@ -0,0 +1,19 @@
+package de.dkfz.tbi.otp.ngsdata
+
+class WorkPackagesDSL {
+
+    List<SeqType> wp = null
+
+    def static make (closure) {
+        WorkPackagesDSL wpDSL = new WorkPackagesDSL()
+        wpDSL.wp = new ArrayList<SeqType>()
+        closure.delegate = wpDSL
+        closure()
+        return wpDSL.wp
+    }
+
+    def seqType (String type, String library) {
+        SeqType s = SeqType.findByNameAndLibraryLayout(type, library)
+        wp << s
+    }
+}
