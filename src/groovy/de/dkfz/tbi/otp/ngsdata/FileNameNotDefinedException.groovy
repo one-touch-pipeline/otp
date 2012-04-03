a4a796b4a7d90c5dc278e17836d7d9d62d16d49d
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-04-03 09:16:19 +0200

    info string contains fileId

--- src/groovy/de/dkfz/tbi/otp/ngsdata/FileNameNotDefinedException.groovy
+++ src/groovy/de/dkfz/tbi/otp/ngsdata/FileNameNotDefinedException.groovy
@@ -2,7 +2,7 @@ package de.dkfz.tbi.otp.ngsdata
 
 public class FileNameNotDefinedException extends MetaDataFormatException {
 
-    public FileNameNotDefinedException(String runName) {
-        super("FileName not defined for a file in run ${runName}")
+    public FileNameNotDefinedException(String dataFileId) {
+        super("FileName not defined for id: ${dataFileId}")
     }
 }
