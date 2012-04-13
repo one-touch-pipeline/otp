8b3f31f73f4dcc84aead4fa8938bca2692c493a2
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-04-13 14:59:56 +0200

    GUI to submit a run

---
+++ grails-app/controllers/de/dkfz/tbi/otp/ngsdata/RunSubmitController.groovy
@@ -0,0 +1,48 @@
+package de.dkfz.tbi.otp.ngsdata
+
+class RunSubmitController {
+
+    def index() {
+        def centers = SeqCenter.list()
+        def seqPlatform = SeqPlatform.list()
+        [centers: centers, seqPlatform: seqPlatform]
+    }
+
+    def submit() {
+
+        SeqPlatform platform = getPlatform(params.seqPlatform)
+
+        Run run = new Run(
+            name: params.runName,
+            multipleSource: false,
+            seqCenter: SeqCenter.findByName(params.seqCenter),
+            seqPlatform: platform
+        )
+        if (!safeSave(run)) {
+            return
+        }
+        RunInitialPath path = new RunInitialPath(
+            dataPath: params.dataPath, 
+            mdPath: params.dataPath,
+            run: run
+        )
+        safeSave(path)
+        redirect(controller: "run", action: "show", id: run.id)
+    }
+
+    SeqPlatform getPlatform(String fullName) {
+        int idx = fullName.indexOf(" ")
+        String name = fullName.substring(0, idx)
+        String model = fullName.substring(idx+1)
+        return SeqPlatform.findByNameAndModel(name, model)
+    }
+
+    boolean safeSave(Object obj) {
+        if (obj.validate() && obj.save(flush: true)) {
+            return true
+        } else {
+            render("<h1>Errors</h1><p>${obj.errors}")
+            return false
+        }
+    }
+}
---
+++ grails-app/views/runSubmit/index.gsp
@@ -0,0 +1,38 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="layout" content="main"/>
+<title>Run Registration</title>
+</head>
+<body>
+  <div class="body">
+     <h1>Run registration</h1>
+
+    <g:form controller="runSubmit" action="submit">
+    <table>
+        <tr>
+            <td class="myKey">run name</td>
+            <td><g:textField name="runName"size="30"/></td>
+        </tr>
+        <tr>
+            <td class="myKey">sequencing center</td>
+            <td><g:select name="seqCenter" from="${centers}" /></td>
+        </tr>
+        <tr>
+            <td class="myKey">sequencing platform</td>
+            <td><g:select name="seqPlatform" from="${seqPlatform}" /></td>
+        </tr>
+        <tr>
+            <td class="myKey">data location</td>
+            <td><g:textField name="dataPath"size="80" /></td>
+        </tr>
+        <tr>
+            <td></td><td><g:submitButton name="submit" value="Submit" /></td>
+        </tr>
+
+    </table>
+    </g:form>
+  </div>
+</body>
+</html>
