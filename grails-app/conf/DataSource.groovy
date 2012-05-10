4c7c20a3d64a1c30425edf1db51e375dceb2d132
Jochen Schramm <j.schramm@dkfz-heidelberg.de>
2012-05-10 10:22:32 +0200

    Production database properties are configurable
    The connection properties of the production database are now set
    via properties file. This is done to make them easily configurable.
    Just add the connection details to your .otp.properties file.
    It is still assumed that PostgreSQL is the only target
    database server system.
    If the dbCreate option is not set in the properties file it is set
    to 'validate'. Validate ensures that Hibernate does not change
    database schema but throws an exception during application's startup if
    database schema and data objects differ somehow.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,5 +1,34 @@
+Properties databaseProperties = new Properties()
+try {
+    databaseProperties.load(new FileInputStream(System.getProperty("user.home") + "/.otp.properties"))
+    String server = databaseProperties.getProperty("otp.database.server")
+    String port = databaseProperties.getProperty("otp.database.port")
+    String database = databaseProperties.getProperty("otp.database.database")
+    databaseProperties.setProperty("otp.database.url", "jdbc:postgresql://${server}:${port}/${database}")
+    databaseProperties.setProperty("otp.database.pooled", "true")
+    // If dbCreate option is not set in the properties file, set it to validate
+    if (databaseProperties.getProperty("otp.database.dbCreate") == null) {
+        databaseProperties.setProperty("otp.database.dbCreate", "validate")
+    }
+    environments {
+        test {
+            throw new Exception("Test system")
+        }
+    }
+} catch (Exception e) {
+    // no database configured yet, use hsqldb
+    databaseProperties.setProperty("otp.database.driver", "org.hsqldb.jdbcDriver")
+    databaseProperties.setProperty("otp.database.username", "sa")
+    databaseProperties.setProperty("otp.database.password", "")
+    databaseProperties.setProperty("otp.database.url", "jdbc:hsqldb:mem:devDb")
+    databaseProperties.setProperty("otp.database.pooled", "false")
+    databaseProperties.setProperty("otp.database.dbCreate", "create")
+    databaseProperties.setProperty("otp.database.dialect", "org.hibernate.dialect.HSQLDialect")
+}
+def databaseConfig = new ConfigSlurper().parse(databaseProperties)
+
 dataSource {
-    pooled = true
+    pooled = Boolean.parseBoolean(databaseConfig.otp.database.pooled)
     driverClassName = "org.h2.Driver" 
 	//driverClassName = "com.mysql.jdbc.Driver"
     //dialect = org.hibernate.dialect.MySQL5InnoDBDialect
@@ -35,13 +64,12 @@ environments {
     }
     production {
         dataSource {
-            // TODO: make configurable
             driverClassName = "org.postgresql.Driver"
             dialect = org.hibernate.dialect.PostgreSQLDialect
-            dbCreate = "update"
-            username = "otp"
-            password = "otp"
-            url = "jdbc:postgresql://localhost:5432/otp"
+            dbCreate = databaseConfig.otp.database.dbCreate
+            username = databaseConfig.otp.database.username
+            password = databaseConfig.otp.database.password
+            url = databaseConfig.otp.database.url
         }
     }
 }
