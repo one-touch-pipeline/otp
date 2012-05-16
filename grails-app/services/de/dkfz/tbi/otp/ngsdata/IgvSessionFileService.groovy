8947436e8445a8c9f541956c0047b17851d4aa65
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-05-16 16:31:17 +0200

    clean-up. removing commented out code

--- grails-app/services/de/dkfz/tbi/otp/ngsdata/IgvSessionFileService.groovy
+++ grails-app/services/de/dkfz/tbi/otp/ngsdata/IgvSessionFileService.groovy
@@ -8,8 +8,6 @@ class IgvSessionFileService {
 
     private final String header = '<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n'
 
-    //private final String myMut = "https://otp.local/otpdevel/igvSessionFile/mutFile/"
-
     String buildSessionFile(List<SeqScan> scans) {
         String text = buildContent(scans)
         String hash = Integer.toHexString(text.hashCode())
@@ -50,17 +48,5 @@ class IgvSessionFileService {
         String dataWebServer = configService.dataWebServer()
         String path = "${dataWebServer}/${dataFile.filePath}/${dataFile.fileName}"
         return path
-        /*
-        String path = ""
-        String projPath = scan.sample.individual.project.dirName
-        String typePath = scan.seqType.dirName
-        String pid = scan.sample.individual.pid
-        String sampleType = scan.sample.type.toString().toLowerCase()
-        String layout = scan.seqType.libraryLayout.toLowerCase()
-        DataFile bamFile = MergedAlignmentDataFile.findByMergingLog(merging)
-        String fileName = bamFile.fileName
-        path = "${webServer}/${projPath}/sequencing/${typePath}/view-by-pid/${pid}/${sampleType}/${layout}/merged-alignment/${fileName}"
-        return path
-        */
     }
 }
