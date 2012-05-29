819e2705ec41d00812631deef1effd74c64f7530
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-05-29 16:21:28 +0200

    Integration test for ProjectProgressService. The aim of the test is to check if there is no Exception due to eg. typo. The values returned by the service are not checked.

---
+++ test/integration/de/dkfz/tbi/otp/ngsdata/ProjectProgressServiceTests.groovy
@@ -0,0 +1,40 @@
+package de.dkfz.tbi.otp.ngsdata
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class ProjectProgressServiceTests {
+
+    def projectProgressService
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testListOfRuns() {
+        List<Project> projects = Project.list()
+        Date date = new Date() - 50
+        List<Run> runs = projectProgressService.getListOfRuns(projects, date)
+        for (Run run in runs) {
+            println run.name
+        }
+    }
+
+    @Test
+    void testSamples() {
+        List<Project> projects = Project.list()
+        Date date = new Date() - 50
+        List<Run> runs = projectProgressService.getListOfRuns(projects, date)
+        for (Run run in runs) {
+            Set<String> samples = projectProgressService.getSamples(run)
+            println samples
+        }
+    }
+}
