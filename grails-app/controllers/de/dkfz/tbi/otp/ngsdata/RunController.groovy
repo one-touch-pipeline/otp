bdd6b9ea0cd64b2de16e53b48d6c57265d82aefb
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2012-06-18 10:37:44 +0200

    Drop submit functionality from Run controller
    There is a dedicated controller for this functionality nowadays.

--- grails-app/controllers/de/dkfz/tbi/otp/ngsdata/RunController.groovy
+++ grails-app/controllers/de/dkfz/tbi/otp/ngsdata/RunController.groovy
@@ -7,7 +7,6 @@ class RunController {
 
     def lsdfFilesService
     def metaDataService
-    def seqTrackService
     def processService
 
     static scaffold = Run
@@ -48,69 +47,5 @@ class RunController {
         List<Run> runs =
              Run.findAllByIdGreaterThan(run.id, [sort: "id", order: "asc", max: 1])
         return (runs.size()>0) ? runs.get(0).id : run.id
-    }
-    
-    
-    def submitForm = {
-        List<SeqCenter> centers = SeqCenter.findAll()
-        List<String> centerNames = new Vector<String>()
-        centers.each { centerNames << it.name }
-        List<SeqPlatform> seqTechs = SeqPlatform.findAll()
-        [centers: centerNames, seqTechs: seqTechs ]
-    }
-
-    def submit = {
-        SeqCenter seqCenter = SeqCenter.findByName(params.center)
-        SeqPlatform seqTech = SeqPlatform.findByName(params.seqTech)
-        Run run = Run.findByName(params.runName)
-        if (run) {
-            log.debug("Run ${params.runName} already exist")
-        } else {
-            run = new Run(
-                    name: params.runName,
-                    seqCenter: seqCenter,
-                    seqTech: seqTech,
-                    )
-        }
-        run.dataPath = params.dataPath
-        run.mdPath = params.mdPath
-        run.save(flush: true)
-        log.debug(run)
-        long id = run.id
-
-        // processing and poor's man time measurement
-
-        def times = []
-        println run.name
-        times << new Date().getTime()
-        metaDataService.registerInputFiles(id)
-        times << new Date().getTime()
-        metaDataService.loadMetaData(id)
-        times << new Date().getTime()
-        boolean valid = metaDataService.validateMetadata(id)
-        if (!valid) {
-            println "!!! Meta Data not valid !!!"
-            render ("!!! Meta Data not valid !!!")
-            return
-        }
-        times << new Date().getTime()
-        metaDataService.buildExecutionDate(id)
-        times << new Date().getTime()
-        seqTrackService.buildSequenceTracks(id)
-        times << new Date().getTime()
-        seqTrackService.checkSequenceTracks(id)
-        times << new Date().getTime()
-        metaDataService.assignFilesToProjects(id)
-        times << new Date().getTime()
-        lsdfFilesService.runInFinalLocation(id)
-        lsdfFilesService.checkAllFiles(id)
-        times << new Date().getTime()
-
-        println "total ${times[8]-times[0]}"
-        for(int i=1; i<times.size(); i++) {
-            println "step ${i} ${times[i]-times[i-1]}"
-        }
-
-        redirect(action: "show", id:id)
     }
 }
