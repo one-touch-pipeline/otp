0142e51770757c289c946be7b23c20886a358a0e
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-08-22 16:48:45 +0200

    taking server URL from the configuration file in development mode

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -73,7 +73,7 @@ grails.exceptionresolver.params.exclude = ['password']
 environments {
     development {
         grails.logging.jul.usebridge = true
-        grails.serverURL = "http://localhost:8080/${appName}"
+        grails.serverURL = "${otpConfig.otp.server.url}${appName}"
     }
     production {
         grails.logging.jul.usebridge = false
