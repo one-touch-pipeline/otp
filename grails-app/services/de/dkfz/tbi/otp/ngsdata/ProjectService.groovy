01f9a7a68dda7c6a3ce7548bf0074a67de5935b2
Martin Graesslin <m.graesslin@dkfz-heidelberg.de>
2012-10-16 10:29:06 +0200

    Add createProject method to ProjectService
    Method takes care of creating the Project and granting read/write
    permission to all Groups which have the specific privilege.
    
    REVIEW: 139

--- grails-app/services/de/dkfz/tbi/otp/ngsdata/ProjectService.groovy
+++ grails-app/services/de/dkfz/tbi/otp/ngsdata/ProjectService.groovy
@@ -2,12 +2,23 @@ package de.dkfz.tbi.otp.ngsdata
 
 import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.security.access.prepost.PostFilter
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.acls.domain.BasePermission
+import org.springframework.security.acls.domain.GrantedAuthoritySid
+import org.springframework.security.acls.model.Sid
+
+import de.dkfz.tbi.otp.security.Group
+
 
 /**
  * Service providing methods to access information about Projects.
  *
  */
 class ProjectService {
+    /**
+     * Dependency Injection for aclUtilService
+     */
+    def aclUtilService
 
     /**
      *
@@ -26,5 +37,30 @@ class ProjectService {
     @PostAuthorize("returnObject == null or hasPermission(returnObject, 'read') or hasRole('ROLE_OPERATOR')")
     public Project getProject(Long id) {
         return Project.get(id)
+    }
+
+    /**
+     * Creates a Project and grants permissions to Groups which have read/write privileges for Projects.
+     * @param name
+     * @param dirName
+     * @param realmName
+     * @return The created project
+     */
+    @PreAuthorize("hasRole('ROLE_OPERATOR')")
+    public Project createProject(String name, String dirName, String realmName) {
+        Project project = new Project(name: name, dirName: dirName, realmName: realmName)
+        project = project.save(flush: true)
+        assert(project != null)
+        // add to groups
+        Group.list().each { Group group ->
+            if (group.readProject) {
+                Sid sid = new GrantedAuthoritySid(group.role.authority)
+                aclUtilService.addPermission(project, sid, BasePermission.READ)
+                if (group.writeProject) {
+                    aclUtilService.addPermission(project, sid, BasePermission.WRITE)
+                }
+            }
+        }
+        return project
     }
 }
