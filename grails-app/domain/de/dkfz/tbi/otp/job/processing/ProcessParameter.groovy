8a448c2385d02f285a0acb5597e28cf954b096fa
Sylwester Radomski <s.radomski@dkfz-heidelberg.de>
2012-12-04 11:41:39 +0100

    Script to remove a run from database
    With this script it is possible to remove a run from a database.
    The provesses executed on the run (and objects belong to a run)
    are not deleted.

--- grails-app/domain/de/dkfz/tbi/otp/job/processing/ProcessParameter.groovy
+++ grails-app/domain/de/dkfz/tbi/otp/job/processing/ProcessParameter.groovy
@@ -12,12 +12,15 @@ class ProcessParameter {
     /**
      * Retrieves the domain object instance this ProcessParameter points to in case className is not null.
      *
-     * If className is null, this method returns null.
+     * If className is null or object does not exists, this method returns null.
      * @return The domain object instance or null
      */
     def toObject() {
         if (className) {
-            return ProcessParameter.executeQuery("FROM " + className + " WHERE id=" + value).first()
+            List resultList = ProcessParameter.executeQuery("FROM " + className + " WHERE id=" + value)
+            if (resultList) {
+                return resultList.first()
+            }
         }
         return null
     }
