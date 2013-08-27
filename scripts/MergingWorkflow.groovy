01308e77dd820779c92b4e5585586fc41576c27a
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2013-08-27 09:24:08 +0200

    add script of needed database changes to merging workflow script

--- scripts/MergingWorkflow.groovy
+++ scripts/MergingWorkflow.groovy
@@ -1,4 +1,5 @@
 import static de.dkfz.tbi.otp.utils.JobExecutionPlanDSL.*
+import de.dkfz.tbi.otp.dataprocessing.*
 
 plan("MergingWorkflow") {
     start("start", "mergingStartJob")
@@ -24,3 +25,21 @@ plan("MergingWorkflow") {
     job("mergingFileIndexValidation", "mergingFileIndexValidationJob")
     job("mergingComplete", "mergingCompleteJob")
 }
+
+//special pbs options for merging workflow
+println ctx.processingOptionService.createOrUpdate(
+  "PBS_mergingJob",
+  "DKFZ",
+  null,
+  '{"-l": {nodes: "1:ppn=6:lsdf", walltime: "100:00:00", mem: "50g"}}',
+  "merging job depending cluster option for dkfz"
+)
+
+//picard option for mark duplicates
+println ctx.processingOptionService.createOrUpdate(
+  "picardMdup",
+  null,
+  null,
+  'VALIDATION_STRINGENCY=SILENT REMOVE_DUPLICATES=FALSE ASSUME_SORTED=TRUE MAX_RECORDS_IN_RAM=12500000 CREATE_INDEX=TRUE',
+  "picard option used in duplicates marking"
+)
