ba0b1cb3d1f71e0e4e2247aa495190b348be812b
Pavel Komardin <p.komardin@dkfz-heidelberg.de>
2013-10-11 09:43:04 +0200

    bug-fix: ConveyBwaAlignmentJob: seqType.name

--- src/groovy/de/dkfz/tbi/otp/job/jobs/alignment/ConveyBwaAlignmentJob.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/alignment/ConveyBwaAlignmentJob.groovy
@@ -55,7 +55,7 @@ class ConveyBwaAlignmentJob extends AbstractJobImpl {
         String qaSwitch = qualityEncoding(alignmentPass)
         String nCores = optionService.findOptionSafe("conveyBwaNumberOfCores", null, null)
         String bwaLogFilePath = processedSaiFileService.bwaAlnErrorLogFilePath(saiFile)
-        String qParameter = optionService.findOptionSafe("bwaQParameter", alignmentPassService.seqType(alignmentPass), alignmentPassService.project(alignmentPass))
+        String qParameter = optionService.findOptionSafe("bwaQParameter", alignmentPassService.seqType(alignmentPass).name, alignmentPassService.project(alignmentPass))
         String bwaCmd = "${conveyBwaCommand} aln ${nCores} ${qaSwitch} ${qParameter} ${referenceGenomePath} ${dataFilePath} -f ${saiFilePath} 2> ${bwaLogFilePath}"
         String bwaErrorCheckingCmd = BwaErrorHelper.failureCheckScript(saiFilePath, bwaLogFilePath)
         String chmodCmd = "chmod 440 ${saiFilePath} ${bwaLogFilePath}"
