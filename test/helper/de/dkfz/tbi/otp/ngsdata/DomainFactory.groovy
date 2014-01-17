496cf062b285394d05a338d068e2864db402f267
Manuel Prinz <m.prinz@dkfz-heidelberg.de>
2014-01-17 14:37:58 +0100

    Added Realms for BioQuant to DomainFactory

--- test/helper/de/dkfz/tbi/otp/ngsdata/DomainFactory.groovy
+++ test/helper/de/dkfz/tbi/otp/ngsdata/DomainFactory.groovy
@@ -9,6 +9,11 @@ class DomainFactory {
     private DomainFactory() {
     }
 
+    static final String DEFAULT_HOST = 'localhost'
+    static final int    DEFAULT_PORT = 22
+    static final String LABEL_DKFZ = 'DKFZ'
+    static final String LABEL_BIOQUANT = 'BioQuant'
+
     public static final Map REALM_DEFAULTS = [
         name: 'FakeRealm',
         env: Environment.current.name,
@@ -16,31 +21,55 @@ class DomainFactory {
         processingRootPath: '#/invalidPath/processing',
         loggingRootPath:    '#/invalidPath/log',
         programsRootPath:   '#/invalidPath/programs',
-        webHost: 'localhost',
-        host: 'localhost',
-        port: 22,
+        webHost: DEFAULT_HOST,
+        host: DEFAULT_HOST,
+        port: DEFAULT_PORT,
         unixUser: '!fakeuser',
         timeout: 60,
         pbsOptions: '',
     ]
 
-    public static final Map REALM_DEFAULTS_DKFZ = REALM_DEFAULTS + [
-        name: 'DKFZ',
+    public static final Map REALM_DEFAULTS_BIOQUANT_CLUSTER = REALM_DEFAULTS + [
+        name: LABEL_BIOQUANT,
+        cluster: Realm.Cluster.BIOQUANT,
+        host: 'otphost-other.example.org',
+        port: DEFAULT_PORT,
+        unixUser: 'unixUser2',
+        pbsOptions: '{"-l": {nodes: "1:xeon", walltime: "5:00"}, "-W": {x: "NACCESSPOLICY:SINGLEJOB"}}',
+    ]
+
+    public static final Map REALM_DEFAULTS_DKFZ_CLUSTER = REALM_DEFAULTS + [
+        name: LABEL_DKFZ,
         cluster: Realm.Cluster.DKFZ,
         host: 'headnode',
-        port: 22,
+        port: DEFAULT_PORT,
         unixUser: 'otptest',
         pbsOptions: '{"-l": {nodes: "1:lsdf", walltime: "5:00"}}',
     ]
 
-    public static Realm createRealmDataManagementDKFZ(Map myProps = null) {
-        new Realm(REALM_DEFAULTS_DKFZ + [
+    public static Realm createRealmDataManagementBioQuant(Map myProps = [:]) {
+        new Realm(REALM_DEFAULTS_BIOQUANT_CLUSTER + [
             operationType: Realm.OperationType.DATA_MANAGEMENT,
         ] + myProps)
     }
 
-    public static Realm createRealmDataProcessingDKFZ(Map myProps = null) {
-        new Realm(REALM_DEFAULTS_DKFZ + [
+    public static Realm createRealmDataProcessingBioQuant(Map myProps = [:]) {
+        // NOTE: Data processing for BQ projects is done on DKFZ cluster, so the name needs to
+        //       to be adjusted.
+        new Realm(REALM_DEFAULTS_DKFZ_CLUSTER + [
+            name: LABEL_BIOQUANT,
+            operationType: Realm.OperationType.DATA_PROCESSING,
+        ] + myProps)
+    }
+
+    public static Realm createRealmDataManagementDKFZ(Map myProps = [:]) {
+        new Realm(REALM_DEFAULTS_DKFZ_CLUSTER + [
+            operationType: Realm.OperationType.DATA_MANAGEMENT,
+        ] + myProps)
+    }
+
+    public static Realm createRealmDataProcessingDKFZ(Map myProps = [:]) {
+        new Realm(REALM_DEFAULTS_DKFZ_CLUSTER + [
             operationType: Realm.OperationType.DATA_PROCESSING,
         ] + myProps)
     }
