3c344e469bee3c1d12bc78d78c54758926668ffe
Manuel Prinz <m.prinz@dkfz-heidelberg.de>
2014-01-23 13:59:27 +0100

    Added documentation to DomainFactory

--- test/helper/de/dkfz/tbi/otp/ngsdata/DomainFactory.groovy
+++ test/helper/de/dkfz/tbi/otp/ngsdata/DomainFactory.groovy
@@ -14,6 +14,23 @@ class DomainFactory {
     static final String LABEL_DKFZ = 'DKFZ'
     static final String LABEL_BIOQUANT = 'BioQuant'
 
+    /**
+     * Defaults for new realms.
+     * <p>
+     * The values are set to something passing the validator but often make no sense,
+     * so they should be overwritten with something proper. This is intentional, so
+     * they do not work by chance. The current defaults are:
+     * <ul>
+     * <li>the name "FakeRealm",</li>
+     * <li>the current environment,</li>
+     * <li>all paths beginning with "<code>#/invalidPath/</code>",</li>
+     * <li>all hosts set to "localhost",</li>
+     * <li>a port of 22 (SSH)</li>
+     * <li>a unix user called "!fakeuser",</li>
+     * <li>a time-out of 60</li>
+     * <li>no PBS options.</li>
+     * </ul>
+     */
     public static final Map REALM_DEFAULTS = [
         name: 'FakeRealm',
         env: Environment.current.name,
@@ -29,6 +46,7 @@ class DomainFactory {
         pbsOptions: '',
     ]
 
+    /** Default settings for the BioQuant cluster. These include the {@link #REALM_DEFAULTS}. */
     public static final Map REALM_DEFAULTS_BIOQUANT_CLUSTER = REALM_DEFAULTS + [
         name: LABEL_BIOQUANT,
         cluster: Realm.Cluster.BIOQUANT,
@@ -38,6 +56,7 @@ class DomainFactory {
         pbsOptions: '{"-l": {nodes: "1:xeon", walltime: "5:00"}, "-W": {x: "NACCESSPOLICY:SINGLEJOB"}}',
     ]
 
+    /** Default settings for the DKFZ cluster. These include the {@link #REALM_DEFAULTS}. */
     public static final Map REALM_DEFAULTS_DKFZ_CLUSTER = REALM_DEFAULTS + [
         name: LABEL_DKFZ,
         cluster: Realm.Cluster.DKFZ,
@@ -47,12 +66,24 @@ class DomainFactory {
         pbsOptions: '{"-l": {nodes: "1:lsdf", walltime: "5:00"}}',
     ]
 
+    /**
+     * Create a data management {@link Realm} for BioQuant with default cluster settings.
+     * A map with additional values can be passed to add to or overwrite existing domain properties.
+     *
+     * @return a new data management {@link Realm} for the BioQuant
+     */
     public static Realm createRealmDataManagementBioQuant(Map myProps = [:]) {
         new Realm(REALM_DEFAULTS_BIOQUANT_CLUSTER + [
             operationType: Realm.OperationType.DATA_MANAGEMENT,
         ] + myProps)
     }
 
+    /**
+     * Create a data processing {@link Realm} for BioQuant with default cluster settings for the DKFZ cluster.
+     * A map with additional values can be passed to add to or overwrite existing domain properties.
+     *
+     * @return a new data processing {@link Realm} for the BioQuant, with settings for the DKFZ cluster
+     */
     public static Realm createRealmDataProcessingBioQuant(Map myProps = [:]) {
         // NOTE: Data processing for BQ projects is done on DKFZ cluster, so the name needs to
         //       to be adjusted.
@@ -62,12 +93,24 @@ class DomainFactory {
         ] + myProps)
     }
 
+    /**
+     * Create a data management {@link Realm} for DKFZ with default cluster settings.
+     * A map with additional values can be passed to add to or overwrite existing domain properties.
+     *
+     * @return a new data management {@link Realm} for the DKFZ
+     */
     public static Realm createRealmDataManagementDKFZ(Map myProps = [:]) {
         new Realm(REALM_DEFAULTS_DKFZ_CLUSTER + [
             operationType: Realm.OperationType.DATA_MANAGEMENT,
         ] + myProps)
     }
 
+    /**
+     * Create a data processing {@link Realm} for DKFZ with default cluster settings.
+     * A map with additional values can be passed to add to or overwrite existing domain properties.
+     *
+     * @return a new data processing {@link Realm} for the DKFZ
+     */
     public static Realm createRealmDataProcessingDKFZ(Map myProps = [:]) {
         new Realm(REALM_DEFAULTS_DKFZ_CLUSTER + [
             operationType: Realm.OperationType.DATA_PROCESSING,
