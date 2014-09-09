625ea62d5eba0791a0e3add846ce10aa81737697
Jules Kerssemakers <j.kerssemakers@dkfz-heidelberg.de>
2014-09-09 15:24:07 +0200

    clean up useless todo in InvalidStateException

--- src/groovy/de/dkfz/tbi/otp/job/processing/InvalidStateException.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/processing/InvalidStateException.groovy
@@ -8,18 +8,17 @@ public class InvalidStateException extends Exception {
     }
 
     public InvalidStateException(String message) {
-        super(message);
+        super(message)
         // TODO Auto-generated constructor stub
     }
 
     public InvalidStateException(Throwable cause) {
-        super(cause);
+        super(cause)
         // TODO Auto-generated constructor stub
     }
 
     public InvalidStateException(String message, Throwable cause) {
-        super(message, cause);
+        super(message, cause)
         // TODO Auto-generated constructor stub
     }
-
 }
