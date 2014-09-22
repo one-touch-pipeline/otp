c45416fce22d08e7946213749f669afc2b59ace7
Manuel Prinz <m.prinz@dkfz-heidelberg.de>
2014-09-22 16:50:14 +0200

    Update the number of merged lanes after successful merging

--- src/groovy/de/dkfz/tbi/otp/job/jobs/merging/MergingCompleteJob.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/merging/MergingCompleteJob.groovy
@@ -4,15 +4,24 @@ import de.dkfz.tbi.otp.dataprocessing.*
 import de.dkfz.tbi.otp.job.processing.*
 import org.springframework.beans.factory.annotation.Autowired
 
+import static de.dkfz.tbi.otp.utils.CollectionUtils.exactlyOneElement
+
 class MergingCompleteJob extends AbstractEndStateAwareJobImpl {
 
     @Autowired
     MergingPassService mergingPassService
 
+    @Autowired
+    ProcessedMergedBamFileService processedMergedBamFileService
+
     @Override
     public void execute() throws Exception {
         long mergingPassId = Long.parseLong(getProcessParameterValue())
         MergingPass mergingPass = MergingPass.get(mergingPassId)
+        ProcessedMergedBamFile processedMergedBamFile = exactlyOneElement(ProcessedMergedBamFile.findByMergingPass(mergingPass))
+        // Update the property "numberOfMergedLanes" in the BAM file
+        processedMergedBamFileService.updateNumberOfMergedLanes(processedMergedBamFile)
+        // Set state for the next steps
         mergingPassService.mergingPassFinishedAndStartQA(mergingPass)
         succeed()
     }
