5ab40dfc4125c9631386ce4acc5dc021b61acdb7
Eva Reisinger <e.reisinger@dkfz-heidelberg.de>
2014-11-25 15:59:18 +0100

    Revert "OTP-1173: fix missing instanceName + write test"
    This reverts commit 85d966ca749d598acc40e2a3e98cfcd40edc3ccb.

--- src/groovy/de/dkfz/tbi/otp/job/jobs/snvcalling/SnvCallingStartJob.groovy
+++ src/groovy/de/dkfz/tbi/otp/job/jobs/snvcalling/SnvCallingStartJob.groovy
@@ -1,35 +1,26 @@
 package de.dkfz.tbi.otp.job.jobs.snvcalling
 
-import de.dkfz.tbi.otp.dataprocessing.AbstractBamFile
-import org.joda.time.DateTimeZone
-import org.joda.time.Instant
-import org.joda.time.format.DateTimeFormat
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Scope
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-
 import de.dkfz.tbi.otp.dataprocessing.ProcessedMergedBamFile
-import de.dkfz.tbi.otp.dataprocessing.ProcessingOptionService
 import de.dkfz.tbi.otp.dataprocessing.snvcalling.*
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
 import de.dkfz.tbi.otp.job.processing.AbstractStartJobImpl
-import de.dkfz.tbi.otp.job.processing.Process
 import de.dkfz.tbi.otp.job.processing.ProcessParameter
 
 @Component("snvStartJob")
 @Scope("singleton")
 class SnvCallingStartJob extends AbstractStartJobImpl {
 
-    @Autowired
-    ProcessingOptionService optionService
 
     @Autowired
     SnvCallingService snvCallingService
 
     final int MAX_RUNNING = 4
 
-    @Scheduled(fixedDelay = 60000l)
+    @Scheduled(fixedRate = 60000l)
     void execute() {
         if (!hasFreeSlot()) {
             return
@@ -52,11 +43,9 @@ class SnvCallingStartJob extends AbstractStartJobImpl {
 
                 SnvCallingInstance snvCallingInstance = new SnvCallingInstance(
                         sampleTypeCombination: sampleTypeCombinationPerIndividual,
-                        instanceName: DateTimeFormat.forPattern("yyyy-MM-dd_HH'h'mm_Z").withZone(DateTimeZone.getDefault()).print(Instant.now()),
                         config: config,
                         sampleType1BamFile: sampleType1BamFile,
-                        sampleType2BamFile: sampleType2BamFile,
-                        latestDataFileCreationDate: AbstractBamFile.getLatestSequenceDataFileCreationDate(sampleType1BamFile, sampleType2BamFile),
+                        sampleType2BamFile: sampleType2BamFile
                         )
                 snvCallingInstance.save(flush: true)
 
--- test/integration/de/dkfz/tbi/otp/job/jobs/snvcalling/SamplePairDiscoveryStartJobTests.groovy
+++ test/integration/de/dkfz/tbi/otp/job/jobs/snvcalling/SamplePairDiscoveryStartJobTests.groovy
@@ -6,7 +6,6 @@ import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
-import de.dkfz.tbi.otp.job.jobs.TestJobHelper
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
 import de.dkfz.tbi.otp.job.processing.Process
 import de.dkfz.tbi.otp.testing.GroovyScriptAwareIntegrationTest
@@ -16,28 +15,35 @@ class SamplePairDiscoveryStartJobTests extends GroovyScriptAwareIntegrationTest 
     @Autowired
     SamplePairDiscoveryStartJob samplePairDiscoveryStartJob
 
-    private static String PLAN_NAME = 'SamplePairDiscoveryWorkflow'
-
     @Test
     void testExecute() {
         createUserAndRoles()
         SpringSecurityUtils.doWithAuth('admin') {
             run('scripts/workflows/SamplePairDiscoveryWorkflow.groovy')
         }
-        samplePairDiscoveryStartJob.jobExecutionPlan = TestJobHelper.findJobExecutionPlan(PLAN_NAME)
-        assert TestJobHelper.findProcessesForPlanName(PLAN_NAME).size() == 0
+        samplePairDiscoveryStartJob.jobExecutionPlan = findJobExecutionPlan()
+        assert findProcesses().size() == 0
 
         samplePairDiscoveryStartJob.execute()
-        assert TestJobHelper.findProcessesForPlanName(PLAN_NAME).size() == 1
+        assert findProcesses().size() == 1
 
         samplePairDiscoveryStartJob.execute()
-        final Collection<Process> processes = TestJobHelper.findProcessesForPlanName(PLAN_NAME)
+        final Collection<Process> processes = findProcesses()
         assert processes.size() == 1
 
         final Process process = processes.iterator().next()
         process.finished = true
         assert process.save()
         samplePairDiscoveryStartJob.execute()
-        assert TestJobHelper.findProcessesForPlanName(PLAN_NAME).size() == 2
+        assert findProcesses().size() == 2
+
+    }
+
+    private Collection<Process> findProcesses() {
+        return Process.findAllByJobExecutionPlan(findJobExecutionPlan())
+    }
+
+    private JobExecutionPlan findJobExecutionPlan() {
+        return exactlyOneElement(JobExecutionPlan.findAllByName('SamplePairDiscoveryWorkflow'))
     }
 }
