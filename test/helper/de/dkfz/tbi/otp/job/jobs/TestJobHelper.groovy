7013a718217b4e72cdfc0615366a03418a92f81e
Eva Reisinger <e.reisinger@dkfz-heidelberg.de>
2014-11-26 09:58:05 +0100

    OTP-1173: set instancNname for SnvCallingStartJob + integration test for SnvCallingStartJob.execute()

--- test/helper/de/dkfz/tbi/otp/job/jobs/TestJobHelper.groovy
+++ test/helper/de/dkfz/tbi/otp/job/jobs/TestJobHelper.groovy
@@ -1,12 +1,12 @@
 package de.dkfz.tbi.otp.job.jobs
 
-import de.dkfz.tbi.otp.job.jobs.TestJob
 import de.dkfz.tbi.otp.job.processing.*
-import de.dkfz.tbi.otp.ngsdata.DomainFactory
+import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
 import static de.dkfz.tbi.otp.ngsdata.DomainFactory.*
+import static de.dkfz.tbi.otp.utils.CollectionUtils.exactlyOneElement
 
 /**
- * Helper class which returns a TestJob with a processingStep
+ * Helper class which returns job system related database objects
  *
  */
 class TestJobHelper {
@@ -18,5 +18,13 @@ class TestJobHelper {
         }
         TestJob testJob = new TestJob(processingStep, [])
         return testJob
+    }
+
+    public static Collection<Process> findProcessesForPlanName(String planName) {
+        return Process.findAllByJobExecutionPlan(findJobExecutionPlan(planName))
+    }
+
+    public static JobExecutionPlan findJobExecutionPlan(String planName) {
+        return exactlyOneElement(JobExecutionPlan.findAllByName(planName))
     }
 }
--- test/integration/de/dkfz/tbi/otp/job/jobs/snvcalling/SamplePairDiscoveryStartJobTests.groovy
+++ test/integration/de/dkfz/tbi/otp/job/jobs/snvcalling/SamplePairDiscoveryStartJobTests.groovy
@@ -1,12 +1,10 @@
 package de.dkfz.tbi.otp.job.jobs.snvcalling
 
-import static de.dkfz.tbi.otp.utils.CollectionUtils.exactlyOneElement
-
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
 
-import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
+import de.dkfz.tbi.otp.job.jobs.TestJobHelper
 import de.dkfz.tbi.otp.job.processing.Process
 import de.dkfz.tbi.otp.testing.GroovyScriptAwareIntegrationTest
 
@@ -15,35 +13,28 @@ class SamplePairDiscoveryStartJobTests extends GroovyScriptAwareIntegrationTest 
     @Autowired
     SamplePairDiscoveryStartJob samplePairDiscoveryStartJob
 
+    private static String PLAN_NAME = 'SamplePairDiscoveryWorkflow'
+
     @Test
     void testExecute() {
         createUserAndRoles()
         SpringSecurityUtils.doWithAuth('admin') {
             run('scripts/workflows/SamplePairDiscoveryWorkflow.groovy')
         }
-        samplePairDiscoveryStartJob.jobExecutionPlan = findJobExecutionPlan()
-        assert findProcesses().size() == 0
+        samplePairDiscoveryStartJob.jobExecutionPlan = TestJobHelper.findJobExecutionPlan(PLAN_NAME)
+        assert TestJobHelper.findProcessesForPlanName(PLAN_NAME).size() == 0
 
         samplePairDiscoveryStartJob.execute()
-        assert findProcesses().size() == 1
+        assert TestJobHelper.findProcessesForPlanName(PLAN_NAME).size() == 1
 
         samplePairDiscoveryStartJob.execute()
-        final Collection<Process> processes = findProcesses()
+        final Collection<Process> processes = TestJobHelper.findProcessesForPlanName(PLAN_NAME)
         assert processes.size() == 1
 
         final Process process = processes.iterator().next()
         process.finished = true
         assert process.save()
         samplePairDiscoveryStartJob.execute()
-        assert findProcesses().size() == 2
-
-    }
-
-    private Collection<Process> findProcesses() {
-        return Process.findAllByJobExecutionPlan(findJobExecutionPlan())
-    }
-
-    private JobExecutionPlan findJobExecutionPlan() {
-        return exactlyOneElement(JobExecutionPlan.findAllByName('SamplePairDiscoveryWorkflow'))
+        assert TestJobHelper.findProcessesForPlanName(PLAN_NAME).size() == 2
     }
 }
