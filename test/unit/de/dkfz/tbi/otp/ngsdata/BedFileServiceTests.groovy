d52d228ae64c5e3534b8c830dee729ed065147b3
Manuel Prinz <m.prinz@dkfz-heidelberg.de>
2015-01-29 16:12:10 +0100

    Refactored BedFileServiceTests

--- test/unit/de/dkfz/tbi/otp/ngsdata/BedFileServiceTests.groovy
+++ test/unit/de/dkfz/tbi/otp/ngsdata/BedFileServiceTests.groovy
@@ -1,95 +1,47 @@
 package de.dkfz.tbi.otp.ngsdata
 
-import grails.test.mixin.*
-import grails.test.mixin.support.GrailsUnitTestMixin
-import org.junit.*
+import grails.test.mixin.TestFor
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
 
 @TestFor(BedFileService)
-@TestMixin(GrailsUnitTestMixin)
-@Mock([Realm, ReferenceGenome, ExomeEnrichmentKit, BedFile])
 class BedFileServiceTests {
 
-    Realm realm
-    File directory
-    File bedFileFs
-    BedFile bedFileDom
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder()
 
-    BedFileService bedFileService
+    def fakeRealm = [] as Realm
+    def fakeBedFileDomain = [fileName: 'bedFileName'] as BedFile
+    def fakeReferenceGenomeService = [
+            filePathToDirectory: { realm, refGenome -> "${temporaryFolder.root}/referenceGenome" }
+    ] as ReferenceGenomeService
 
-    private final static String referenceGenomePath = "/tmp/reference_genomes/referenceGenome/"
-    private final static String target_regions_dir = "targetRegions"
-
-    @Before
-    void setUp() {
-        ReferenceGenomeService referenceGenomeService = new ReferenceGenomeService()
-        referenceGenomeService.configService = new ConfigService()
-        bedFileService = new BedFileService()
-        bedFileService.referenceGenomeService = referenceGenomeService
-
-        directory = new File("${referenceGenomePath}/${target_regions_dir}")
-        if (!directory.exists()) {
-            directory.mkdirs()
-        }
-
-        bedFileFs = new File("${referenceGenomePath}/${target_regions_dir}/bedFileName")
-        if (!bedFileFs.exists()) {
-            bedFileFs.createNewFile()
-            bedFileFs << "test"
-        }
-
-        realm = DomainFactory.createRealmDataProcessingDKFZ([
-            processingRootPath: 'tmp',
-            ])
-
-        ReferenceGenome referenceGenome = new ReferenceGenome()
-        referenceGenome.name = "hg19_1_24"
-        referenceGenome.path = "referenceGenome"
-        referenceGenome.fileNamePrefix = "prefixName"
-        referenceGenome.save(flush: true)
-
-        ExomeEnrichmentKit kit = new ExomeEnrichmentKit(
-                name: "kitName1"
-                )
-        kit.save(flush: true)
-
-        bedFileDom = new BedFile(
-                fileName: "bedFileName",
-                targetSize: 111,
-                referenceGenome: referenceGenome,
-                exomeEnrichmentKit: kit
-                )
-        bedFileDom.save(flush: true)
+    void test_filePath_WhenProjectIsNull_ShouldFailWithException() {
+        shouldFail(IllegalArgumentException) { service.filePath(null, fakeBedFileDomain) }
     }
 
-    @After
-    void tearDown() {
-        realm = null
-        bedFileDom = null
-        directory.deleteOnExit()
-        bedFileFs.deleteOnExit()
-        bedFileService = null
+    void test_filePath_WhenBedFileIsNull_ShouldFailWithException() {
+        shouldFail(IllegalArgumentException) { service.filePath(fakeRealm, null) }
     }
 
-    @Test(expected = IllegalArgumentException)
-    void testNullProject() {
-        bedFileService.filePath(null, bedFileDom)
+    void test_filePath_WhenBedFileDoesNotExist_ShouldFailWithException() {
+        service.referenceGenomeService = fakeReferenceGenomeService
+
+        assert shouldFail(RuntimeException) {
+            service.filePath(fakeRealm, fakeBedFileDomain)
+        } =~ /the bedFile can not be read/
     }
 
-    @Test(expected = IllegalArgumentException)
-    void testNullBedFile() {
-        bedFileService.filePath(realm, null)
-    }
+    void test_filePath_WhenBedFileExists_ShouldReturnPathToFile() {
+        // setup:
+        File referenceGenomesBaseDirectory = temporaryFolder.newFolder("referenceGenome/targetRegions")
+        referenceGenomesBaseDirectory.mkdirs()
 
-    @Test(expected = RuntimeException)
-    void testFileCanNotRead() {
-        assertTrue bedFileFs.delete()
-        bedFileService.filePath(realm, bedFileDom)
-    }
+        new File(referenceGenomesBaseDirectory, 'bedFileName').createNewFile()
 
-    @Test
-    void testCorrectCase() {
-        String path = bedFileService.filePath(realm, bedFileDom)
-        String expectedPath = "${referenceGenomePath}/${target_regions_dir}/bedFileName"
-        assertEquals(path, expectedPath)
+        service.referenceGenomeService = fakeReferenceGenomeService
+
+        // expect:
+        assert service.filePath(fakeRealm, fakeBedFileDomain) == "${temporaryFolder.root.path}/referenceGenome/targetRegions/bedFileName"
     }
 }
