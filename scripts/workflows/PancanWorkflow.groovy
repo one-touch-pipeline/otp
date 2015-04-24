db044a9bde2a033faefc35c7154ea6987c890853
Amal Mertens <a.mertens@dkfz-heidelberg.de>
2015-04-24 11:06:51 +0200

    OTP-1444: Pancancer Roddy alignment workflow

---
+++ scripts/workflows/PancanWorkflow.groovy
@@ -0,0 +1,14 @@
+import static de.dkfz.tbi.otp.utils.JobExecutionPlanDSL.*
+import de.dkfz.tbi.otp.job.processing.AbstractStartJobImpl;
+
+String workflow = "PanCanWorkflow"
+
+plan(workflow, ctx, true) {
+    start("PanCanStart", "PanCanStartJob")
+    job("executePanCan", "executePanCanJob")
+    job("parsePanCanQC", "parsePanCanQCJob")
+    job("movePanCanFilesToFinalDestination", "movePanCanFilesToFinalDestinationJob")
+}
+
+println ctx.processingOptionService.createOrUpdate(AbstractStartJobImpl.TOTAL_SLOTS_OPTION_NAME, workflow, null, '5', '')
+println ctx.processingOptionService.createOrUpdate(AbstractStartJobImpl.SLOTS_RESERVED_FOR_FAST_TRACK_OPTION_NAME, workflow, null, '0', '')
