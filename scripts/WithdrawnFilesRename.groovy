c3c9e812509b1a88de00eaeaf0fb5529df104a09
Andreas Kling <a.kling@dkfz-heidelberg.de>
2015-09-04 15:48:40 +0200

    OTP-1187: rename existing withdrawn result files
    in the project folder
    
    Review #1586

---
+++ scripts/WithdrawnFilesRename.groovy
@@ -0,0 +1,54 @@
+import de.dkfz.tbi.otp.dataprocessing.*
+import de.dkfz.tbi.otp.dataprocessing.snvcalling.SnvJobResult
+
+
+AbstractMergedBamFileService abstractMergedBamFileService = ctx.abstractMergedBamFileService
+
+/**
+ * rename existing withdrawn result files in the project folder
+ *
+ *
+ * Withdrawn result files (ProcessedMergedBamFiles, RoddyBamFiles)
+ * in the project folder are renamed by appending "-withdrawn",
+ * for SNV results complete directories are renamed.
+ *
+ */
+
+
+List<File> renameFiles = []
+
+MergingWorkPackage.list().each { MergingWorkPackage mergingWorkPackage ->
+    AbstractMergedBamFile bamFile = mergingWorkPackage.bamFileInProjectFolder
+    if (bamFile && bamFile.withdrawn) {
+        final File file = new File(abstractMergedBamFileService.destinationDirectory(bamFile), bamFile.bamFileName)
+        renameFiles.add(file)
+
+        List<SnvJobResult> snvJobResult = findSnvJobResultForBamFile(bamFile)
+        snvJobResult.each { SnvJobResult result ->
+            assert result.withdrawn
+            // rename folder containing results
+            renameFiles.add(result.snvCallingInstance.snvInstancePath.absoluteDataManagementPath)
+        }
+    }
+}
+
+new File("$SCRIPT_ROOT_PATH/withdraw/renameWithdrawnFiles.sh").withPrintWriter { writer ->
+    writer.write("#!/bin/bash\n")
+
+    renameFiles.each {
+        writer.write("mv ${it} ${it}-withdrawn\n")
+    }
+}
+
+
+
+List<SnvJobResult> findSnvJobResultForBamFile(AbstractMergedBamFile bamFile) {
+    return SnvJobResult.createCriteria().list {
+        snvCallingInstance {
+            or {
+                eq('sampleType1BamFile', bamFile)
+                eq('sampleType2BamFile', bamFile)
+            }
+        }
+    }
+}
