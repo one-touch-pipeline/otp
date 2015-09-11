768550c6342d21ad76e5912da1138a51e1fd9914
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2015-09-11 12:46:27 +0200

    Bugfix in script "RetriggerSnv": set logthread variable
    During making jobs withdrawn some comments are writen to logthread.
    Therefore the logthread needs to be set.

--- scripts/operations/workflowTriggering/snv/RetriggerSnv.groovy
+++ scripts/operations/workflowTriggering/snv/RetriggerSnv.groovy
@@ -1,11 +1,12 @@
 import de.dkfz.tbi.otp.dataprocessing.snvcalling.*
+import de.dkfz.tbi.otp.utils.logging.LogThreadLocal
 
 println "\n\n retrigger snv: "
 def snvCallingInstances = SnvCallingInstance.withCriteria {
     'in'("id", [
             // ids of SnvCallingInstances
 
-    ] as long[] )
+    ] as long[])
 }
 
 //show all
@@ -13,17 +14,19 @@ snvCallingInstances.each { println "${it}" }
 println snvCallingInstances.size()
 
 /*
-SamplePair.withTransaction {
-    snvCallingInstances.each {
-        if (it.processingState == SnvProcessingStates.IN_PROGRESS) {
-            SnvJobResult.findAllBySnvCallingInstance(it)*.makeWithdrawn()
-            it.processingState = SnvProcessingStates.FAILED
-            it.save(flush: true, failOnError: true)
-        }
+LogThreadLocal.withThreadLog(System.out, {
+    SamplePair.withTransaction {
+        snvCallingInstances.each {
+            if (it.processingState == SnvProcessingStates.IN_PROGRESS) {
+                SnvJobResult.findAllBySnvCallingInstance(it)*.makeWithdrawn()
+                it.processingState = SnvProcessingStates.FAILED
+                it.save(flush: true, failOnError: true)
+            }
 
-        it.samplePair.processingStatus = SamplePair.ProcessingStatus.NEEDS_PROCESSING
-        println it.samplePair.save(flush: true, failOnError: true)
+            it.samplePair.processingStatus = SamplePair.ProcessingStatus.NEEDS_PROCESSING
+            println it.samplePair.save(flush: true, failOnError: true)
+        }
     }
-}
+})
 println snvCallingInstances.size()
 // */
