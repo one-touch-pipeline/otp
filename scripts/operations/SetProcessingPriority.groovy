e798b1d926be7722894fec6e1ea40c9f0f9fe415
Jan Philip Matuschek <j.matuschek@dkfz-heidelberg.de>
2015-09-14 13:32:03 +0200

    Script for setting the processing priority of projects
    Review #1627

---
+++ scripts/operations/SetProcessingPriority.groovy
@@ -0,0 +1,22 @@
+import static de.dkfz.tbi.otp.utils.CollectionUtils.*
+
+import de.dkfz.tbi.otp.dataprocessing.ProcessingPriority
+import de.dkfz.tbi.otp.ngsdata.Project
+
+Collection<String> projectNames =  // ['']
+short newProcessingPriority =  // ProcessingPriority.
+
+Project.withTransaction {
+    projectNames.each {
+        Project project
+        try {
+            project = exactlyOneElement(Project.findAllByName(it))
+        } catch (Throwable t) {
+            throw new RuntimeException("Problem finding project ${it}", t)
+        }
+        println "Changing processing priority of ${project} from ${project.processingPriority} to ${newProcessingPriority}"
+        project.processingPriority = newProcessingPriority
+        project.save(failOnError: true, flush: true)
+    }
+}
+''
