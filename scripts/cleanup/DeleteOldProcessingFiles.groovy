35d8d24bc124530e4b7f5c5557a12630fe4ec7f1
Jan Philip Matuschek <j.matuschek@dkfz-heidelberg.de>
2015-10-08 11:18:12 +0200

    Improve timeout logic and flush log file in DeleteOldProcessingFiles.groovy
    Review #1659

--- scripts/cleanup/DeleteOldProcessingFiles.groovy
+++ scripts/cleanup/DeleteOldProcessingFiles.groovy
@@ -1,18 +1,39 @@
 import de.dkfz.tbi.otp.utils.logging.LogThreadLocal
 import org.joda.time.*
 
-PrintWriter out = new PrintWriter(new FileOutputStream(
-    "$SCRIPT_ROOT_PATH/script_output/cleanup/DeleteOldProcessingFiles_" + new Date().format("yyyy-MM-dd_HH.mm.ss.SSS_Z") + ".log"), true)
+/**
+ * Only files created before this instant will be deleted.
+ */
+final ReadableInstant minCreationInstant = new Interval(Period.weeks(8), Instant.now()).start
 
-LogThreadLocal.withThreadLog(out, {
-    final ReadableInstant someTimeAgo = new Interval(Period.weeks(8), Instant.now()).start
+/**
+ * Roughly the maximum allowed runtime for this script.
+ */
+final ReadableDuration maxRuntime = Duration.standardHours(1)
+
+
+try {
+    PrintWriter out = new PrintWriter(
+        "$SCRIPT_ROOT_PATH/script_output/cleanup/DeleteOldProcessingFiles_" + new Date().format("yyyy-MM-dd_HH.mm.ss.SSS_Z") + ".log")
     try {
+        final Instant startTime = Instant.now()
         long freedBytes = 0L
-        freedBytes += ctx.processedAlignmentFileService.deleteOldAlignmentProcessingFiles(someTimeAgo.toDate(), Duration.standardMinutes(60).millis)
-        freedBytes += ctx.mergingPassService.deleteOldMergingProcessingFiles(someTimeAgo.toDate(), Duration.standardMinutes(60).millis)
-        log.info "${freedBytes} bytes have been freed in total."
+        LogThreadLocal.withThreadLog(out, {
+            freedBytes += ctx.processedAlignmentFileService.deleteOldAlignmentProcessingFiles(minCreationInstant.toDate(), maxRuntime.millis)
+            final Duration maxRemainingRuntime = new Duration(Instant.now(), startTime.plus(maxRuntime))
+            if (maxRemainingRuntime > Duration.ZERO) {
+                freedBytes += ctx.mergingPassService.deleteOldMergingProcessingFiles(minCreationInstant.toDate(), maxRemainingRuntime.millis)
+            }
+            threadLog.info "${freedBytes} bytes have been freed in total."
+        })
+        assert !out.checkError()
     } catch (final Throwable e) {
         e.printStackTrace(out)
-        e.printStackTrace(System.out)
+        throw e
+    } finally {
+        out.flush()
+        out.close()
     }
-})
+} catch (final Throwable e) {
+    e.printStackTrace(System.out)
+}
