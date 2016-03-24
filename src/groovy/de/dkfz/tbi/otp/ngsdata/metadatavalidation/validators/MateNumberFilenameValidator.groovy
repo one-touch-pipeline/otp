5ecb16f90664ed684444b2917ad62fe8f5b8ada2
Philipp Strubel <p.strubel@dkfz-heidelberg.de>
2016-03-24 09:47:57 +0100

    OTP-1941: Validate consistency of mate number and filename

---
+++ src/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/MateNumberFilenameValidator.groovy
@@ -0,0 +1,81 @@
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import de.dkfz.tbi.otp.ngsdata.LibraryLayout
+import de.dkfz.tbi.otp.ngsdata.MetaDataService
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidationContext
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidator
+import de.dkfz.tbi.util.spreadsheet.validation.Level
+import de.dkfz.tbi.util.spreadsheet.validation.ValueTuple
+import de.dkfz.tbi.util.spreadsheet.validation.ValueTuplesValidator
+import org.springframework.stereotype.Component
+
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.*
+
+@Component
+class MateNumberFilenameValidator extends ValueTuplesValidator<MetadataValidationContext> implements MetadataValidator {
+
+    @Override
+    Collection<String> getDescriptions() {
+        return ["If a mate number can be parsed from the filename, it is consistent with the entry in the '${MATE}' column."]
+    }
+
+    @Override
+    List<String> getColumnTitles(MetadataValidationContext context) {
+        return [FASTQ_FILE.name(), LIBRARY_LAYOUT.name(), MATE.name()]
+    }
+
+    @Override
+    boolean columnMissing(MetadataValidationContext context, String columnTitle) {
+        if (columnTitle == FASTQ_FILE.name()) {
+            mandatoryColumnMissing(context, columnTitle)
+            return false
+        } else {
+            return true
+        }
+    }
+
+    @Override
+    void validateValueTuples(MetadataValidationContext context, Collection<ValueTuple> valueTuples) {
+        Integer fastqFileColumnIndex = context.spreadsheet.header.getCellByColumnTitle(FASTQ_FILE.name()).columnIndex
+        Integer libraryLayoutColumnIndex = context.spreadsheet.header.getCellByColumnTitle(LIBRARY_LAYOUT.name())?.columnIndex
+        Integer mateColumnIndex = context.spreadsheet.header.getCellByColumnTitle(MATE.name())?.columnIndex
+        valueTuples.each { ValueTuple valueTuple ->
+            String libraryLayout = valueTuple.getValue(LIBRARY_LAYOUT.name())
+            String mateNumber = valueTuple.getValue(MATE.name())
+            String fileName = valueTuple.getValue(FASTQ_FILE.name())
+            Integer mateCount = LibraryLayout.values().find { it.name() == libraryLayout }?.mateCount
+            try {
+                int fileNameMateNumber = MetaDataService.findOutMateNumber(fileName)
+                if (mateCount == 1) {
+                    if (!(fileNameMateNumber == 1)) {
+                        context.addProblem(valueTuple.cells.findAll({
+                            it.columnIndex == fastqFileColumnIndex ||
+                                    it.columnIndex == libraryLayoutColumnIndex
+                        }), Level.WARNING, "The mate number '${fileNameMateNumber}' parsed from filename '${fileName}' is not viable with library layout '${libraryLayout}'." +
+                                " If you ignore this warning, OTP will ignore the mate number parsed from the filename.")
+                    }
+                } else {
+                    if (mateNumber != null && fileNameMateNumber.toString() != mateNumber) {
+                        context.addProblem(valueTuple.cells.findAll({
+                            it.columnIndex == fastqFileColumnIndex ||
+                            it.columnIndex == mateColumnIndex
+                        }), Level.WARNING, "The value '${mateNumber}' in the MATE column is different from the mate number '${fileNameMateNumber}' parsed from the filename '${fileName}'. " +
+                                "If you ignore this warning, OTP will use the mate number parsed from the filename and ignore the value in the MATE column.")
+
+                    }
+                }
+            } catch (RuntimeException e) {
+                if (libraryLayout != null && mateCount != 1) {
+                    if (e.message == "cannot find mateNumber for ${fileName}".toString()) {
+                        context.addProblem(valueTuple.cells.findAll({
+                            it.columnIndex == fastqFileColumnIndex ||
+                            it.columnIndex == libraryLayoutColumnIndex
+                        }), Level.ERROR, "Cannot parse mate number from filename '${fileName}' or imply from the library layout '${libraryLayout}'.")
+                    } else {
+                        throw e
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/unit/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/MateNumberFilenameValidatorSpec.groovy
@@ -0,0 +1,132 @@
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import de.dkfz.tbi.otp.ngsdata.LibraryLayout
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidationContext
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidationContextFactory
+import de.dkfz.tbi.util.spreadsheet.validation.Level
+import de.dkfz.tbi.util.spreadsheet.validation.Problem
+import spock.lang.Specification
+
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.*
+import static de.dkfz.tbi.otp.utils.CollectionUtils.containSame
+
+class MateNumberFilenameValidatorSpec extends Specification {
+
+    void 'validate, when column FASTQ_FILE missing, then add expected problem'() {
+        given:
+
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext()
+        Collection<Problem> expectedProblems = [
+                new Problem(Collections.emptySet(), Level.ERROR,
+                        "Mandatory column 'FASTQ_FILE' is missing.")
+        ]
+        when:
+        new MateNumberFilenameValidator().validate(context)
+
+        then:
+        containSame(context.problems, expectedProblems)
+    }
+
+    void 'validate, when columns FASTQ_FILE and LIBRARY_LAYOUT, adds expected problems'() {
+        given:
+
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${FASTQ_FILE}\t${LIBRARY_LAYOUT}\n" +
+                        "NB_E_789_R.1.fastq.gz\t${LibraryLayout.SINGLE}\n" +
+                        "NB_E_789_R.2.fastq.gz\t${LibraryLayout.SINGLE}\n" +
+                        "testFileName.fastq.gz\t${LibraryLayout.SINGLE}\n" +
+                        "NB_E_789_R.1.fastq.gz\ttestLibraryLayout\n" +
+                        "testFileName.fastq.gz\ttestLibraryLayout\n"
+        )
+        Collection<Problem> expectedProblems = [
+                new Problem(context.spreadsheet.dataRows[1].cells as Set, Level.WARNING,
+                        "The mate number '2' parsed from filename 'NB_E_789_R.2.fastq.gz' is not viable with library layout 'SINGLE'." +
+                                " If you ignore this warning, OTP will ignore the mate number parsed from the filename."),
+                new Problem(context.spreadsheet.dataRows[4].cells as Set, Level.ERROR,
+                        "Cannot parse mate number from filename 'testFileName.fastq.gz' or imply from the library layout 'testLibraryLayout'."),
+        ]
+        when:
+        new MateNumberFilenameValidator().validate(context)
+
+        then:
+        containSame(context.problems, expectedProblems)
+    }
+
+    void 'validate, when columns FASTQ_FILE and MATE, adds expected problems'() {
+        given:
+
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${FASTQ_FILE}\t${MATE}\n" +
+                        "NB_E_789_R.1.fastq.gz\t1\n" +
+                        "NB_E_789_R.1.fastq.gz\t2\n" +
+                        "NB_E_789_R.1.fastq.gz\tabc\n" +
+                        "testFileName.fastq.gz\t1\n" +
+                        "testFileName.fastq.gz\t2\n" +
+                        "testFileName.fastq.gz\tabc\n"
+        )
+        Collection<Problem> expectedProblems = [
+                new Problem(context.spreadsheet.dataRows[1].cells as Set, Level.WARNING,
+                        "The value '2' in the MATE column is different from the mate number '1' parsed from the filename 'NB_E_789_R.1.fastq.gz'. " +
+                                "If you ignore this warning, OTP will use the mate number parsed from the filename and ignore the value in the MATE column."),
+                new Problem(context.spreadsheet.dataRows[2].cells as Set, Level.WARNING,
+                        "The value 'abc' in the MATE column is different from the mate number '1' parsed from the filename 'NB_E_789_R.1.fastq.gz'. " +
+                                "If you ignore this warning, OTP will use the mate number parsed from the filename and ignore the value in the MATE column."),
+        ]
+        when:
+        new MateNumberFilenameValidator().validate(context)
+
+        then:
+        containSame(context.problems, expectedProblems)
+    }
+
+    void 'validate, when columns FASTQ_FILE, LIBRARY_LAYOUT and MATE, adds expected problems'() {
+        given:
+
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${FASTQ_FILE}\t${LIBRARY_LAYOUT}\t${MATE}\n" +
+                        "NB_E_789_R.1.fastq.gz\t${LibraryLayout.SINGLE}\t1\n" +
+                        "NB_E_789_R.1.fastq.gz\t${LibraryLayout.SINGLE}\t2\n" +
+                        "NB_E_789_R.1.fastq.gz\t${LibraryLayout.SINGLE}\tabc\n" +
+                        "NB_E_789_R.2.fastq.gz\t${LibraryLayout.SINGLE}\t1\n" +
+                        "NB_E_789_R.2.fastq.gz\t${LibraryLayout.SINGLE}\t2\n" +
+                        "NB_E_789_R.2.fastq.gz\t${LibraryLayout.SINGLE}\tabc\n" +
+                        "testFileName.fastq.gz\t${LibraryLayout.SINGLE}\t1\n" +
+                        "testFileName.fastq.gz\t${LibraryLayout.SINGLE}\t2\n" +
+                        "testFileName.fastq.gz\t${LibraryLayout.SINGLE}\tabc\n" +
+                        "NB_E_789_R.1.fastq.gz\ttestLibraryLayout\t1\n" +
+                        "NB_E_789_R.1.fastq.gz\ttestLibraryLayout\t2\n" +
+                        "NB_E_789_R.1.fastq.gz\ttestLibraryLayout\tabc\n" +
+                        "testFileName.fastq.gz\ttestLibraryLayout\t1\n" +
+                        "testFileName.fastq.gz\ttestLibraryLayout\t2\n" +
+                        "testFileName.fastq.gz\ttestLibraryLayout\tabc\n"
+        )
+        Collection<Problem> expectedProblems = [
+                new Problem(context.spreadsheet.dataRows[3].cells.findAll({it.columnAddress=="A" || it.columnAddress=="B"}) as Set, Level.WARNING,
+                        "The mate number '2' parsed from filename 'NB_E_789_R.2.fastq.gz' is not viable with library layout 'SINGLE'." +
+                                " If you ignore this warning, OTP will ignore the mate number parsed from the filename."),
+                new Problem(context.spreadsheet.dataRows[4].cells.findAll({it.columnAddress=="A" || it.columnAddress=="B"}) as Set, Level.WARNING,
+                        "The mate number '2' parsed from filename 'NB_E_789_R.2.fastq.gz' is not viable with library layout 'SINGLE'." +
+                                " If you ignore this warning, OTP will ignore the mate number parsed from the filename."),
+                new Problem(context.spreadsheet.dataRows[5].cells.findAll({it.columnAddress=="A" || it.columnAddress=="B"}) as Set, Level.WARNING,
+                        "The mate number '2' parsed from filename 'NB_E_789_R.2.fastq.gz' is not viable with library layout 'SINGLE'." +
+                                " If you ignore this warning, OTP will ignore the mate number parsed from the filename."),
+                new Problem(context.spreadsheet.dataRows[10].cells.findAll({it.columnAddress=="A" || it.columnAddress=="C"}) as Set, Level.WARNING,
+                        "The value '2' in the MATE column is different from the mate number '1' parsed from the filename 'NB_E_789_R.1.fastq.gz'. " +
+                                "If you ignore this warning, OTP will use the mate number parsed from the filename and ignore the value in the MATE column."),
+                new Problem(context.spreadsheet.dataRows[11].cells.findAll({it.columnAddress=="A" || it.columnAddress=="C"}) as Set, Level.WARNING,
+                        "The value 'abc' in the MATE column is different from the mate number '1' parsed from the filename 'NB_E_789_R.1.fastq.gz'. " +
+                                "If you ignore this warning, OTP will use the mate number parsed from the filename and ignore the value in the MATE column."),
+                new Problem(context.spreadsheet.dataRows[12].cells.findAll({it.columnAddress=="A" || it.columnAddress=="B"}) as Set, Level.ERROR,
+                        "Cannot parse mate number from filename 'testFileName.fastq.gz' or imply from the library layout 'testLibraryLayout'."),
+                new Problem(context.spreadsheet.dataRows[13].cells.findAll({it.columnAddress=="A" || it.columnAddress=="B"}) as Set, Level.ERROR,
+                        "Cannot parse mate number from filename 'testFileName.fastq.gz' or imply from the library layout 'testLibraryLayout'."),
+                new Problem(context.spreadsheet.dataRows[14].cells.findAll({it.columnAddress=="A" || it.columnAddress=="B"}) as Set, Level.ERROR,
+                        "Cannot parse mate number from filename 'testFileName.fastq.gz' or imply from the library layout 'testLibraryLayout'."),
+        ]
+        when:
+        new MateNumberFilenameValidator().validate(context)
+
+        then:
+        containSame(context.problems, expectedProblems)
+    }
+}
