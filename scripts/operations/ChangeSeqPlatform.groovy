69afae7db02dea113e4896b45a45815404ffb5d6
Jan Philip Matuschek <j.matuschek@dkfz-heidelberg.de>
2016-06-09 12:47:05 +0200

    Operator script to change the SeqPlatform of a Run
    Review #2173

---
+++ scripts/operations/ChangeSeqPlatform.groovy
@@ -0,0 +1,39 @@
+/*
+ * Change the SeqPlatform of a Run and its SeqTracks.
+ */
+
+import de.dkfz.tbi.otp.ngsdata.*
+
+import static de.dkfz.tbi.otp.utils.CollectionUtils.*
+
+def runName =         ''
+def currentPlatform = [platform: 'Illumina', model: 'HiSeq ', kit: 'V']
+def newPlatform =     [platform: 'Illumina', model: 'HiSeq ', kit: 'V']
+
+Run.withTransaction {
+    SeqPlatform currentSeqPlatform = ctx.seqPlatformService.findSeqPlatform(
+            currentPlatform.platform, currentPlatform.model, currentPlatform.kit)
+    assert currentSeqPlatform
+
+    SeqPlatform newSeqPlatform = ctx.seqPlatformService.findSeqPlatform(
+            newPlatform.platform, newPlatform.model, newPlatform.kit)
+    assert newSeqPlatform
+
+    assert newSeqPlatform.seqPlatformGroup == currentSeqPlatform.seqPlatformGroup
+
+    Run run = exactlyOneElement(Run.findAllByName(runName))
+    assert run.seqPlatform == currentSeqPlatform
+
+    def seqTracks = SeqTrack.findAllByRun(run)
+    seqTracks.each {
+        assert it.seqPlatform == currentSeqPlatform
+    }
+
+    run.seqPlatform = newSeqPlatform
+    assert run.save(flush: true)
+
+    seqTracks.each {
+        it.seqPlatform = newSeqPlatform
+        assert it.save(flush: true)
+    }
+}
