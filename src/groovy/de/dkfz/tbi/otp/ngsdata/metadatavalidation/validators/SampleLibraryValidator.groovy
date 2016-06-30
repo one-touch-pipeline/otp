87a08f0bbfe9b3a38f8e00142ec2fe730de40263
Laura Vogler <l.vogler@dkfz-heidelberg.de>
2016-06-30 10:13:02 +0200

    OTP-2200: SampleLibraryValidator

---
+++ src/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/SampleLibraryValidator.groovy
@@ -0,0 +1,44 @@
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.*
+import de.dkfz.tbi.util.spreadsheet.validation.*
+import org.springframework.stereotype.*
+
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.*
+
+@Component
+class SampleLibraryValidator extends ValueTuplesValidator<MetadataValidationContext> implements MetadataValidator {
+
+    final String LIB = "lib"
+
+    @Override
+    Collection<String> getDescriptions() {
+        return ["If '${LIB}' is contained in the ${SAMPLE_ID} then the library should be given."]
+    }
+
+    @Override
+    List<String> getColumnTitles(MetadataValidationContext context) {
+        return [SAMPLE_ID.name(), CUSTOMER_LIBRARY.name()]
+    }
+
+    @Override
+    boolean columnMissing(MetadataValidationContext context, String columnTitle) {
+
+        if (columnTitle == SAMPLE_ID.name()) {
+            mandatoryColumnMissing(context, columnTitle)
+            return false
+        } else {
+            return true
+        }
+    }
+
+    @Override
+    void validateValueTuples(MetadataValidationContext context, Collection<ValueTuple> valueTuples) {
+        valueTuples.each { ValueTuple valueTuple ->
+            String sample = valueTuple.getValue(SAMPLE_ID.name())
+            if (sample.toLowerCase(Locale.ENGLISH).contains(LIB) && !valueTuple.getValue(CUSTOMER_LIBRARY.name())) {
+                context.addProblem(valueTuple.cells, Level.WARNING, "For sample '${sample}' which contains 'lib', there should be a value in the ${CUSTOMER_LIBRARY} column.")
+            }
+        }
+    }
+}
---
+++ test/unit/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/SampleLibraryValidatorSpec.groovy
@@ -0,0 +1,102 @@
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.*
+import de.dkfz.tbi.util.spreadsheet.validation.*
+import spock.lang.*
+import static de.dkfz.tbi.TestCase.*
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.*
+import static de.dkfz.tbi.otp.utils.CollectionUtils.*
+
+class SampleLibraryValidatorSpec extends Specification {
+
+    void 'validate, when SAMPLE_ID and CUSTOMER_LIBRARY are missing, add error'() {
+
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext()
+
+        when:
+        new SampleLibraryValidator().validate(context)
+
+        then:
+        Collection<Problem> expectedProblems = [
+                new Problem(Collections.emptySet(), Level.ERROR,
+                        "Mandatory column 'SAMPLE_ID' is missing.")
+        ]
+        assertContainSame(context.problems, expectedProblems)
+    }
+
+    void 'validate, when CUSTOMER_LIBRARY is missing, add warning'() {
+
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${SAMPLE_ID}\n" +
+                "testSample\n" +
+                "testSampleLib\n"
+        )
+
+        when:
+        new SampleLibraryValidator().validate(context)
+
+        then:
+        Collection<Problem> expectedProblems = [
+                new Problem(context.spreadsheet.dataRows[1].cells as Set, Level.WARNING, "For sample 'testSampleLib' which contains 'lib', there should be a value in the ${CUSTOMER_LIBRARY} column.")
+        ]
+        assertContainSame(context.problems, expectedProblems)
+    }
+
+    void 'validate, when SAMPLE_ID is missing, add error'() {
+
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${CUSTOMER_LIBRARY}\n" +
+                        "lib"
+        )
+
+        when:
+        new SampleLibraryValidator().validate(context)
+
+        then:
+        Collection<Problem> expectedProblems = [
+                new Problem(Collections.emptySet(), Level.ERROR,
+                        "Mandatory column 'SAMPLE_ID' is missing.")
+        ]
+        assertContainSame(context.problems, expectedProblems)
+    }
+
+    void 'validate, valid SAMPLE_ID and CUSTOMER_LIBRARY combinations, succeeds'() {
+
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${SAMPLE_ID}\t${CUSTOMER_LIBRARY}\n" +
+                        "testSampleLib\tlib\n" +
+                        "testSample\tlib\n" +
+                        "testSample\n" +
+                        "testLIbSample\tlib\n"
+        )
+
+        when:
+        new SampleLibraryValidator().validate(context)
+
+        then:
+        context.problems.empty
+    }
+
+    void 'validate, when SAMPLE_ID does contain "lib" and CUSTOMER_LIBRARY is empty, adds warning'() {
+
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${SAMPLE_ID}\t${CUSTOMER_LIBRARY}\n" +
+                        "testSampleLib\n"
+        )
+
+        when:
+        new SampleLibraryValidator().validate(context)
+
+        then:
+        Collection<Problem> expectedProblems = [
+                new Problem(context.spreadsheet.dataRows[0].cells as Set, Level.WARNING, "For sample 'testSampleLib' which contains 'lib', there should be a value in the ${CUSTOMER_LIBRARY} column.")
+        ]
+        assertContainSame(context.problems, expectedProblems)
+    }
+
+}
