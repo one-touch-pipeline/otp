44ad2734c135bccc11c2d78c83363e7e92b03e46
Eva Reisinger <e.reisinger@dkfz-heidelberg.de>
2016-08-29 09:26:31 +0200

    OTP-2263: Create Roddy-Snv-Workflow script

---
+++ scripts/workflows/RoddySnvWorkflow.groovy
@@ -0,0 +1,18 @@
+import de.dkfz.tbi.otp.tracking.OtrsTicket
+
+import static de.dkfz.tbi.otp.utils.JobExecutionPlanDSL.*
+import de.dkfz.tbi.otp.job.processing.AbstractStartJobImpl;
+
+String workflow = "RoddySnvWorkflow"
+
+plan(workflow, ctx, true) {
+    start("roddySnvStart", "roddySnvStartJob")
+    job("executeRoddySnv", "executeRoddySnvJob")
+    job("roddySnvCompletion", "roddySnvCompletionJob")
+    job("notifyProcessFinished", "notifyProcessFinishedJob") {
+        constantParameter("step", OtrsTicket.ProcessingStep.SNV.name())
+    }
+}
+
+println ctx.processingOptionService.createOrUpdate(AbstractStartJobImpl.TOTAL_SLOTS_OPTION_NAME, workflow, null, '5', '')
+println ctx.processingOptionService.createOrUpdate(AbstractStartJobImpl.SLOTS_RESERVED_FOR_FAST_TRACK_OPTION_NAME, workflow, null, '0', '')
