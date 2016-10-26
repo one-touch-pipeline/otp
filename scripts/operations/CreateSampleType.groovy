aa2db5ca041b913b2567c4ec084753000fdb46bd
Jan Philip Matuschek <j.matuschek@dkfz-heidelberg.de>
2016-10-26 13:01:42 +0200

    Script for adding a SampleType
    Review #2453

---
+++ scripts/operations/CreateSampleType.groovy
@@ -0,0 +1,7 @@
+import de.dkfz.tbi.otp.ngsdata.*
+
+new SampleType(
+        name: '',
+//        specificReferenceGenome: SampleType.SpecificReferenceGenome.USE_PROJECT_DEFAULT,
+//        specificReferenceGenome: SampleType.SpecificReferenceGenome.USE_SAMPLE_TYPE_SPECIFIC,
+).save(flush: true)
