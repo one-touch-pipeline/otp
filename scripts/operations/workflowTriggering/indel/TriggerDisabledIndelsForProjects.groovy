b7c92b61e17630438ede889a037875b8d654db53
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2017-02-09 15:31:19 +0100

    Operator script to start disabled indels of projects

---
+++ scripts/operations/workflowTriggering/indel/TriggerDisabledIndelsForProjects.groovy
@@ -0,0 +1,37 @@
+import de.dkfz.tbi.otp.dataprocessing.*
+import de.dkfz.tbi.otp.dataprocessing.snvcalling.*
+import de.dkfz.tbi.otp.utils.logging.*
+
+println "\n\ntrigger disabled indel projects: "
+def samplePairs = SamplePair.withCriteria {
+    mergingWorkPackage1 {
+        sample {
+            individual {
+                project {
+                    'in'('name', [
+                            'PROJECT_NAME',
+                            'PROJECT_NAME'
+                    ])
+                }
+            }
+        }
+    }
+    eq('indelProcessingStatus', SamplePair.ProcessingStatus.DISABLED)
+}
+
+
+//show all
+samplePairs.each { println "${it}" }
+println samplePairs.size()
+
+/*
+LogThreadLocal.withThreadLog(System.out, {
+    SamplePair.withTransaction {
+        samplePairs.each {
+            it.indelProcessingStatus = SamplePair.ProcessingStatus.NEEDS_PROCESSING
+            println it.save(flush: true, failOnError: true)
+        }
+    }
+})
+println samplePairs.size()
+// */
