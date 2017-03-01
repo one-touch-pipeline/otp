ac97a0b5fdaeadd8f8e498835726f80fe0723b8d
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2017-03-01 09:42:55 +0100

    Bugfix and improve TriggerAlignmentForPids
    - use left outer join for ilse
    - allow to use also project name

--- scripts/operations/workflowTriggering/alignment/TriggerAlignmentForPids.groovy
+++ scripts/operations/workflowTriggering/alignment/TriggerAlignmentForPids.groovy
@@ -2,8 +2,10 @@
  * script to trigger alignments for a patient.
  * set the align flag for all runsegments used by seqtracks of the given individuals.
  */
+
 import de.dkfz.tbi.otp.ngsdata.*
-import de.dkfz.tbi.otp.utils.logging.LogThreadLocal
+import de.dkfz.tbi.otp.utils.logging.*
+import org.hibernate.sql.*
 
 LogThreadLocal.withThreadLog(System.out, {
     SeqTrack.withTransaction {
@@ -12,31 +14,33 @@ LogThreadLocal.withThreadLog(System.out, {
             or {
                 sample {
                     individual {
-                        'in'('mockPid', [
-                                '',
-                                // pids of patient
-
-                        ])
+                        or {
+                            'in'('mockPid', [
+                                    '',
+                            ])
+                            project {
+                                'in'('name', [
+                                        '',
+                                ])
+                            }
+                        }
                     }
                 }
-                ilseSubmission {
+                ilseSubmission(JoinType.LEFT_OUTER_JOIN.getJoinTypeValue()) {
                     'in'('ilseNumber', [
                             -1,
-                            // Ilse numbers
-
                     ])
                 }
             }
             'in'('seqType', SeqType.getAllAlignableSeqTypes())
         }
 
-
-
         //show seqtracks
         seqTracks.each {
-            println "${it.id}    ${it.project.name}    ${it.individual.mockPid}     ${it.sampleType.name}    ${it.seqType}    ${it.laneId}    ${it.run.name}    ${it.ilseId}"
+            println "${it.id}    ${it.project.name}    ${it.individual.mockPid}     ${it.sampleType.name}    ${it.seqType}    ${it.laneId}    ${it.run.name}    ${it.ilseId}    ${it.libraryPreparationKit}  ${it.seqPlatformGroup}"
         }
         println seqTracks.size()
+        println seqTracks*.sample.unique().size()
 
         /*
         //make all used run segments alignable
