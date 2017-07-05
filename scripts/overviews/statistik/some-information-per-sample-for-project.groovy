10876f795c8f5200139b36fe5978dfd5beb98128
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2017-07-05 15:31:58 +0200

    Statistik: seqPlatform and libraryPreparationKit per sample for given projects and seqtypes

---
+++ scripts/overviews/statistik/some-information-per-sample-for-project.groovy
@@ -0,0 +1,49 @@
+package overviews.statistik
+
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.utils.*
+
+
+/**
+ * Creates an overview of used seqPlatform and libraryPreparationKit and nreads per sample for the given Projects and SeqTypes.
+ * The sample is defined by project name, pid, sample type, and seqType.
+ * For each it shows all different seqPlatform and libraryPreparationKit, separated by semicolon.
+ * Furthermore it shows all nReads values, separated by semicolon.
+ */
+
+List<Project> projects = """
+PROJECT_NAME
+PROJECT_NAME
+""".split('\n')*.trim().findAll().collect {
+    CollectionUtils.exactlyOneElement(Project.findAllByName(it))
+}
+
+List<SeqType> seqTypes = [
+        SeqType.wholeGenomeBisulfitePairedSeqType,
+        SeqType.wholeGenomeBisulfiteTagmentationPairedSeqType
+]
+
+
+
+println SeqTrack.createCriteria().list {
+    sample {
+        individual {
+            'in'('project', projects)
+        }
+    }
+    'in'('seqType', seqTypes)
+}.groupBy { SeqTrack seqTrack->
+    [
+            seqTrack.project.name,
+            seqTrack.individual.pid,
+            seqTrack.sampleType.name,
+            seqTrack.seqType.toString(),
+    ].join(',')
+}.collect { String key, List<SeqTrack> seqTracks ->
+    [
+            key,
+            seqTracks*.seqPlatform*.toString().unique().sort().join(';'),
+            seqTracks*.libraryPreparationKit*.name.unique().sort().join(';'),
+            DataFile.findAllBySeqTrackInList(seqTracks)*.nReads.join(';'),
+    ].join(',')
+}.sort().join('\n')
