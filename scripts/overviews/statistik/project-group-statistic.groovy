57b8c24b51e4cfbd3560c8332d6352ce7b3111b7
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2017-07-05 13:58:22 +0200

    Statistik: Project group statistik

---
+++ scripts/overviews/statistik/project-group-statistic.groovy
@@ -0,0 +1,35 @@
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.utils.*
+
+/**
+ * Create Statistic about SeqType and Projects for project groups
+ */
+
+"""
+HIPO
+HIPO2
+""".split('\n')*.trim().findAll().each {
+    ProjectGroup projectGroup = CollectionUtils.exactlyOneElement(ProjectGroup.findAllByName(it))
+    List list = SeqTrack.createCriteria().list {
+        projections {
+            groupProperty('seqType')
+            sample {
+                individual {
+                    project {
+                        eq('projectGroup', projectGroup)
+                        countDistinct('id')
+                    }
+                    countDistinct('id')
+                }
+                countDistinct('id')
+            }
+        }
+    }.sort {
+        it[0].alias
+    }.collect {
+        "    ${it[0]}: Projecte: ${it[1]}, Individuals: ${it[2]}, Samples: ${it[3]} "
+    }
+    println "${projectGroup}: ${Project.countByProjectGroup(projectGroup)} Projekte"
+    println list.join('\n')
+}
+''
