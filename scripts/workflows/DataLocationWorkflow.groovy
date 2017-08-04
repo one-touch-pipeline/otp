8d70dc3791c46e35aaf0ebbecf3691a1a1428bde
Eva Reisinger <e.reisinger@dkfz-heidelberg.de>
2017-08-04 13:25:47 +0200

    OTP-2653: Delete DataLocationWorkflow
    Fixes OTP-2653

--- scripts/workflows/DataLocationWorkflow.groovy
+++
@@ -1,7 +0,0 @@
-import static de.dkfz.tbi.otp.utils.JobExecutionPlanDSL.*
-
-plan("DataLocationWorkflow") {
-    start("start", "dataLocationStartJob")
-    job("checkFinalLocation", "checkFinalLocationJob")
-    job("checkViewByPid", "checkViewByPidJob")
-}
