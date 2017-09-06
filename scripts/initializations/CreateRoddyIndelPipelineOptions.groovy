ed1272294e1eba592bb28ebfed23a784138b3e35
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2017-09-06 16:44:26 +0200

    Bugfix: Indel workflow test
    Currently the WES indel workflow test do not work, since WES needs another version then WGS.
    Therefore I addapt the test to use different versions for the different seqtypes. To simplefy it,
    I refactor the code to reuse projectService.configureIndelPipelineProject as in other analysis workflow tests.

---
+++ scripts/initializations/CreateRoddyIndelPipelineOptions.groovy
@@ -0,0 +1,31 @@
+import de.dkfz.tbi.otp.dataprocessing.*
+import de.dkfz.tbi.otp.dataprocessing.ProcessingOption.OptionName
+import de.dkfz.tbi.otp.ngsdata.*
+
+ProcessingOptionService processingOptionService = ctx.getBean("processingOptionService")
+
+println processingOptionService.createOrUpdate(
+        ProcessingOption.OptionName.PIPELINE_RODDY_INDEL_PLUGIN_NAME,
+        null,
+        null,
+        'IndelCallingWorkflow'
+)
+println processingOptionService.createOrUpdate(
+        OptionName.PIPELINE_RODDY_INDEL_PLUGIN_VERSION,
+        SeqType.wholeGenomePairedSeqType.roddyName,
+        null,
+        '1.0.176-1'
+)
+println processingOptionService.createOrUpdate(
+        OptionName.PIPELINE_RODDY_INDEL_PLUGIN_VERSION,
+        SeqType.exomePairedSeqType.roddyName,
+        null,
+        '1.0.167'
+)
+println processingOptionService.createOrUpdate(
+        ProcessingOption.OptionName.PIPELINE_RODDY_INDEL_PLUGIN_CONFIG,
+        null,
+        null,
+        'otpIndelCallingWorkflow-1.0'
+)
+
--- test/integration/workflows/analysis/pair/indel/AbstractIndelWorkflowTests.groovy
+++ test/integration/workflows/analysis/pair/indel/AbstractIndelWorkflowTests.groovy
@@ -1,21 +1,20 @@
 package workflows.analysis.pair.indel
 
 import de.dkfz.tbi.otp.dataprocessing.*
-import de.dkfz.tbi.otp.dataprocessing.roddyExecution.*
 import de.dkfz.tbi.otp.ngsdata.*
-import de.dkfz.tbi.otp.utils.*
-import de.dkfz.tbi.otp.utils.logging.*
+import grails.plugin.springsecurity.*
 import org.junit.*
 import workflows.analysis.pair.*
 
 abstract class AbstractIndelWorkflowTests extends AbstractRoddyBamFilePairAnalysisWorkflowTests<IndelCallingInstance> {
 
-    static final String PLUGIN_NAME = 'IndelCallingWorkflow'
-    static final String PLUGIN_VERSION = '1.0.176-1'
-    static final String PLUGIN = "${PLUGIN_NAME}:${PLUGIN_VERSION}"
-    static final String CONFIG_VERSION = 'v1_0'
-    static final String ANALYSIS = 'indelCallingAnalysis'
-    static final String IMPORT = 'otpIndelCallingWorkflow-1.0'
+    ConfigService configService
+
+    LsdfFilesService lsdfFilesService
+
+    ProjectService projectService
+
+    ProcessingOptionService processingOptionService
 
 
     @Test
@@ -26,76 +25,30 @@ abstract class AbstractIndelWorkflowTests extends AbstractRoddyBamFilePairAnalys
     }
 
 
-    File createXml() {
-        String name = RoddyWorkflowConfig.getNameUsedInConfig(
-                Pipeline.Name.RODDY_INDEL,
-                seqType,
-                PLUGIN,
-                CONFIG_VERSION
-        )
-
-        String xml = """
-<configuration configurationType='project'
-            name='${name}'
-            imports="${IMPORT}"
-            description='Indel project configuration for ${seqType.roddyName} in OTP.'>
-    <subconfigurations>
-        <configuration name='config' usedresourcessize='t'>
-            <availableAnalyses>
-                <analysis id='${seqType.roddyName}' configuration='${ANALYSIS}'/>
-            </availableAnalyses>
-        </configuration>
-    </subconfigurations>
-</configuration>
-"""
-
-        File configFilePath = RoddyWorkflowConfig.getStandardConfigFile(
-                project,
-                Pipeline.Name.RODDY_INDEL,
-                seqType,
-                PLUGIN_VERSION,
-                CONFIG_VERSION
-        )
-
-        File configDirectory = configFilePath.parentFile
-        String md5 = HelperUtils.getRandomMd5sum()
-
-        String script = """\
-#!/bin/bash
-set -evx
-
-umask 0027
-
-mkdir -p -m 2750 ${configDirectory}
-
-cat <<${md5} > ${configFilePath}
-${xml}
-${md5}
-
-chmod 0440 ${configFilePath}
-
-echo 'OK'
-"""
-
-        LogThreadLocal.withThreadLog(System.out) {
-            assert executionService.executeCommand(realm, script).trim() == "OK"
-        }
-
-        return configFilePath
-    }
-
     @Override
     ConfigPerProject createConfig() {
-        File xmlFilePath = createXml()
-
-        config = RoddyWorkflowConfig.importProjectConfigFile(
-                project,
-                seqType,
-                PLUGIN,
-                DomainFactory.createIndelPipelineLazy(),
-                xmlFilePath.absolutePath,
-                CONFIG_VERSION,
+        DomainFactory.createIndelPipelineLazy()
+        DomainFactory.createIndelSeqTypes()
+        DomainFactory.createReferenceGenomeProjectSeqType(
+                referenceGenome: referenceGenome,
+                project: project,
+                seqType: seqType,
         )
+        lsdfFilesService.createDirectory(new File(configService.getProjectSequencePath(project)), realm)
+
+        SpringSecurityUtils.doWithAuth("operator") {
+            config = projectService.configureIndelPipelineProject(
+                    new RoddyConfiguration([
+                            project          : project,
+                            seqType          : seqType,
+                            pluginName       : ProcessingOptionService.findOption(ProcessingOption.OptionName.PIPELINE_RODDY_INDEL_PLUGIN_NAME, null, null),
+                            pluginVersion    : ProcessingOptionService.findOption(ProcessingOption.OptionName.PIPELINE_RODDY_INDEL_PLUGIN_VERSION, seqType.roddyName, null),
+                            baseProjectConfig: ProcessingOptionService.findOption(ProcessingOption.OptionName.PIPELINE_RODDY_INDEL_PLUGIN_CONFIG, seqType.roddyName, null),
+                            configVersion    : 'v1_0',
+                            resources        : 't',
+                    ])
+            )
+        }
     }
 
 
@@ -105,6 +58,7 @@ echo 'OK'
                 "scripts/workflows/RoddyIndelWorkflow.groovy",
                 "scripts/initializations/AddPathToConfigFilesToProcessingOptions.groovy",
                 "scripts/initializations/AddRoddyPathAndVersionToProcessingOptions.groovy",
+                "scripts/initializations/CreateRoddyIndelPipelineOptions.groovy",
         ]
     }
 
