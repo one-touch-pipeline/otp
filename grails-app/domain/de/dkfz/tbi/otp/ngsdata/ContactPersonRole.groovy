ada1256c53eaef28be438b6f56c4eacf336aa907
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2018-02-14 10:57:26 +0100

    OTP-2739: Adapt ContactPersonRole

--- grails-app/domain/de/dkfz/tbi/otp/ngsdata/ContactPersonRole.groovy
+++ grails-app/domain/de/dkfz/tbi/otp/ngsdata/ContactPersonRole.groovy
@@ -2,9 +2,12 @@ package de.dkfz.tbi.otp.ngsdata
 
 import de.dkfz.tbi.otp.utils.Entity
 
-class ContactPersonRole implements Entity{
+class ContactPersonRole implements Entity {
 
     String name
+    boolean accessToOtp
+    boolean accessToFiles
+    boolean allowedToAddNewProjectUser
 
     static constraints = {
         name(blank: false, unique: true)
