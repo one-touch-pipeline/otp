96f846cff00f8072487c6a9d9f37d9da21c411fc
Philipp Strubel <p.strubel@dkfz-heidelberg.de>
2018-04-10 13:55:13 +0200

    changes alias tables to have a primary key

--- bashScripts/grails-migration-plugin/create-dbm-gorm-diff-without-false-changes.sh
+++ bashScripts/grails-migration-plugin/create-dbm-gorm-diff-without-false-changes.sh
@@ -19,7 +19,7 @@ prefix="s/\n\tchangeSet.*\n\t\t"
 suffix=".*\n\t\}\n//g"
 changelogPath="migrations/changelogs/${year}/${1}.groovy"
 indices="(_class_idx)|(config_per_project_project_seqtype_pipeline_individual_idx)"
-tables="(job_error_definition_job_error_definition)|(aceseq_instance_roddy_execution_directory_names)|(externally_processed_merged_bam_file_further_files)|(import_process_externally_processed_merged_bam_file)|(merging_work_package_seq_track)|(indel_calling_instance_roddy_execution_directory_names)|(job_error_definition_job_definition)|(roddy_bam_file_seq_track)|(roddy_snv_calling_instance_roddy_execution_directory_names)|(roddy_bam_file_roddy_execution_directory_names)|(seq_track_log_message)|(seq_platform_group_comment)|(sophia_instance_roddy_execution_directory_names)|(swap_info_seq_track)"
+tables="(job_error_definition_job_error_definition)|(aceseq_instance_roddy_execution_directory_names)|(externally_processed_merged_bam_file_further_files)|(import_process_externally_processed_merged_bam_file)|(merging_work_package_seq_track)|(indel_calling_instance_roddy_execution_directory_names)|(job_error_definition_job_definition)|(roddy_bam_file_seq_track)|(roddy_snv_calling_instance_roddy_execution_directory_names)|(roddy_bam_file_roddy_execution_directory_names)|(seq_track_log_message)|(seq_platform_group_comment)|(sophia_instance_roddy_execution_directory_names)|(swap_info_seq_track)|(seq_platform_model_label_alias)|(seq_type_alias)|(sequencing_kit_label_alias)"
 
 perl -0pi -e "${prefix}.*\"meta_data_key\".*\n.*\n${suffix}" $changelogPath
 perl -0pi -e "${prefix}.*file_system_changes_idx${suffix}" $changelogPath
--- migrations/changelog.groovy
+++ migrations/changelog.groovy
@@ -3,4 +3,8 @@ databaseChangeLog = {
 	changeSet(author: "strubelp", id:"OTP-2633-SQL") {
 		sqlFile(path: 'changelogs/2018/OTP-2633.sql')
 	}
+
+	changeSet(author: "strubelp", id:"fixed-alias-tables-sql") {
+		sqlFile(path: 'changelogs/2018/fixed-alias-tables.sql')
+	}
 }
