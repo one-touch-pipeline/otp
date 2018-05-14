713b03fd69b92ea24f052c4bde7c0288331d5a50
Philipp Strubel <p.strubel@dkfz-heidelberg.de>
2018-05-14 12:42:47 +0200

    fixed iframe pixel on sequences page

--- grails-app/assets/javascripts/pages/sequence/index/datatable.js
+++ grails-app/assets/javascripts/pages/sequence/index/datatable.js
@@ -21,6 +21,7 @@ $.otp.sequence = {
                     var iframe = document.createElement('iframe');
                     iframe.style.height = "0px";
                     iframe.style.width = "0px";
+                    iframe.style.border = "0px";
                     iframe.src = link + "?filtering=" + encodeURIComponent(JSON.stringify(searchCriteria()));
                     document.body.appendChild( iframe );
                 }
