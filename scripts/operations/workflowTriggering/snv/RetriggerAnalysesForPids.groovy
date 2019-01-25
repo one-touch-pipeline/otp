480d9f688f4159aeae390365b83b4b3bfa2ee0d8
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-01-25 13:45:11 +0100

    chg: Refactored RetriggerAnalysesForPids script
    - all the analyses are now commented out by default, to prevent accidents
    - the script only withdraws instances of the selected analyses
    - prints out each affected sample pair and its respective instances for each analysis type
    - prints out planned actions to review before execution
    - respects DISABLED ProcessingStatus

--- scripts/operations/workflowTriggering/snv/RetriggerAnalysesForPids.groovy
+++ scripts/operations/workflowTriggering/snv/RetriggerAnalysesForPids.groovy
@@ -2,7 +2,7 @@ import de.dkfz.tbi.otp.dataprocessing.*
 import de.dkfz.tbi.otp.dataprocessing.snvcalling.*
 import de.dkfz.tbi.otp.utils.logging.*
 
-println "\n\nRetrigger analyses for pids: "
+// INPUT
 def samplePairs = SamplePair.withCriteria {
     mergingWorkPackage1 {
         sample {
@@ -15,34 +15,79 @@ def samplePairs = SamplePair.withCriteria {
     }
 }
 
+List<String> analyses = [
+        //"snv",
+        //"indel",
+        //"sophia",
+        //"aceseq",
+        //"runYapsa",
+]
 
-//show all
-samplePairs.each { println "${it}" }
-println samplePairs.size()
 
-/*
+// OVERVIEW
+println "Affected analyses: (${analyses.size()})"
+analyses.each { println " - ${it}" }
+println "\nAffected SamplePairs: (${samplePairs.size()})"
+samplePairs.each { println " - ${it}" }
+println("\n")
+
+
+// CONFIGURATION
+// this maps the different names and objects each analysis needs to its name
+Map<String, Map<String, Object>> analysesObjectMapping = [
+        snv: [
+                processingStatusName: "snvProcessingStatus",
+                instanceClass:        RoddySnvCallingInstance,
+        ],
+        indel: [
+                processingStatusName: "indelProcessingStatus",
+                instanceClass:        IndelCallingInstance,
+        ],
+        sophia: [
+                processingStatusName: "sophiaProcessingStatus",
+                instanceClass:        SophiaInstance,
+        ],
+        aceseq: [
+                processingStatusName: "aceseqProcessingStatus",
+                instanceClass:        AceseqInstance,
+        ],
+        runYapsa: [
+                processingStatusName: "runYapsaProcessingStatus",
+                instanceClass:        RunYapsaInstance,
+        ],
+]
+
+
+// PROCESSING
 LogThreadLocal.withThreadLog(System.out, {
     SamplePair.withTransaction {
         samplePairs.each { SamplePair samplePair ->
-            BamFilePairAnalysis.findAllBySamplePair(samplePair).each { BamFilePairAnalysis instance ->
-                if (instance.processingState == AnalysisProcessingStates.IN_PROGRESS) {
-                    instance.withdrawn = true
-                    instance.save(flush: true, failOnError: true)
-                }
-            }
+            List<String> output = []
+            analyses.each { String analysis ->
+                List<String> actions = []
+                Map<String, Object> analysisObjects = analysesObjectMapping[analysis]
+                String processingStatusName = analysisObjects['processingStatusName']
+                Class instanceClass = analysisObjects['instanceClass']
 
-            [
-                "snvProcessingStatus",
-                "indelProcessingStatus",
-                "sophiaProcessingStatus",
-                "aceseqProcessingStatus",
-                "runYapsaProcessingStatus",
-            ].each {
-                samplePair."$it" = SamplePair.ProcessingStatus.NEEDS_PROCESSING
+                if (samplePair."$processingStatusName" == SamplePair.ProcessingStatus.DISABLED) {
+                    actions << "${processingStatusName} is ${SamplePair.ProcessingStatus.DISABLED} -> ignore"
+                } else {
+                    instanceClass.findAllBySamplePair(samplePair).each { BamFilePairAnalysis instance ->
+                        if (instance.processingState == AnalysisProcessingStates.IN_PROGRESS) {
+                            instance.withdrawn = true
+                            instance.save(flush: true, failOnError: true)
+                            actions << "withdraw: ${instance.class.simpleName}:${instance.id}"
+                        }
+                    }
+
+                    actions << "${processingStatusName}: ${samplePair."$processingStatusName"} -> ${SamplePair.ProcessingStatus.NEEDS_PROCESSING}"
+                    samplePair."$processingStatusName" = SamplePair.ProcessingStatus.NEEDS_PROCESSING
+                    samplePair.save(flush: true, failOnError: true)
+                }
+                output << sprintf("  * %-10s : %s", [analysis, actions.join('; ')])
             }
-            println samplePair.save(flush: true, failOnError: true)
+            println("\n${samplePair}\n${output.join('\n')}")
         }
+        assert false : "Failed intentionally, remove this to assert to start reprocessing"
     }
 })
-println samplePairs.size()
-// */
