558f21d6174c5c98b18f0456bac0c1344cf05561
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2019-02-05 16:18:47 +0100

    add: Operation script: Show blocked bamfiles for project with thressholds
    The script show for a project all current bam file with qc state blocked
    inclusive the raised thressholds.

---
+++ scripts/operations/BlockedQcThressholdForProject.groovy
@@ -0,0 +1,83 @@
+/**
+ * Shows all not passed threshold for bam files set to blocked for a selected project.
+ *
+ * The project is called for a given project (input area).
+ *
+ * The script search for bam files of following criteria:
+ * - asociated to the given project
+ * - QcTrafficLightStatus is set to BLOCKED
+ * - bam file is not withdrawn
+ * - bam file is the latest for the mergingWorkPackage
+ *
+ * Fo these bam files all not passed thressholds are listed
+ *
+ */
+
+import de.dkfz.tbi.otp.dataprocessing.*
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.qcTrafficLight.*
+import de.dkfz.tbi.otp.utils.*
+
+//----------------------------------------------------
+//input area
+
+String projectName = ""
+
+//----------------------------------------------------
+//processing
+
+assert projectName: 'No Project nam given'
+
+Project project = Project.findByName(projectName)
+assert project: "Project ${projectName} not found"
+
+//K20K-JRJ7D2 und K20K-ZFA224
+
+List<AbstractMergedBamFile> bamFiles = AbstractMergedBamFile.createCriteria().list {
+    workPackage {
+        sample {
+            individual {
+                eq('project', project)
+            }
+        }
+    }
+    eq('qcTrafficLightStatus', AbstractMergedBamFile.QcTrafficLightStatus.BLOCKED)
+    eq('withdrawn', false)
+}.findAll { AbstractMergedBamFile bamFile ->
+    bamFile.isMostRecentBamFile()
+}
+
+println "Found ${bamFiles.size()} blocked bam files"
+
+bamFiles.each { AbstractBamFile bamFile ->
+    println "Bamfile ${bamFile}"
+
+    AbstractQualityAssessment qc = CollectionUtils.exactlyOneElement(
+            bamFile.seqType.name == 'RNA' ?
+                    RnaQualityAssessment.createCriteria().list {
+                        qualityAssessmentMergedPass {
+                            eq('abstractMergedBamFile', bamFile)
+                        }
+                        eq('chromosome', RoddyQualityAssessment.ALL)
+                    } :
+                    RoddyMergedBamQa.createCriteria().list {
+                        qualityAssessmentMergedPass {
+                            eq('abstractMergedBamFile', bamFile)
+                        }
+                        eq('chromosome', RoddyQualityAssessment.ALL)
+                    }
+    )
+
+    List<QcThreshold> qcThresholds = QcThreshold.getValidQcPropertyForQcClass(qc.class.name).collect { String property ->
+        QcThreshold.findByQcClassAndSeqTypeAndQcProperty1AndProject(qc.class.name, bamFile.seqType, property, bamFile.project) ?:
+                QcThreshold.findByQcClassAndSeqTypeAndQcProperty1AndProjectIsNull(qc.class.name, bamFile.seqType, property)
+    }.findAll { QcThreshold qcThreshold ->
+        return qcThreshold?.qcPassed(qc) == QcThreshold.ThresholdLevel.ERROR
+    }
+    println "    Found thressholds (${qcThresholds.size()}:"
+    qcThresholds.each {
+        println "        ${it.qcProperty1}: ${qc[it.qcProperty1]}"
+    }
+}
+
+''
