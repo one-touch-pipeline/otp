df427167ba2aad1004ce251288fca5373cf4856c
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-02-26 13:36:29 +0100

    !minor: updateMD5SumOfDataFiles also handles fileSize and adds automatic comments
    - merged the script CorrectFileSizeInDatabase into updateMD5SumOfDataFiles
    - updateMD5SumOfDataFiles automatically appends a comment to the affected datafiles containing the old and new md5sum and fileSize

--- scripts/operations/dataInstallation/updateMD5SumOfDataFiles.groovy
+++ scripts/operations/dataInstallation/updateMD5SumOfDataFiles.groovy
@@ -10,20 +10,49 @@ import static de.dkfz.tbi.otp.utils.CollectionUtils.exactlyOneElement
  *               dataFileMap = [fileName: md5Sum, fileName: md5Sum]
  */
 
-String runName = ""
-Map dataFileMap = [:]
+String executingUser = ""
+List<String> runNames = [""]
+Map<String, String> dataFileMap = [:]
 
-Run run = exactlyOneElement(Run.findAllByName(runName))
+
+assert executingUser: "Your username is used to set a comment that informs of the changes to the DataFile"
+assert runNames: "Define the Runs"
+runNames.each {
+    assert it: "Invalid run in list of runs"
+}
+
+List<Run> runs = Run.findAllByNameInList(runNames)
+assert runs.size() == runNames.size(): "Didn't find Runs for all run names"
 
 DataFile.withTransaction {
-    dataFileMap.each { fileName, md5Sum ->
-        DataFile dataFile = exactlyOneElement(DataFile.findAllByRunAndFileName(run, fileName))
+    dataFileMap.each { String fileName, String newMd5sum ->
+        DataFile dataFile = exactlyOneElement(DataFile.findAllByRunInListAndFileName(runs, fileName))
 
-        dataFile.md5sum = md5Sum
-        assert dataFile.save(flush: true, failOnError: true)
+        String fromMd5 = dataFile.md5sum
+        String toMd5 = newMd5sum
+        Long fromFileSize = dataFile.fileSize
+        Long toFileSize = new File(ctx.lsdfFilesService.getFileViewByPidPath(dataFile)).size()
 
-        final MetaDataEntry metaDataEntry = exactlyOneElement(MetaDataEntry.findAllWhere(dataFile: dataFile, key: exactlyOneElement(MetaDataKey.findAllByName(MetaDataColumn.MD5.toString()))))
+        if (fromMd5 != toMd5) {
+            dataFile.md5sum = newMd5sum
+            dataFile.fileSize = toFileSize
+            String newComment = """${new Date().format("yyyy-MM-dd HH:mm:ss")}
+  changed md5sum   from: ${fromMd5} to: ${toMd5}
+  changed fileSize from: ${fromFileSize} to: ${toFileSize}"""
+            println(newComment)
+            String previous = dataFile.getComment()?.comment
+            ctx.commentService.createOrUpdateComment(
+                    dataFile,
+                    previous + (previous? "\n\n" : "") + newComment,
+                    executingUser
+            )
+            assert dataFile.save(flush: true, failOnError: true)
 
-        ctx.metaDataService.updateMetaDataEntry(metaDataEntry, md5Sum)
+            final MetaDataEntry metaDataEntry = exactlyOneElement(MetaDataEntry.findAllWhere(dataFile: dataFile, key: exactlyOneElement(MetaDataKey.findAllByName(MetaDataColumn.MD5.toString()))))
+            ctx.metaDataService.updateMetaDataEntry(metaDataEntry, newMd5sum)
+        } else {
+            println "Nothing done for ${fileName} with md5sum ${newMd5sum}"
+        }
     }
+    assert false: "Intentional failure for debugging"
 }
