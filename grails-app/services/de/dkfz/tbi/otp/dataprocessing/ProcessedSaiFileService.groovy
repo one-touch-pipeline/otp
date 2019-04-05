02a348b484e3c41f29beedf6e9c48ab62f65ec4d
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2019-04-05 12:55:14 +0200

    chg: Junit test with build: Delete OTP workflow depending methods and test in ProcessedSaiFileService

--- grails-app/services/de/dkfz/tbi/otp/dataprocessing/ProcessedSaiFileService.groovy
+++ grails-app/services/de/dkfz/tbi/otp/dataprocessing/ProcessedSaiFileService.groovy
@@ -26,9 +26,6 @@ import grails.gorm.transactions.Transactional
 
 import de.dkfz.tbi.otp.ngsdata.SavingException
 import de.dkfz.tbi.otp.ngsdata.SeqTrack
-import de.dkfz.tbi.otp.utils.logging.LogThreadLocal
-
-import static org.springframework.util.Assert.notNull
 
 @Transactional
 class ProcessedSaiFileService {
@@ -68,39 +65,4 @@ class ProcessedSaiFileService {
         return "${sampleType}_${runName}_${filename}.sai"
     }
 
-    /**
-     * Checks consistency for {@link #deleteProcessingFiles(ProcessedSaiFile)}.
-     *
-     * If there are inconsistencies, details are logged to the thread log (see {@link LogThreadLocal}).
-     *
-     * @return true if there is no serious inconsistency.
-     */
-    boolean checkConsistencyForProcessingFilesDeletion(final ProcessedSaiFile saiFile) {
-        notNull saiFile
-        return dataProcessingFilesService.checkConsistencyWithDatabaseForDeletion(saiFile, new File(getFilePath(saiFile)))
-    }
-
-    /**
-     * Deletes the *.sai file and the *.sai_bwaAlnErrorLog.txt file from the "processing" directory on
-     * the file system. Sets {@link ProcessedSaiFile#fileExists} to <code>false</code> and
-     * {@link ProcessedSaiFile#deletionDate} to the current time.
-     *
-     * @return The number of bytes that have been freed on the file system.
-     */
-    long deleteProcessingFiles(final ProcessedSaiFile saiFile) {
-        notNull saiFile
-        return dataProcessingFilesService.deleteProcessingFiles(
-                saiFile,
-                new File(getFilePath(saiFile)),
-                new File(bwaAlnErrorLogFilePath(saiFile)),
-        )
-    }
-
-    private def assertSave(def object) {
-        object = object.save(flush: true)
-        if (!object) {
-            throw new SavingException(object.toString())
-        }
-        return object
-    }
 }
