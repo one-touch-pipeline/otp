f7e9615c549d9cc8d8c262b173302c5930d19800
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2019-04-08 11:26:40 +0200

    chg: Junit test with build: Migrate AbstractBamFileUnitTests to AbstractBamFileSpec

--- src/test/groovy/de/dkfz/tbi/otp/dataprocessing/AbstractBamFileSpec.groovy
+++ src/test/groovy/de/dkfz/tbi/otp/dataprocessing/AbstractBamFileSpec.groovy
@@ -22,50 +22,73 @@
 
 package de.dkfz.tbi.otp.dataprocessing
 
-import grails.buildtestdata.mixin.Build
-import grails.test.mixin.TestFor
-import org.junit.Assert
-import org.junit.Test
+import grails.artefact.Artefact
+import grails.artefact.DomainClass
+import grails.testing.gorm.DataTest
+import grails.validation.Validateable
+import org.grails.core.artefact.DomainClassArtefactHandler
+import org.grails.datastore.gorm.GormEntity
+import spock.lang.Specification
 
 import de.dkfz.tbi.otp.ngsdata.*
 import de.dkfz.tbi.otp.utils.logging.LogThreadLocal
 
-@TestFor(MockAbstractBamFile)
-@Build([FileType, DataFile, ProcessedBamFile])
-class AbstractBamFileUnitTests {
+class AbstractBamFileSpec extends Specification implements DataTest {
 
-    @Test
+    @Override
+    Class[] getDomainClassesToMock() {
+        [
+                FileType,
+                DataFile,
+                MockAbstractBamFile,
+                ProcessedBamFile,
+        ]
+    }
+
+
     void testSave() {
+        given:
         AbstractBamFile bamFile = new MockAbstractBamFile()
-        Assert.assertTrue(bamFile.validate())
+
+        expect:
+        bamFile.validate()
         bamFile.save(flush: true)
     }
 
-    @Test
+
     void testSaveCoverageNotNull() {
+        given:
         AbstractBamFile bamFile = new MockAbstractBamFile()
         bamFile.coverage = 30.0
-        Assert.assertTrue(bamFile.validate())
+
+        expect:
+        bamFile.validate()
         bamFile.save(flush: true)
     }
 
-    @Test
-    void testWithdraw_ChangeStatusFromNeedsProcessingToDeclared() {
-        AbstractBamFile bamFile = new MockAbstractBamFile(
-                status: AbstractBamFile.State.NEEDS_PROCESSING,
-        )
-        assert bamFile.status == AbstractBamFile.State.NEEDS_PROCESSING
 
+    void testWithdraw_ChangeStatusFromNeedsProcessingToDeclared() {
+        given:
+        AbstractBamFile bamFile = new MockAbstractBamFile([
+                status: AbstractBamFile.State.NEEDS_PROCESSING,
+        ])
+        expect:
+        bamFile.status == AbstractBamFile.State.NEEDS_PROCESSING
+
+        when:
         LogThreadLocal.withThreadLog(System.out) {
             bamFile.withdraw()
         }
-        assert bamFile.withdrawn
-        assert bamFile.status == AbstractBamFile.State.DECLARED
+
+        then:
+        bamFile.withdrawn
+        bamFile.status == AbstractBamFile.State.DECLARED
     }
 }
 
 
-class MockAbstractBamFile extends AbstractBamFile {
+@Artefact(DomainClassArtefactHandler.TYPE)
+class MockAbstractBamFile extends AbstractBamFile implements DomainClass, GormEntity<MockAbstractBamFile>, Validateable {
 
     @Override
     MergingWorkPackage getMergingWorkPackage() {
