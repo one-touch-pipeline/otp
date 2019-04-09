e3bd037ba1bbb99dd8f1b2e2e299704fc808d044
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2019-04-09 11:08:09 +0200

    chg: Junit test without build: Fix tests in FileNotReadableExceptionUnitTests

--- src/test/groovy/de/dkfz/tbi/otp/ngsdata/FileNotReadableExceptionUnitTests.groovy
+++ src/test/groovy/de/dkfz/tbi/otp/ngsdata/FileNotReadableExceptionUnitTests.groovy
@@ -32,24 +32,24 @@ class FileNotReadableExceptionUnitTests {
     @Test
     void testFileNotReadableExceptionString() {
         FileNotReadableException e = new FileNotReadableException("tmp")
-        assertEquals("can not read file: tmp", e.message)
+        assert "can not read file: tmp" == e.message
     }
 
     @Test
     void testFileNotReadableExceptionStringParamIsNull() {
         FileNotReadableException e = new FileNotReadableException(null as String)
-        assertEquals("can not read file: null", e.message)
+        assert "can not read file: null" == e.message
     }
 
     @Test
     void testFileNotReadableExceptionFile() {
         FileNotReadableException e = new FileNotReadableException(new File("tmp"))
-        assertEquals("can not read file: tmp", e.message)
+        assert "can not read file: tmp" == e.message
     }
 
     @Test
     void testFileNotReadableExceptionFileParamIsNull() {
         FileNotReadableException e = new FileNotReadableException(null as File)
-        assertEquals("can not read file: null", e.message)
+        assert "can not read file: null" == e.message
     }
 }
