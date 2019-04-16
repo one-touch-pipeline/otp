3e6393646d05f50f11fc63be5384d922e2514f0c
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-04-16 14:11:15 +0200

    Fixed DataInstallationStartJobIntegrationSpec

--- src/integration-test/groovy/de/dkfz/tbi/otp/job/jobs/dataInstallation/DataInstallationStartJobIntegrationSpec.groovy
+++ src/integration-test/groovy/de/dkfz/tbi/otp/job/jobs/dataInstallation/DataInstallationStartJobIntegrationSpec.groovy
@@ -26,12 +26,14 @@ import grails.testing.mixin.integration.Integration
 import grails.transaction.Rollback
 import spock.lang.Specification
 
+import de.dkfz.tbi.TestCase
 import de.dkfz.tbi.otp.dataprocessing.ProcessingOptionService
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
 import de.dkfz.tbi.otp.job.scheduler.SchedulerService
 import de.dkfz.tbi.otp.ngsdata.*
 import de.dkfz.tbi.otp.tracking.OtrsTicket
 import de.dkfz.tbi.otp.tracking.TrackingService
+import de.dkfz.tbi.otp.utils.SessionUtils
 
 @Rollback
 @Integration
@@ -39,21 +41,22 @@ class DataInstallationStartJobIntegrationSpec extends Specification {
 
     void "execute calls setStartedForSeqTracks"() {
         given:
-        SeqTrack seqTrack = DomainFactory.createSeqTrack()
-        OtrsTicket otrsTicket = DomainFactory.createOtrsTicket()
-        JobExecutionPlan plan = DomainFactory.createJobExecutionPlan(enabled: true)
-        DomainFactory.createDataFile(runSegment: DomainFactory.createRunSegment(otrsTicket: otrsTicket), seqTrack: seqTrack)
+        SessionUtils.metaClass.static.withNewSession = { Closure c -> c() }
 
-        DataInstallationStartJob dataInstallationStartJob = new DataInstallationStartJob()
+        DataInstallationStartJob dataInstallationStartJob = Spy(DataInstallationStartJob)
+        dataInstallationStartJob.optionService = new ProcessingOptionService()
+        dataInstallationStartJob.trackingService = new TrackingService()
+        dataInstallationStartJob.seqTrackService = new SeqTrackService()
         dataInstallationStartJob.schedulerService = Mock(SchedulerService) {
             1 * createProcess(_, _, _) >> null
             _ * isActive() >> true
         }
-        dataInstallationStartJob.optionService = new ProcessingOptionService()
 
-        dataInstallationStartJob.trackingService = new TrackingService()
-        dataInstallationStartJob.setJobExecutionPlan(plan)
-        dataInstallationStartJob.seqTrackService = new SeqTrackService()
+        dataInstallationStartJob.setJobExecutionPlan(DomainFactory.createJobExecutionPlan(enabled: true))
+
+        SeqTrack seqTrack = DomainFactory.createSeqTrack()
+        OtrsTicket otrsTicket = DomainFactory.createOtrsTicket()
+        DomainFactory.createDataFile(runSegment: DomainFactory.createRunSegment(otrsTicket: otrsTicket), seqTrack: seqTrack)
 
         when:
         dataInstallationStartJob.execute()
@@ -61,6 +64,9 @@ class DataInstallationStartJobIntegrationSpec extends Specification {
         then:
         otrsTicket.installationStarted != null
         seqTrack.dataInstallationState == SeqTrack.DataProcessingState.IN_PROGRESS
+
+        cleanup:
+        TestCase.removeMetaClass(SessionUtils)
     }
 
 }
