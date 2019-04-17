37d16c30d50cd1aeeb812da11e0369b0cfd51fd7
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2019-04-17 17:38:11 +0200

    bugfix: fix used files for check code narc

--- bashScripts/gitlab-ci/codenarc-informations.sh
+++ bashScripts/gitlab-ci/codenarc-informations.sh
@@ -21,16 +21,16 @@
 # SOFTWARE.
 
 echo "Defined thresholds:"
-cat  grails-app/conf/BuildConfig.groovy  | grep -e "maxPriority.Violations" | sed -e "s/ *maxPriority/\tmax priority /g" -e "s/Violations = / violations: /g"
+cat  build.gradle  | grep -e "maxPriority.Violations" | sed -e "s/ *maxPriority/\tmax priority /g" -e "s/Violations = / violations: /g"
 
 echo "Found violations:"
-head target/CodeNarc-Report.txt | grep -e "^Summary: TotalFiles=" | sed -e "s/ P/\n\tfound priority /g" -e "s/=/ violations: /g" |  grep -v Summary
+head build/reports/codenarc/all.txt | grep -e "^Summary: TotalFiles=" | sed -e "s/ P/\n\tfound priority /g" -e "s/=/ violations: /g" |  grep -v Summary
 
 echo "Count of rules with violations:"
 for p in 1 2 3 4
 do
-    cat target/CodeNarc-Report.txt | grep Violation | grep -v Summary | grep " P=$p " | awk '{print $2}' | sort -u | wc -l | sed -e "s/^/\trule count of priority $p violations: /"
+    cat build/reports/codenarc/all.txt | grep Violation | grep -v Summary | grep " P=$p " | awk '{print $2}' | sort -u | wc -l | sed -e "s/^/\trule count of priority $p violations: /"
 done
 
 echo "Rules with violations count (priority in brace):"
-cat target/CodeNarc-Report.txt | grep Violation | grep -v Summary | awk '{print $2"("$3")"}' | sed -e "s/Rule=//" -e "s/P=//g" | sort | uniq -c | sort -k1,1hr
+cat build/reports/codenarc/all.txt | grep Violation | grep -v Summary | awk '{print $2"("$3")"}' | sed -e "s/Rule=//" -e "s/P=//g" | sort | uniq -c | sort -k1,1hr
