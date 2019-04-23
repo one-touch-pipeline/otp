c3f74aa281006df0133a927a1b6c23df4df7cde7
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-04-23 17:16:10 +0200

    Merged tests of ProcessingStepUnitTests into ProcessingStepSpec

--- src/test/groovy/de/dkfz/tbi/otp/job/processing/ProcessingStepSpec.groovy
+++ src/test/groovy/de/dkfz/tbi/otp/job/processing/ProcessingStepSpec.groovy
@@ -22,9 +22,10 @@
 
 package de.dkfz.tbi.otp.job.processing
 
-
 import grails.testing.gorm.DataTest
+import grails.validation.ValidationException
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import de.dkfz.tbi.otp.job.plan.JobDefinition
 import de.dkfz.tbi.otp.job.plan.JobExecutionPlan
@@ -60,5 +61,139 @@ class ProcessingStepSpec extends Specification implements DataTest {
 
         then:
         update.state == ExecutionState.SUCCESS
+    }
+
+    @Unroll
+    void "test nullable"() {
+        given:
+        ProcessingStep step = DomainFactory.createProcessingStep()
+        step.jobDefinition = jobDefinition ? step.jobDefinition : null
+        step.process = process ? step.process : null
+
+        when:
+        step.save(flush: true)
+
+        then:
+        ValidationException e = thrown()
+
+        if (jobDefinition) {
+            e.message.contains("on field 'jobDefinition': rejected value [null]")
+        }
+
+        if (process) {
+            e.message.contains("on field 'process': rejected value [null]")
+        }
+
+        where:
+        jobDefinition | process
+        null          | null
+        null          | "notNull"
+        "notNull"     | null
+    }
+
+    @Unroll
+    void "test previous expects same Process but different JobDefinition but has problem with #errorField"() {
+        given:
+        given:
+        Process process = DomainFactory.createProcess()
+        JobDefinition jobDefinition = DomainFactory.createJobDefinition(plan: process.jobExecutionPlan)
+        Process process2 = DomainFactory.createProcess()
+        JobDefinition jobDefinition2 = DomainFactory.createJobDefinition(plan: process2.jobExecutionPlan)
+
+        ProcessingStep step = DomainFactory.createProcessingStep(process: process, jobDefinition: jobDefinition)
+
+        Map<String, ProcessingStep> errorCausingProcessingSteps = [
+                "process": DomainFactory.createProcessingStep(process: process2, jobDefinition: jobDefinition2),
+                "jobDefinition": DomainFactory.createProcessingStep(process: process, jobDefinition: jobDefinition),
+        ]
+
+        when:
+        step.previous = errorCausingProcessingSteps[errorField]
+        step.save(flush: true)
+
+        then:
+        ValidationException e = thrown()
+        e.message.contains("on field 'previous': rejected value")
+        step.errors["previous"].code == errorField
+
+        where:
+        errorField      |_
+        "process"       |_
+        "jobDefinition" |_
+    }
+
+    @Unroll
+    void "test next expects same Process but different JobDefinition but has problem with #errorField"() {
+        given:
+        Process process = DomainFactory.createProcess()
+        JobDefinition jobDefinition = DomainFactory.createJobDefinition(plan: process.jobExecutionPlan)
+        Process process2 = DomainFactory.createProcess()
+        JobDefinition jobDefinition2 = DomainFactory.createJobDefinition(plan: process2.jobExecutionPlan)
+
+        ProcessingStep step = DomainFactory.createProcessingStep(process: process, jobDefinition: jobDefinition)
+
+        Map<String, ProcessingStep> errorCausingProcessingSteps = [
+                "process": DomainFactory.createProcessingStep(process: process2, jobDefinition: jobDefinition2),
+                "jobDefinition": DomainFactory.createProcessingStep(process: process, jobDefinition: jobDefinition),
+        ]
+
+        when:
+        step.next = errorCausingProcessingSteps[errorField]
+        step.save(flush: true)
+
+        then:
+        ValidationException e = thrown()
+        e.message.contains("on field 'next': rejected value")
+        step.errors["next"].code == errorField
+
+        where:
+        errorField      |_
+        "process"       |_
+        "jobDefinition" |_
+    }
+
+    void "test next and previous of a ProcessingStep can not be the same"() {
+        given:
+        Process process = DomainFactory.createProcess()
+        Process process2 = DomainFactory.createProcess()
+        JobDefinition jobDefinition = DomainFactory.createJobDefinition(plan: process.jobExecutionPlan)
+        JobDefinition jobDefinition2 = DomainFactory.createJobDefinition(plan: process2.jobExecutionPlan)
+
+        ProcessingStep step = DomainFactory.createProcessingStep(process: process, jobDefinition: jobDefinition)
+
+        when:
+        jobDefinition2.plan = process.jobExecutionPlan
+        ProcessingStep otherProcessingStep = DomainFactory.createProcessingStep(process: process, jobDefinition: jobDefinition2)
+        step.previous = otherProcessingStep
+        step.next = otherProcessingStep
+        step.save(flush: true)
+
+        then:
+        ValidationException e = thrown()
+        e.message.contains("on field 'previous': rejected value")
+        step.errors["previous"].code == "next"
+        e.message.contains("on field 'next': rejected value")
+        step.errors["next"].code == "previous"
+    }
+
+    void "test Process and JobDefinition of ProcessingStep reference the same JobExecutionPlan"() {
+        given: "valid ProcessingStep and Process and JobDefinition with unequal JobExecutionPlans"
+        Process process = DomainFactory.createProcess()
+        JobDefinition jobDefinition = DomainFactory.createJobDefinition()
+
+        ProcessingStep step = DomainFactory.createProcessingStep(process: process)
+
+        expect: "created ProcessingStep is valid and Plans differ"
+        step.validate()
+        step.process.jobExecutionPlan != jobDefinition.plan
+
+        when: "using objects with differing JobExecutionPlans"
+        step.jobDefinition = jobDefinition
+        step.save(flush: true)
+
+        then: "custom validator fails"
+        ValidationException e = thrown()
+        e.message.contains("on field 'process': rejected value")
+        e.message.contains("does not pass custom validation")
     }
 }
