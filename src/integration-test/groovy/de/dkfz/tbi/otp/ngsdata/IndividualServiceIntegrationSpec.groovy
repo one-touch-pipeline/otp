f190ac34b3d39bdb1f25a964764c28609a2e38fb
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-04-24 12:05:41 +0200

    Fixed and converted IndividualServiceTests

---
+++ src/integration-test/groovy/de/dkfz/tbi/otp/ngsdata/IndividualServiceIntegrationSpec.groovy
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2011-2019 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.dkfz.tbi.otp.ngsdata
+
+import grails.plugin.springsecurity.SpringSecurityUtils
+import grails.testing.mixin.integration.Integration
+import grails.transaction.Rollback
+import org.joda.time.DateTime
+import org.joda.time.DateTimeUtils
+import org.springframework.security.access.AccessDeniedException
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import de.dkfz.tbi.TestCase
+import de.dkfz.tbi.otp.security.User
+import de.dkfz.tbi.otp.security.UserAndRoles
+
+import static org.junit.Assert.assertNull
+import static org.junit.Assert.assertSame
+
+@Rollback
+@Integration
+class IndividualServiceIntegrationSpec extends Specification implements UserAndRoles {
+
+    IndividualService individualService
+    private static long ARBITRARY_TIMESTAMP = 1337
+
+    void setupData() {
+        createUserAndRoles()
+    }
+
+    @Unroll
+    void "getIndividual by #identifier as #role"() {
+        given:
+        setupData()
+        Individual individual = DomainFactory.createIndividual()
+        Map<String, Object> searchProperty = [id: individual.id, name: individual.mockFullName]
+        Individual returnedIndividual
+
+        when: "requesting an available individual"
+        SpringSecurityUtils.doWithAuth(role) {
+            returnedIndividual = individualService.getIndividual(searchProperty[identifier] as String)
+        }
+
+        then: "permission granted, returns individual"
+        returnedIndividual == individual
+
+        when: "requesting an unavailable individual"
+        SpringSecurityUtils.doWithAuth(role) {
+            returnedIndividual = individualService.getIndividual("unavail_" + searchProperty[identifier] as String)
+        }
+
+        then: "permission granted, returns null"
+        returnedIndividual == null
+
+        where:
+        role     | identifier
+        OPERATOR | "name"
+        OPERATOR | "id"
+        ADMIN    | "name"
+        ADMIN    | "id"
+    }
+
+    @Unroll
+    void "getIndividual by #identifier with permission granted via project membership"() {
+        given:
+        setupData()
+        Individual individual = DomainFactory.createIndividual()
+        Map<String, Object> searchProperty = [id: individual.id, name: individual.mockFullName]
+
+        when: "an unauthorized user requests an available individual"
+        SpringSecurityUtils.doWithAuth(TESTUSER) {
+            individualService.getIndividual(searchProperty[identifier] as String)
+        }
+
+        then: "throw an AccessDeniedException"
+        thrown(AccessDeniedException)
+
+        when: "add permission to user and request again"
+        SpringSecurityUtils.doWithAuth(ADMIN) {
+            addUserWithReadAccessToProject(User.findByUsername(TESTUSER), individual.project)
+        }
+
+        Individual returnedIndividual
+        SpringSecurityUtils.doWithAuth(TESTUSER) {
+            returnedIndividual = individualService.getIndividual(searchProperty[identifier] as String)
+        }
+
+        then: "return the requested individual"
+        returnedIndividual == individual
+
+        when: "non-project user requests the same individual"
+        SpringSecurityUtils.doWithAuth(USER) {
+            individualService.getIndividual(searchProperty[identifier] as String)
+        }
+
+        then: "still throws an AccessDeniedException"
+        thrown(AccessDeniedException)
+
+        where:
+        identifier | _
+        "name"     | _
+        "id"       | _
+    }
+
+    void "createCommentString, when several properties equal, should just add different ones"() {
+        given:
+        setupData()
+
+        String operation = "operation"
+        Map mapA = [a: 1, b: 2, c: 3]
+        Map mapB = [a: 1, b: 3, c: 4]
+        Date date = new Date()
+
+        when:
+        String result = individualService.createCommentString(operation, mapA, mapB, date, null)
+
+        then:
+        result == """== operation - ${date.format("yyyy-MM-dd HH:mm")} ==
+Old:
+b: 2
+c: 3
+New:
+b: 3
+c: 4
+"""
+    }
+
+    void "createCommentString, when comment with additional information, should return correct string"() {
+        given:
+        setupData()
+
+        String operation = "operation"
+        Map mapA = [a: 1]
+        Map mapB = [a: 2]
+        Date date = new Date()
+        String additionalInformation = "additional information"
+
+        when:
+        String result = individualService.createCommentString(operation, mapA, mapB, date, additionalInformation)
+
+        then:
+        result == """== operation - ${date.format("yyyy-MM-dd HH:mm")} ==
+${additionalInformation}
+Old:
+a: 1
+New:
+a: 2
+"""
+    }
+
+    void "createComment, when comment already exists, should add new comment"() {
+        given:
+        setupData()
+
+        Individual indOld = DomainFactory.createIndividual(comment: DomainFactory.createComment(comment: "old comment"))
+        Individual indNew = DomainFactory.createIndividual()
+        String operation = "operation"
+        Map mapOld = [individual: indOld]
+        Map mapNew = [individual: indNew]
+
+        DateTimeUtils.setCurrentMillisFixed(ARBITRARY_TIMESTAMP)
+
+        when:
+        SpringSecurityUtils.doWithAuth(OPERATOR) {
+            individualService.createComment(operation, mapOld, mapNew)
+        }
+
+        String comment = indNew.comment.comment
+
+        then:
+        comment == """== operation - ${new DateTime().toDate().format("yyyy-MM-dd HH:mm")} ==
+Old:
+individual: ${indOld}
+New:
+individual: ${indNew}
+
+${indOld.comment.comment}"""
+
+        DateTimeUtils.setCurrentMillisSystem()
+    }
+
+    void "createComment, should fail when parameters are null"() {
+        given:
+        setupData()
+
+        when:
+        individualService.createComment(null, null, null)
+
+        then:
+        thrown(AssertionError)
+    }
+
+    void "createComment, should fail, when maps have different key sets"() {
+        given:
+        setupData()
+
+        when:
+        individualService.createComment(null, [individual: DomainFactory.createIndividual(), a: 1], [individual: DomainFactory.createIndividual()])
+
+        then:
+        thrown(AssertionError)
+    }
+
+    void "createComment, should fail, when input maps contain no individual"() {
+        given:
+        setupData()
+
+        when:
+        individualService.createComment("", [property: null], [property: null])
+
+        then:
+        thrown(AssertionError)
+    }
+}
