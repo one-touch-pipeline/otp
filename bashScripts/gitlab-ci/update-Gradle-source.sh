c8d3caf1e4ea1dad42c25652339272e9fcc53973
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2019-05-24 15:52:37 +0200

    bugfix: Fix source of gradle
    Since there are problems in gitlab ci to download it from the internet, the source is
    located in the docker itself. Therefore it is necessary to update the gradle/wrapper/gradle-wrapper.properties
    to set the local source

---
+++ bashScripts/gitlab-ci/update-Gradle-source.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2011-2019 The OTP authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+#Since there are a proxy problem on the gitlab ci, it is not possible to load the data from outside.
+#Therefore a local source needs to be used.
+
+sed -i \
+    -e "s#distributionUrl=https\\\://downloads.gradle.org/distributions/gradle-4.9-all.zip#distributionUrl=file\\\:/data/gradle/gradle-current.zip#" \
+    gradle/wrapper/gradle-wrapper.properties
