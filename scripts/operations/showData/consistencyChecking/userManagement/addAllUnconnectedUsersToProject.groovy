58d0dd62d8dfb466865ad6478c0836ba1a2a11eb
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-06-12 11:06:07 +0200

    new: Added scripts to correct user inconsistencies
    These scripts should correct all user inconsistecies between the database and ldap.
    Executing them in the following order should resolve all problems:
      - createUnknownUsers.groovy
      - addAllUnconnectedUsersToProject.groovy
      - correctFileAccessFlagToMatchLdap.groovy

---
+++ scripts/operations/showData/consistencyChecking/userManagement/addAllUnconnectedUsersToProject.groovy
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2011-2019 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import de.dkfz.tbi.otp.administration.LdapService
+import de.dkfz.tbi.otp.administration.UserService
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.security.User
+import de.dkfz.tbi.otp.utils.CollectionUtils
+
+/**
+ *  Creates a UserProjectRole entry for all users that can be found in the LDAP group
+ *  but are not yet connected to the project.
+ *
+ *  Only uses usernames that already have a User entity, does not create new Users.
+ *  If you need to create all missing users, use createUnknownUsers.groovy
+ *
+ *  The used ProjectRole and flags can be configured at the top of the script.
+ *
+ *  For documentational purposes this script generates very verbose output.
+ */
+
+// ProjectRole to use for the new UserProjectRoles
+ProjectRole projectRole = ProjectRole.findByName("BIOINFORMATICIAN")
+// UserProjectRole flags
+boolean enabled = true
+boolean accessToOtp = true
+boolean accessToFiles = true
+boolean manageUsers = false
+boolean manageUsersAndDelegate = false
+boolean receivesNotifications = true
+
+List<String> blacklistedUsernames = [
+]
+
+assert projectRole: "ProjectRole not found"
+
+String getSortedListInfo(String text, List list) {
+    return "${text}\n${list.sort()}\n${list.size()} entries\n"
+}
+
+LdapService ldapService = ctx.ldapService
+
+List<String> allUsernames = User.list()*.username
+
+List<UserProjectRole> newUPRs = []
+
+Project.list().sort { it.name }.each { Project project ->
+    println("Project: ${project.name}")
+
+    List<String> usernamesInProject = UserProjectRole.createCriteria().list {
+        eq("project", project)
+        user {
+            isNotNull("username")
+        }
+    }*.user.username
+    println(getSortedListInfo("Usernames of project:", usernamesInProject ?: []))
+
+    String groupDistinguishedName = ldapService.getDistinguishedNameOfGroupByGroupName(project.unixGroup)
+    List<String> usernamesInLdapGroup = ldapService.getGroupMembersByDistinguishedName(groupDistinguishedName)
+    println(getSortedListInfo("Username in LDAP group:", usernamesInLdapGroup ?: []))
+
+    List<String> unconnectedUsernames = usernamesInLdapGroup - usernamesInProject
+    println(getSortedListInfo("Unconnected usernames:", unconnectedUsernames ?: []))
+
+    List<String> usersToConnect = unconnectedUsernames.intersect(allUsernames)
+    println(getSortedListInfo("Unconnected usernames with an OTP User:", usersToConnect ?: []))
+    println("\n")
+    usersToConnect.each { String username ->
+        if (!(username in blacklistedUsernames)) {
+            newUPRs << new UserProjectRole(
+                    user: CollectionUtils.exactlyOneElement(User.findAllByUsername(username)),
+                    project: project,
+                    projectRole: projectRole,
+                    enabled: enabled,
+                    accessToOtp: accessToOtp,
+                    accessToFiles: accessToFiles,
+                    manageUsers: manageUsers,
+                    manageUsersAndDelegate: manageUsersAndDelegate,
+                    receivesNotifications: receivesNotifications,
+            )
+        }
+    }
+}
+
+println("UserProjectRoles to be created:")
+println("username,project_name,project_role_name")
+newUPRs.each { UserProjectRole userProjectRole ->
+    println("${userProjectRole.user.username},${userProjectRole.project.name},${userProjectRole.projectRole.name}")
+}
+
+assert false: "Assert for debug, remove to continue"
+
+newUPRs*.save(flush: true)
+
+''
---
+++ scripts/operations/showData/consistencyChecking/userManagement/createUnknownUsers.groovy
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2011-2019 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import grails.validation.*
+
+import de.dkfz.tbi.otp.administration.*
+import de.dkfz.tbi.otp.ngsdata.Project
+import de.dkfz.tbi.otp.security.User
+
+/**
+ *  Looks up all users that are unknown to OTP and creates them.
+ *  An unknown user is a user, whose username is associated with a group in LDAP, but not with a User object in OTP.
+ *  LDAP groups are resolved via the unix group field of Project.
+ *
+ *  Failed User creations are listed. Resolve those manually.
+ */
+
+List<String> blacklistedUsernames = [
+
+]
+
+
+String getListInfo(String text, List list) {
+    return "${text}\n${list}\n${list.size()} entries\n"
+}
+
+LdapService ldapService = ctx.ldapService
+UserService userService = ctx.userService
+
+List<String> allUsernames = User.list()*.username.sort()
+
+println(getListInfo("Unique usernames of OTP users:", allUsernames))
+
+List<String> usernamesInUnixGroups = []
+
+Project.list().each { Project project ->
+    String groupDistinguishedName = ldapService.getDistinguishedNameOfGroupByGroupName(project.unixGroup)
+    usernamesInUnixGroups << ldapService.getGroupMembersByDistinguishedName(groupDistinguishedName)
+}
+
+usernamesInUnixGroups = usernamesInUnixGroups.flatten().unique().sort()
+
+println(getListInfo("Unique users over all unix groups used in OTP:", usernamesInUnixGroups))
+
+List<String> ldapOnlyUsers = usernamesInUnixGroups - allUsernames - blacklistedUsernames
+
+println(getListInfo("Users to be created:", ldapOnlyUsers))
+
+assert false: "Assert for debug, remove to continue"
+
+String separator = ","
+List<String> userPropertiesToPrint = ["username", "email", "realName"]
+
+List<String> fails = []
+
+println("Created users:")
+println(userPropertiesToPrint.join(separator))
+ldapOnlyUsers.each { String username ->
+    LdapUserDetails ldapUserDetails = ldapService.getLdapUserDetailsByUsername(username)
+    try {
+        User user = userService.createUser(ldapUserDetails.cn, ldapUserDetails.mail, ldapUserDetails.realName)
+        println(userPropertiesToPrint.collect { user[it] }.join(separator))
+    } catch (ValidationException e) {
+        fails << "Failed when creating user '${username}' (cn: ${ldapUserDetails.cn}, mail: ${ldapUserDetails.mail})\n${e}"
+    }
+}
+println("\n\nFailed to create:")
+println(fails.join("\n"))
+
+''
