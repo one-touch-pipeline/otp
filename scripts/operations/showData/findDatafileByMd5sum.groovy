0db4e75cdf265d2d41a0a46c5d4518116ea885f0
Alexander Kosnac <a.kosnac@dkfz-heidelberg.de>
2019-08-28 15:17:29 +0000

    Script to list information on DataFiles by MD5
    Lists MD5, file name, project, pid, seq type and SAMPLE_ID of the
    datafiles associated with the given md5sums.
    
    It finds the datafiles via the md5sum attribute and double checks
    for consistency with MetaDataEntries.

---
+++ scripts/operations/showData/findDatafileByMd5sum.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011-2019 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import de.dkfz.tbi.otp.ngsdata.*
+
+import static de.dkfz.tbi.otp.utils.CollectionUtils.exactlyOneElement
+
+String md5sums = """
+# md5sum 1
+# md5sum 2
+
+"""
+
+Closure<List<String>> nameStringToList = { String nameString ->
+    List<String> list = []
+    nameString.eachLine {
+        if (it.trim().isEmpty() || it.trim().startsWith("#")) {
+            return
+        }
+        list << it.trim()
+    }
+    return list
+}
+
+MetaDataKey sampleIdKey = MetaDataKey.findByName("SAMPLE_ID")
+
+List<String> md5sumList = nameStringToList(md5sums)
+
+md5sumList.each { String md5sum ->
+    List<DataFile> dataFiles = DataFile.findAllByMd5sum(md5sum)
+
+    int numberOfFoundFiles = dataFiles.size()
+    println "${md5sum} ${numberOfFoundFiles > 1 ? "[found ${numberOfFoundFiles} files with the given md5!]" : ''}"
+    dataFiles.each { DataFile dataFile ->
+        MetaDataEntry sampleIdMDEntry = exactlyOneElement(MetaDataEntry.findAllByDataFileAndKey(dataFile, sampleIdKey), "Could not find exactly one SAMPLE_ID metadata entry for DataFile")
+        SeqTrack dfSeqTrack = dataFile.seqTrack
+        println """\
+        ---> ${dataFile.fileName}
+             ${ctx.lsdfFilesService.getFileFinalPath(dataFile)}
+             Project:    ${dfSeqTrack.project}
+             PID:        ${dfSeqTrack.individual.pid}
+             SeqType:    ${dfSeqTrack.seqType}
+             SampleType: ${dfSeqTrack.sample.sampleType}
+             SAMPLE_ID:  ${sampleIdMDEntry.value}
+        """.stripIndent()
+    }
+}
+
+''
