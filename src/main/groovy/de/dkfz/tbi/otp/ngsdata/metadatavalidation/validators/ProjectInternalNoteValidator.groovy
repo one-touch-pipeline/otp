67bde4aa3156ba29200a0432cfd3129054b38223
Philipp Strubel <philippstrubel@gmail.com>
2019-10-23 15:13:48 +0000

    new: otp-242: Show internal note of a project during data validation

---
+++ src/main/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/ProjectInternalNoteValidator.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2011-2019 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.ngsdata.Project
+import de.dkfz.tbi.otp.ngsdata.SampleIdentifier
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.bam.BamMetadataValidator
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidator
+import de.dkfz.tbi.util.spreadsheet.validation.*
+
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.PROJECT
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.SAMPLE_ID
+import static de.dkfz.tbi.otp.utils.CollectionUtils.atMostOneElement
+
+@Component
+class ProjectInternalNoteValidator extends ValueTuplesValidator<ValidationContext> implements MetadataValidator, BamMetadataValidator {
+
+    @Override
+    Collection<String> getDescriptions() {
+        return [
+                "Shows the Internal Note of a Project.",
+        ]
+    }
+
+    @Override
+    List<String> getRequiredColumnTitles(ValidationContext context) {
+        []
+    }
+
+    @Override
+    List<String> getOptionalColumnTitles(ValidationContext context) {
+        return [PROJECT, SAMPLE_ID]*.name()
+    }
+
+    @Override
+    void checkMissingOptionalColumn(ValidationContext context, String columnTitle) {
+    }
+
+    @Override
+    void validateValueTuples(ValidationContext context, Collection<ValueTuple> valueTuples) {
+        Set<Project> projects = []
+        valueTuples.each {
+            String sampleId = it.getValue(SAMPLE_ID.name())
+            String projectName = it.getValue(PROJECT.name()) ?: ''
+            SampleIdentifier sampleIdentifier = atMostOneElement(SampleIdentifier.findAllByName(sampleId))
+            if (sampleIdentifier) {
+                projects << sampleIdentifier.project
+
+            } else {
+                Project projectFromProjectColumn = atMostOneElement(Project.findAllByNameOrNameInMetadataFiles(projectName, projectName))
+                if (projectFromProjectColumn) {
+                    projects << projectFromProjectColumn
+                }
+            }
+        }
+        projects.each { Project project ->
+            if (project && project.internalNotes) {
+                context.addProblem(Collections.emptySet(), Level.INFO, "Internal notes for project '${project.name}': ${project.internalNotes}", "A Project has internal notes")
+            }
+        }
+    }
+}
---
+++ src/test/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/ProjectInternalNoteValidatorSpec.groovy
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2011-2019 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import grails.testing.gorm.DataTest
+import spock.lang.Specification
+
+import de.dkfz.tbi.otp.dataprocessing.ProcessingOption
+import de.dkfz.tbi.otp.domainFactory.DomainFactoryCore
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidationContextFactory
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidationContext
+import de.dkfz.tbi.otp.utils.CollectionUtils
+import de.dkfz.tbi.util.spreadsheet.validation.Level
+import de.dkfz.tbi.util.spreadsheet.validation.Problem
+
+import static de.dkfz.tbi.otp.utils.CollectionUtils.containSame
+import static de.dkfz.tbi.otp.utils.CollectionUtils.exactlyOneElement
+
+class ProjectInternalNoteValidatorSpec extends Specification implements DataTest, DomainFactoryCore {
+
+    protected final static String INTERNAL_NOTES = 'notes'
+
+    @Override
+    Class[] getDomainClassesToMock() {
+        [
+                ProcessingOption,
+                Project,
+                Realm,
+                SampleIdentifier,
+        ]
+    }
+
+    void 'validate metadata, when project column does not exist, no problem'() {
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext()
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        context.problems.empty
+    }
+
+    void 'validate metadata, when project column exist but is empty or unknown project, no problem'() {
+        given:
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${MetaDataColumn.PROJECT}\n" +
+                        "\n" +
+                        "unknown"
+        )
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        context.problems.empty
+    }
+
+    void 'validate metadata, when project column exist but project has no internal note, no problem'() {
+        given:
+        Project project = createProject()
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${MetaDataColumn.PROJECT}\n" +
+                        "${project.name}\n"
+        )
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        context.problems.empty
+    }
+
+    void 'validate metadata, when project column exist and project has internal note, add info'() {
+        given:
+        Project project = createProject(internalNotes: INTERNAL_NOTES)
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${MetaDataColumn.PROJECT}\n" +
+                        "${project.name}\n" +
+                        "${project.name}\n"
+        )
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        Problem problem = exactlyOneElement(context.problems)
+        problem.level == Level.INFO
+        containSame(problem.affectedCells*.cellAddress, Collections.emptySet())
+        problem.message.contains("Internal notes for project '${project.name}': ${project.internalNotes}")
+    }
+
+    void 'validate metadata, when project column exists, one project has internal note another one does not, add info for the one that has'() {
+        given:
+        Project project = createProject(internalNotes: INTERNAL_NOTES)
+        Project project2 = createProject()
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${MetaDataColumn.PROJECT}\n" +
+                        "${project.name}\n" +
+                        "${project2.name}\n"
+        )
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        Problem problem = exactlyOneElement(context.problems)
+        problem.level == Level.INFO
+        containSame(problem.affectedCells*.cellAddress, Collections.emptySet())
+        problem.message.contains("Internal notes for project '${project.name}': ${project.internalNotes}")
+    }
+
+    void 'validate metadata, when project column exists, two projects each one with there own note, add infos'() {
+        given:
+        Project project = createProject(internalNotes: INTERNAL_NOTES)
+        Project project2 = createProject(internalNotes: INTERNAL_NOTES)
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${MetaDataColumn.PROJECT}\n" +
+                        "${project.name}\n" +
+                        "${project2.name}\n"
+        )
+        Collection<Problem> expectedProblems = [
+                new Problem(Collections.emptySet(), Level.INFO, "Internal notes for project '${project.name}': ${project.internalNotes}", "A Project has internal notes"),
+                new Problem(Collections.emptySet(), Level.INFO, "Internal notes for project '${project2.name}': ${project2.internalNotes}", "A Project has internal notes"),
+        ]
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        CollectionUtils.containSame(context.problems,  expectedProblems)
+    }
+
+    void 'validate metadata, when sample ID column exist for project with internal note, adds info'() {
+        given:
+        SampleIdentifier sampleIdentifier = DomainFactory.createSampleIdentifier()
+        sampleIdentifier.project.internalNotes = INTERNAL_NOTES
+        sampleIdentifier.project.save(flush: true)
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext(
+                "${MetaDataColumn.SAMPLE_ID}\n" +
+                        "${sampleIdentifier.name}\n"
+        )
+
+        when:
+        new ProjectInternalNoteValidator().validate(context)
+
+        then:
+        Problem problem = exactlyOneElement(context.problems)
+        problem.level == Level.INFO
+        containSame(problem.affectedCells*.cellAddress, Collections.emptySet())
+        problem.message.contains("Internal notes for project '${sampleIdentifier.project.name}': ${sampleIdentifier.project.internalNotes}")
+    }
+
+}
