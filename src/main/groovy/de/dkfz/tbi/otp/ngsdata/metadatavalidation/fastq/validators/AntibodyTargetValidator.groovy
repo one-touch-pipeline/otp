6e45b14021fcb35e54c16c4c55d612f94251f015
jpalbrecht <a-phill@online.de>
2020-04-22 11:43:57 +0000

    Apply suggestion to src/main/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/fastq/validators/AntibodyTargetValidator.groovy

--- src/main/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/fastq/validators/AntibodyTargetValidator.groovy
+++ src/main/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/fastq/validators/AntibodyTargetValidator.groovy
@@ -41,7 +41,7 @@ class AntibodyTargetValidator extends SingleValueValidator<MetadataValidationCon
 
     @Override
     Collection<String> getDescriptions() {
-        return ["If the antibody target is given. it must be registered in the OTP database (case-insensitive)."]
+        return ["If the antibody target is given, it must be registered in the OTP database (case-insensitive)."]
     }
 
     @Override
