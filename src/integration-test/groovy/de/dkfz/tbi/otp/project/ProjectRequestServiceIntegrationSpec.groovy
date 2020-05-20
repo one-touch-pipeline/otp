c4f3ad80f4c004106206a5de9ec7c06707a566c2
Stefan Borufka <stefan.borufka@bioquant.uni-heidelberg.de>
2020-05-20 15:18:32 +0200

    chg: otp-333: Move ProjectRequestServiceIntegrationSpec to package project

--- src/integration-test/groovy/de/dkfz/tbi/otp/project/ProjectRequestServiceIntegrationSpec.groovy
+++ src/integration-test/groovy/de/dkfz/tbi/otp/project/ProjectRequestServiceIntegrationSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2019 The OTP authors
+ * Copyright 2011-2020 The OTP authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package de.dkfz.tbi.otp.ngsdata
+package de.dkfz.tbi.otp.project
 
 import grails.plugin.springsecurity.SpringSecurityService
 import grails.test.mixin.integration.Integration
@@ -34,10 +34,7 @@ import de.dkfz.tbi.TestCase
 import de.dkfz.tbi.otp.dataprocessing.ProcessingOption
 import de.dkfz.tbi.otp.dataprocessing.ProcessingOptionService
 import de.dkfz.tbi.otp.domainFactory.DomainFactoryCore
-import de.dkfz.tbi.otp.project.Project
-import de.dkfz.tbi.otp.project.ProjectRequest
-import de.dkfz.tbi.otp.project.ProjectRequestCreationCommand
-import de.dkfz.tbi.otp.project.ProjectRequestService
+import de.dkfz.tbi.otp.ngsdata.*
 import de.dkfz.tbi.otp.security.User
 import de.dkfz.tbi.otp.security.UserAndRoles
 import de.dkfz.tbi.otp.utils.MailHelperService
