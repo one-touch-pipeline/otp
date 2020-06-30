71f4f1ab6e06dc5e70bc547e7d61d048feaec88e
Stefan Borufka <stefan.borufka@bioquant.uni-heidelberg.de>
2020-06-30 13:47:02 +0200

    new: otp-582: data installation start job migration: create service to create objects needed for the new data installation workflow

---
+++ grails-app/services/de/dkfz/tbi/otp/workflow/datainstallation/DataInstallationInitializationService.groovy
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2011-2020 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.datainstallation
+
+import grails.gorm.transactions.Transactional
+
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.workflowExecution.*
+
+import java.nio.file.Paths
+
+@Transactional
+class DataInstallationInitializationService {
+
+    static final String WORKFLOW = "FASTQ installation"
+
+    static final String OUTPUT_ROLE = "FASTQ"
+
+    LsdfFilesService lsdfFilesService
+
+    WorkflowArtefactService workflowArtefactService
+
+    WorkflowRunService workflowRunService
+
+    /**
+     * Create for all {@link SeqTrack}s of the {@link FastqImportInstance} a {@link WorkflowRun} and an output {@link WorkflowArtefact}
+     * with role {@link #OUTPUT_ROLE} connecting the {@link WorkflowRun} with the {@link SeqTrack}.
+     *
+     * The returned {@link WorkflowRun}s are saved, but not flushed yet.
+     */
+    List<WorkflowRun> createWorkflowRuns(FastqImportInstance instance, ProcessingPriority priority = null) {
+        Workflow workflow = Workflow.getExactlyOneWorkflow(WORKFLOW)
+        return instance.dataFiles.groupBy {
+            it.seqTrack
+        }.collect { SeqTrack seqTrack, List<DataFile> dataFiles ->
+            createRunForSeqTrack(workflow, seqTrack, dataFiles, priority ?: seqTrack.processingPriority)
+        }
+    }
+
+    private WorkflowRun createRunForSeqTrack(Workflow workflow, SeqTrack seqTrack, List<DataFile> dataFiles, ProcessingPriority priority) {
+        String directory = Paths.get(lsdfFilesService.getFileViewByPidPath(dataFiles.first())).parent
+        WorkflowRun run = workflowRunService.createWorkflowRun(workflow, priority, directory)
+        WorkflowArtefact artefact = workflowArtefactService.createWorkflowArtefact(run, OUTPUT_ROLE, seqTrack.individual, seqTrack.seqType)
+        seqTrack.workflowArtefact = artefact
+        seqTrack.save(flush: false)
+        return run
+    }
+}
---
+++ src/test/groovy/de/dkfz/tbi/otp/workflow/datainstallation/DataInstallationInitializationServiceSpec.groovy
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2011-2020 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.datainstallation
+
+import grails.testing.gorm.DataTest
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+import de.dkfz.tbi.TestCase
+import de.dkfz.tbi.otp.domainFactory.workflowSystem.WorkflowSystemDomainFactory
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.workflowExecution.*
+
+class DataInstallationInitializationServiceSpec extends Specification
+        implements ServiceUnitTest<DataInstallationInitializationService>, DataTest, WorkflowSystemDomainFactory {
+
+    @Override
+    Class[] getDomainClassesToMock() {
+        [
+                FastqImportInstance,
+                SeqTrack,
+                WorkflowArtefact,
+                WorkflowRun,
+                WorkflowRunInputArtefact,
+        ]
+    }
+
+    void "createWorkflowRuns, when called, then return a WorkflowRun for each seqTrack of the datafiles of the instance"() {
+        given:
+        Workflow workflow = createWorkflow([
+                name: DataInstallationInitializationService.WORKFLOW,
+        ])
+
+        Collection<SeqTrack> seqTracks = (1..3).collect {
+            createSeqTrackWithTwoDataFile()
+        }
+
+        FastqImportInstance instance = createFastqImportInstance([
+                dataFiles: seqTracks*.dataFiles.flatten(),
+        ])
+
+        service.lsdfFilesService = Mock(LsdfFilesService) {
+            3 * getFileViewByPidPath(_) >> TestCase.uniqueNonExistentPath
+        }
+        service.workflowRunService = new WorkflowRunService()
+        service.workflowArtefactService = new WorkflowArtefactService()
+
+        when:
+        List<WorkflowRun> runs = service.createWorkflowRuns(instance)
+
+        then:
+        runs
+        runs.size() == 3
+        runs.each {
+            assert it.inputArtefacts.isEmpty()
+            assert it.workflow == workflow
+            assert it.outputArtefacts.size() == 1
+        }
+        TestCase.assertContainSame(runs, seqTracks*.workflowArtefact*.producedBy)
+    }
+}
