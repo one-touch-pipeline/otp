eed490b435a773eff59db7f1f6b4943afe272a78
Stefan Borufka <stefan.borufka@bioquant.uni-heidelberg.de>
2020-09-10 10:49:14 +0000

    scripts: otp-753: Scripts to show and update thresholds

---
+++ scripts/operations/threshold/threshold-overview.groovy
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2011-2020 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import de.dkfz.tbi.otp.dataprocessing.ProcessingThresholds
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.project.Project
+import de.dkfz.tbi.otp.utils.CollectionUtils
+
+/**
+ * This script shows the categories and thresholds of all configured sampleTypes for a given project.
+ *
+ * Additionally, it shows missing categories and thresholds for the project.
+ */
+
+//-----------------------------------------------
+//input
+
+String projectName = ""
+
+
+//------------------------------------------------
+//working
+
+Project project = CollectionUtils.exactlyOneElement(Project.findAllByName(projectName), "Could not find project $projectName")
+
+
+SampleTypeService sampleTypeService = ctx.sampleTypeService
+
+SeqType wes = SeqTypeService.exomePairedSeqType
+SeqType wgs = SeqTypeService.wholeGenomePairedSeqType
+
+List<SampleType> sampleTypes = sampleTypeService.findUsedSampleTypesForProject(project)
+List<SampleTypePerProject> sampleTypePerProjects = SampleTypePerProject.findAllByProject(project)
+List<SampleType> sampleTypesWithCategory = sampleTypePerProjects*.sampleType
+List<ProcessingThresholds> processingThresholds = ProcessingThresholds.findAllByProject(project)
+List<SampleType> sampleTypesWithWesThreshold = processingThresholds.findAll { it.seqType == wes }*.sampleType
+List<SampleType> sampleTypesWithWgsThreshold = processingThresholds.findAll { it.seqType == wgs }*.sampleType
+
+
+println '\n\ncategories: '
+println sampleTypePerProjects.collect {
+    [
+            it.sampleType,
+            it.category,
+    ].join(' ')
+}.sort().join('\n')
+
+println '\n\nprocessingThresholds: '
+println processingThresholds.collect {
+    [
+            it.sampleType,
+            it.seqType,
+            it.numberOfLanes,
+            it.coverage,
+    ].join(' ')
+}.sort().join('\n')
+
+println '\n\nmissed categories: '
+println sampleTypes.findAll {
+    !sampleTypesWithCategory.contains(it)
+}*.name.sort().join('\n')
+
+println '\n\nmissed wes processingThresholds: '
+println sampleTypes.findAll {
+    !sampleTypesWithWesThreshold.contains(it)
+}*.name.sort().join('\n')
+
+println '\n\nmissed wgs processingThresholds: '
+println sampleTypes.findAll {
+    !sampleTypesWithWgsThreshold.contains(it)
+}*.name.sort().join('\n')
+
+''
---
+++ scripts/operations/threshold/threshold-setting.groovy
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2011-2020 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import de.dkfz.tbi.otp.dataprocessing.ProcessingThresholdsService
+import de.dkfz.tbi.otp.dataprocessing.snvcalling.SamplePairDeciderService
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.project.Project
+import de.dkfz.tbi.otp.utils.CollectionUtils
+
+/**
+ * This script allows to create/update category and threshold for all sampleTypes of a project.
+ *
+ * The sampleTypes need to exist already in OTP, but must not be connected to the project yet.```
+ */
+
+//-----------------------------------------------
+//input
+
+String projectName = ""
+
+String sampleTypesDiseaseInput = """
+#sampletype1
+#sampletype2
+
+
+"""
+
+
+String sampleTypesControlInput = """
+#sampletype2
+#sampletype3
+
+
+"""
+
+int laneCountWes = 1
+int laneCountWgs = 1
+
+//------------------------------------------------
+//working
+
+Project project = CollectionUtils.exactlyOneElement(Project.findAllByName(projectName), "Could not find project $projectName")
+
+List<SampleType> sampleTypesDisease = sampleTypesDiseaseInput.split('\n')*.trim().findAll { String line ->
+    line && !line.startsWith('#')
+}.collect {
+    SampleType sampleType = SampleType.findSampleTypeByName(it)
+    assert sampleType : "Sample type $it not found"
+    return sampleType
+}
+List<SampleType> sampleTypesControl = sampleTypesControlInput.split('\n')*.trim().findAll { String line ->
+    line && !line.startsWith('#')
+}.collect {
+    SampleType sampleType = SampleType.findSampleTypeByName(it)
+    assert sampleType : "Sample type $it not found"
+    return sampleType
+}
+List<SampleType> allSampleTypes = [
+        sampleTypesDisease,
+        sampleTypesControl,
+].flatten()
+
+SampleTypePerProjectService sampleTypePerProjectService = ctx.sampleTypePerProjectService
+ProcessingThresholdsService processingThresholdsService = ctx.processingThresholdsService
+SamplePairDeciderService samplePairDeciderService = ctx.samplePairDeciderService
+
+SeqType wes = SeqTypeService.exomePairedSeqType
+SeqType wgs = SeqTypeService.wholeGenomePairedSeqType
+
+
+SampleType.withNewTransaction {
+    sampleTypesDisease.each {
+        sampleTypePerProjectService.createOrUpdate(project, it, SampleType.Category.DISEASE)
+    }
+    println "create disease: ${sampleTypesDisease*.name.join(', ')}"
+
+    sampleTypesControl.each {
+        sampleTypePerProjectService.createOrUpdate(project, it, SampleType.Category.CONTROL)
+    }
+    println "create control: ${sampleTypesControl*.name.join(', ')}"
+
+    allSampleTypes.each { SampleType sampleType ->
+        processingThresholdsService.createUpdateOrDelete(project, sampleType, wes, laneCountWes, null)
+        processingThresholdsService.createUpdateOrDelete(project, sampleType, wgs, laneCountWgs, null)
+    }
+    println "create/update thresholds "
+    println "    - for WGS with: ${laneCountWgs} lanes and no coverage"
+    println "    - for WES with: ${laneCountWes} lanes and no coverage"
+    println "    - the following sample types:  ${allSampleTypes*.name.join(', ')}"
+
+    samplePairDeciderService.findOrCreateSamplePairsForProject(project)
+    println "create sample pairs "
+
+
+    assert false: "DEBUG: transaction intentionally failed to rollback changes"
+}
+''
