179de4924326fbb41274318e14172a70d94f722f
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2020-10-12 07:41:46 +0000

    Scripts: otp-793: Improve Script DeleteIndividual
    - Adapt to allow delete multiple individuals together
    - add some documentation
    - add some output

--- scripts/operations/dataCleanup/DeleteIndividual.groovy
+++ scripts/operations/dataCleanup/DeleteIndividual.groovy
@@ -19,26 +19,60 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 import de.dkfz.tbi.otp.config.ConfigService
 import de.dkfz.tbi.otp.infrastructure.FileService
 import de.dkfz.tbi.otp.ngsdata.Individual
+import de.dkfz.tbi.otp.utils.CollectionUtils
 import de.dkfz.tbi.otp.utils.DeletionService
 
 import java.nio.file.Path
 
+/**
+ * Script to delete individuals with all data.
+ *
+ * It deletes all data of the individual in OTP database and generates a bash script to delete it on the files system.
+ * The file name is constructed by all names, but truncated to 100 chars.
+ *
+ * The individuals are given by there pid.
+ *
+ * Please note, that the script needs some time per pid, depending of the amount of data. This can cause proxy timeout. But a proxy timeout does not stop the
+ * request in OTP, it will continue to run until it succeeds or an error is thrown.
+ */
+
+
 // input area
 //----------------------
 
-String pid = ""
+/**
+ * input area for pids, one pid per line.
+ * All values are trimmed, empty lines and lines starting with '#' are ignored.
+ */
+String pids = """
+#pid
+#pid2
 
+
+"""
+
+
+/**
+ * Flag to indicate, if it should be checked for files only linked and for external bam files.
+ * If checked and some found, an exception is thrown
+ */
 boolean check = true
 
 
 //script area
 //-----------------------------
 
-assert pid : 'No pid is provided'
+List<Individual> individuals = pids.split('\n')*.trim().findAll { String line ->
+    line && !line.startsWith('#')
+}.collect {
+    CollectionUtils.exactlyOneElement(Individual.findAllByPid(it), "Could not find pid '${it}'")
+}.unique()
+
+String combinedPids = individuals*.pid.join('__')
+String fileName = "Delete_${combinedPids.size() < 110 ? combinedPids : combinedPids.substring(0, 100) + '_and_others'}.sh"
 
 DeletionService deletionService = ctx.deletionService
 FileService fileService = ctx.fileService
@@ -46,11 +80,25 @@ FileService fileService = ctx.fileService
 Path baseOutputDir = ConfigService.getInstance().getScriptOutputPath().toPath().resolve('sample_swap')
 
 Individual.withTransaction {
-    List<String> allFilesToRemove = deletionService.deleteIndividual(pid, check)
+    List<String> allFilesToRemove = [
+            "#!/bin/bash",
+            "",
+            "set -evx",
+    ]
 
-    Path deleteFileCmd = fileService.createOrOverwriteScriptOutputFile(baseOutputDir, "Delete_${pid}.sh")
+    individuals.each {
+        println "Delete: ${it} of project ${it.project}"
+        allFilesToRemove << "\n\n#${it}"
+        allFilesToRemove << deletionService.deleteIndividual(it, check)[0]
+    }
 
-    deleteFileCmd << allFilesToRemove[0]
+    Path deleteFileCmd = fileService.createOrOverwriteScriptOutputFile(baseOutputDir, fileName)
 
-    assert false
+    deleteFileCmd << allFilesToRemove.join('\n')
+
+    println ''
+    println "Deletion file is written to:"
+    println deleteFileCmd
+
+    assert false: "DEBUG: transaction intentionally failed to rollback changes"
 }
