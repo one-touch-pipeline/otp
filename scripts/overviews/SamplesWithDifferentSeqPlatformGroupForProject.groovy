f73f1adef56943f6c4bbbe683f7fe0f2325f94a1
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2020-12-21 11:38:08 +0100

    scripts: otp-901: Fix and improve script to show seqPlatform groups problems
    - remove using name of group, since it does not exist anymore
    - use tab as seperator
    - each seqPlatform group get its own column
    - add header column
    - sort seqPlatformgroup description alphabetically
    - check that project exists
    - add documentation

--- scripts/overviews/SamplesWithDifferentSeqPlatformGroupForProject.groovy
+++ scripts/overviews/SamplesWithDifferentSeqPlatformGroupForProject.groovy
@@ -21,34 +21,66 @@
  */
 
 import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.project.Project
+import de.dkfz.tbi.otp.utils.CollectionUtils
 
+/**
+ * show for one project all samples, those seqTracks belongs to different seqPlatformGroups.
+ *
+ * Only seqTracks of alignable seqTypes are considered.
+ *
+ * The check only work, if the mergingCriteria is defined for the project and seqType, since that is needed to find the seqPlatformGroup.
+ *
+ * The input is the name of the project to check.
+ *
+ * The output is an table for all sample/seqtype combination with more then one seqPlatformgroup with the following columns:
+ * - pid
+ * - sample type
+ * - seq type (name, sequencongReadType, single cell)
+ * - first seqplatform group
+ * - second seqplatform group
+ * - ... further seq platform groups
+ */
+
+
+//---------------------------
+//input area
 String projectName = ''
 
-assert projectName : 'Please select a project'
 
-List<SeqType> seqTypes = [
-        SeqTypeService.exomePairedSeqType,
-        SeqTypeService.wholeGenomePairedSeqType,
-]
+//---------------------------
+//work area
+assert projectName : 'Please select a project'
+Project project = CollectionUtils.exactlyOneElement(Project.findAllByName(projectName),
+        "Could not find project with name: '${projectName}'")
+
+List<SeqType> seqTypes = SeqTypeService.allAlignableSeqTypes
 
 println "sample with different seqplatformgroups"
+println "\n"
+println "pid\tsampleType\tseqType\tseqPlatformgroup1\tseqPlatformgroup2\t..."
 List<SeqTrack> seqTracks = SeqTrack.createCriteria().list {
     sample {
         individual {
-            project {
-                eq('name', projectName)
-            }
+            eq('project', project)
         }
     }
     'in'('seqType', seqTypes)
 }
 
 println seqTracks.collect {
-    [[it.individual.pid, it.sampleType.name, it.seqType.name, it.seqType.libraryLayout], it.seqPlatformGroup?.name]
+    [
+            [
+                    it.individual.pid,
+                    it.sampleType.name,
+                    it.seqType.displayNameWithLibraryLayout,
+            ],
+            it.seqPlatformGroup,
+    ]
 }.unique().groupBy {
     it[0]
 }.findAll { key, value ->
     value.size() > 1
 }.collect { key, value ->
-    "${key.join(' ')}   ${value*.get(1).join(',')}"
+    (key + value*.get(1)*.toString().sort()).join('\t')
 }.sort().join('\n')
