de25032ca0f75aaa42fd5b091c0860adf1210c4b
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2021-01-14 14:21:22 +0000

    scripts: otp-903: Extend deleteExternalBamFiles to work additional dependencies
    The script based on bam files of the initial import, which based only on registration of epmbf.
    
    Therefore it needs to extend to:
    - delete also the qualityAssessment
    - Analyssis running on the bam files
    - SamplePairs based on the ExternalMergingWorkPackage

--- scripts/operations/dataCleanup/admin/deleteExternalBamFiles.groovy
+++ scripts/operations/dataCleanup/admin/deleteExternalBamFiles.groovy
@@ -20,14 +20,47 @@
  * SOFTWARE.
  */
 
-import de.dkfz.tbi.otp.dataprocessing.*
-import de.dkfz.tbi.otp.utils.CollectionUtils
 
+import de.dkfz.tbi.otp.dataprocessing.*
+import de.dkfz.tbi.otp.dataprocessing.snvcalling.AnalysisDeletionService
+import de.dkfz.tbi.otp.dataprocessing.snvcalling.SamplePair
+
+/**
+ * Script to delete ExternallyProcessedMergedBamFile inclusive dependencies for given pids.
+ *
+ * The script change only the database. For the filesystem a bash script is displayed.
+ *
+ * The script has a tryRun mode, were all changes are roll backed.
+ *
+ * input:
+ * - a list of pids
+ *
+ * output:
+ * - list of deleted bam files and analysis
+ * - bash script to delete it from the file system
+ *
+ * The script
+ */
+
+//--------------------------------
+//input
+
+/**
+ * list of Pids.
+ */
 List<String> pidsToDelete = [
         'pid1',
         'pid2',
 ]
 
+/**
+ * flag to allow a try and rollback the changes at the end (true) or do the changes(false)
+ */
+boolean tryRun = true
+
+
+//--------------------------------
+//work
 
 def bamFiles = ExternallyProcessedMergedBamFile.createCriteria().listDistinct {
     workPackage {
@@ -39,15 +72,82 @@ def bamFiles = ExternallyProcessedMergedBamFile.createCriteria().listDistinct {
     }
 }
 
-assert CollectionUtils.containSame(bamFiles*.individual*.pid.unique(), pidsToDelete)
+List<String> dirsToDelete = []
+
+AnalysisDeletionService analysisDeletionService = ctx.analysisDeletionService
+
 
 ExternallyProcessedMergedBamFile.withTransaction {
-    bamFiles.each {
-        ExternalMergingWorkPackage workPackage = it.workPackage
-        it.delete()
-        workPackage.delete()
-        println "${it} deleted"
+    bamFiles.each { ExternallyProcessedMergedBamFile epmbf ->
+        println "to delete: ${epmbf}"
+
+        ExternalMergingWorkPackage workPackage = epmbf.workPackage
+        workPackage.bamFileInProjectFolder = null
+        workPackage.save(flush: true)
+
+        dirsToDelete << epmbf.importFolder
+        BamFilePairAnalysis.findAllBySampleType1BamFileOrSampleType2BamFile(epmbf, epmbf).each {
+            println "  --> delete analysis: ${it}"
+            dirsToDelete << AnalysisDeletionService.deleteInstance(it)
+        }
+        SamplePair.findAllByMergingWorkPackage1OrMergingWorkPackage2(workPackage, workPackage).each { SamplePair samplePair ->
+            println "  --> delete pair: ${samplePair}"
+            List<SamplePair> otherSamplePairs = SamplePair.withCriteria {
+                mergingWorkPackage1 {
+                    eq('sample', samplePair.mergingWorkPackage1.sample)
+                    eq('seqType', samplePair.seqType)
+                }
+                mergingWorkPackage2 {
+                    eq('sample', samplePair.mergingWorkPackage2.sample)
+                }
+                ne('id', samplePair.id)
+            }
+            List<File> files = analysisDeletionService.deleteSamplePairsWithoutAnalysisInstances([samplePair])
+            if (otherSamplePairs) {
+                println "    --> there exist other sample pairs with same seqtype and sampletypes --> keep the result paths itself"
+            } else {
+                dirsToDelete.addAll(files)
+            }
+        }
+
+        ExternalProcessedMergedBamFileQualityAssessment.createCriteria().list {
+            qualityAssessmentMergedPass {
+                eq('abstractMergedBamFile', epmbf)
+            }
+        }.each { ExternalProcessedMergedBamFileQualityAssessment qualityAssessment ->
+            println "  --> delete qa: ${qualityAssessment}"
+            QualityAssessmentMergedPass qualityAssessmentMergedPass = qualityAssessment.qualityAssessmentMergedPass
+            qualityAssessment.delete(flush: true)
+            qualityAssessmentMergedPass.delete(flush: true)
+
+        }
+
+        ImportProcess.withCriteria {
+            externallyProcessedMergedBamFiles {
+                eq('id', epmbf.id)
+            }
+        }.each { ImportProcess importProcess->
+            println "  --> remove from: ${importProcess}"
+            importProcess.externallyProcessedMergedBamFiles.remove(epmbf)
+            importProcess.save(flush: true)
+            if (importProcess.externallyProcessedMergedBamFiles.empty) {
+                println "    --> is now empty --> delete it"
+                importProcess.delete(flush: true)
+            }
+        }
+
+        epmbf.delete(flush: true)
+
+        workPackage.delete(flush: true)
+        println "  ==> deleted"
     }
     it.flush()
-    assert false
+
+    println "\n\nDirectories to delete"
+    println dirsToDelete.collect {
+        "rm -rf ${it}"
+    }.join('\n')
+    println "\n"
+
+    assert !tryRun: "Rollback, since only tryRun."
 }
