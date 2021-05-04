95f18ea4c1a46e0c1bc91bc61436a4e28f001dc8
Wei-Guo Peng <wei-guo.peng@dkfz.de>
2021-05-04 08:21:08 +0000

    new: otp-960: Create FastqcConditionalFailJob

---
+++ src/main/groovy/de/dkfz/tbi/otp/workflow/fastqc/FastqcConditionalFailJob.groovy
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.fastqc
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.infrastructure.FileService
+import de.dkfz.tbi.otp.ngsdata.DataFile
+import de.dkfz.tbi.otp.ngsdata.SeqTrack
+import de.dkfz.tbi.otp.workflow.jobs.AbstractConditionalFailJob
+import de.dkfz.tbi.otp.workflow.shared.WorkflowException
+import de.dkfz.tbi.otp.workflowExecution.WorkflowStep
+
+import java.nio.file.FileSystem
+import java.nio.file.Path
+
+@Component
+@Slf4j
+@CompileStatic
+class FastqcConditionalFailJob extends AbstractConditionalFailJob implements FastqcShared {
+
+    @Override
+    protected void check(WorkflowStep workflowStep) {
+        SeqTrack seqTrack = getSeqTrack(workflowStep)
+        List<DataFile> dataFiles = seqTrack.dataFiles
+        if (!dataFiles) {
+            throw new WorkflowException("SeqTrack '${seqTrack}' has no dataFiles")
+        }
+
+        FileSystem fs = getFileSystem(workflowStep)
+
+        final Collection<Path> missingPaths = dataFiles.collect { DataFile file ->
+            lsdfFilesService.getFileViewByPidPathAsPath(file, fs)
+        }.findAll { Path path ->
+            !FileService.isFileReadableAndNotEmpty(path)
+        }
+        if (missingPaths) {
+            throw new WorkflowException("The following ${missingPaths.size()} files are missing:\n${missingPaths.join("\n")}")
+        }
+    }
+}
---
+++ src/test/groovy/de/dkfz/tbi/otp/workflow/fastqc/FastqcConditionalFailJobSpec.groovy
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.fastqc
+
+import grails.testing.gorm.DataTest
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import de.dkfz.tbi.TestCase
+import de.dkfz.tbi.otp.domainFactory.workflowSystem.WorkflowSystemDomainFactory
+import de.dkfz.tbi.otp.job.processing.FileSystemService
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.workflow.shared.WorkflowException
+import de.dkfz.tbi.otp.workflowExecution.WorkflowStep
+
+import java.nio.file.FileSystems
+import java.nio.file.Path
+
+class FastqcConditionalFailJobSpec extends Specification implements DataTest, WorkflowSystemDomainFactory {
+
+    @Override
+    Class[] getDomainClassesToMock() {
+        return [
+            DataFile,
+            WorkflowStep,
+        ]
+    }
+
+    @Rule
+    TemporaryFolder temporaryFolder
+
+    void "test check, succeeds"() {
+        given:
+        WorkflowStep workflowStep = createWorkflowStep()
+        SeqTrack seqTrack = createSeqTrackWithTwoDataFile()
+
+        Path path = temporaryFolder.newFile().toPath()
+        path.text = "non-empty"
+
+        FastqcConditionalFailJob job = Spy(FastqcConditionalFailJob) {
+            getSeqTrack(workflowStep) >> seqTrack
+        }
+        job.fileSystemService = Mock(FileSystemService) {
+            getRemoteFileSystem(_) >> FileSystems.default
+        }
+        job.lsdfFilesService = Mock(LsdfFilesService) {
+            getFileViewByPidPathAsPath(_, _) >> path
+        }
+
+        when:
+        job.check(workflowStep)
+
+        then:
+        notThrown(WorkflowException)
+    }
+
+    void "test check, fails because seqTrack has no dataFiles"() {
+        given:
+        WorkflowStep workflowStep = createWorkflowStep()
+        SeqTrack seqTrack = createSeqTrack()
+
+        FastqcConditionalFailJob job = Spy(FastqcConditionalFailJob) {
+            getSeqTrack(workflowStep) >> seqTrack
+        }
+
+        when:
+        job.check(workflowStep)
+
+        then:
+        WorkflowException e = thrown(WorkflowException)
+        e.message.contains("has no dataFiles")
+    }
+
+    void "test check, fails because physical files are missing"() {
+        given:
+        WorkflowStep workflowStep = createWorkflowStep()
+        SeqTrack seqTrack = createSeqTrackWithTwoDataFile()
+
+        FastqcConditionalFailJob job = Spy(FastqcConditionalFailJob) {
+            getSeqTrack(workflowStep) >> seqTrack
+        }
+        job.fileSystemService = Mock(FileSystemService) {
+            getRemoteFileSystem(_) >> FileSystems.default
+        }
+        job.lsdfFilesService = Mock(LsdfFilesService) {
+            getFileViewByPidPathAsPath(_, _) >> TestCase.uniqueNonExistentPath.toPath()
+        }
+
+        when:
+        job.check(workflowStep)
+
+        then:
+        WorkflowException e = thrown(WorkflowException)
+        e.message.contains("files are missing")
+    }
+}
