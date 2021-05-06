95e142f986b55e0757a8885165de0d4840470a33
Wei-Guo Peng <wei-guo.peng@dkfz.de>
2021-05-06 07:29:12 +0000

    new: otp-961: Create FastqcPrepareJob

---
+++ src/main/groovy/de/dkfz/tbi/otp/workflow/fastqc/FastqcPrepareJob.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2011-2020 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.fastqc
+
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.dataprocessing.FastqcDataFilesService
+import de.dkfz.tbi.otp.ngsdata.SeqTrack
+import de.dkfz.tbi.otp.tracking.NotificationCreator
+import de.dkfz.tbi.otp.tracking.OtrsTicket
+import de.dkfz.tbi.otp.utils.LinkEntry
+import de.dkfz.tbi.otp.workflow.jobs.AbstractPrepareJob
+import de.dkfz.tbi.otp.workflowExecution.WorkflowStep
+
+import java.nio.file.FileSystem
+import java.nio.file.Path
+
+@Component
+@Slf4j
+class FastqcPrepareJob extends AbstractPrepareJob implements FastqcShared {
+
+    @Autowired
+    NotificationCreator notificationCreator
+
+    @Autowired
+    FastqcDataFilesService fastqcDataFilesService
+
+    @Override
+    protected void doFurtherPreparation(WorkflowStep workflowStep) {
+        SeqTrack seqTrack = getSeqTrack(workflowStep)
+        notificationCreator.setStartedForSeqTracks([seqTrack], OtrsTicket.ProcessingStep.FASTQC)
+        seqTrack.fastqcState = SeqTrack.DataProcessingState.IN_PROGRESS
+        seqTrack.save(flush: true)
+    }
+
+    @Override
+    protected Path buildWorkDirectoryPath(WorkflowStep workflowStep) {
+        SeqTrack seqTrack = getSeqTrack(workflowStep)
+        FileSystem fileSystem = getFileSystem(workflowStep)
+        return fileSystem.getPath(fastqcDataFilesService.fastqcOutputDirectory(seqTrack))
+    }
+
+    @Override
+    protected Collection<LinkEntry> generateMapForLinking(WorkflowStep workflowStep) {
+        return []
+    }
+}
