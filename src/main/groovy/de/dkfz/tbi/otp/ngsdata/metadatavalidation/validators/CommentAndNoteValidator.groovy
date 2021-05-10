1ca9635d99eebc17b371918eab554860853e010d
Andreas Kling <andreas.kling@bioquant.uni-heidelberg.de>
2021-05-10 12:00:00 +0200

    new: otp-994: Extract Information of Comment column during Import

---
+++ src/main/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/CommentAndNoteValidator.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.bam.BamMetadataValidator
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidator
+import de.dkfz.tbi.util.spreadsheet.*
+import de.dkfz.tbi.util.spreadsheet.validation.*
+
+import java.util.regex.Pattern
+
+@Component
+class CommentAndNoteValidator<C extends ValidationContext> implements MetadataValidator, BamMetadataValidator, Validator<C> {
+
+    static final Pattern COMMENT_OR_NOTE = Pattern.compile("(comment|note)", Pattern.CASE_INSENSITIVE)
+
+    @Override
+    Collection<String> getDescriptions() {
+        return []
+    }
+
+    @Override
+    void validate(C context) {
+        List<Column> columns = []
+        context.spreadsheet.header.cells.each { Cell cell ->
+            if (COMMENT_OR_NOTE.matcher(cell.text).find()) {
+                columns.add(context.spreadsheet.getColumn(cell.text))
+            }
+        }
+
+        columns.each { Column column ->
+            List<Cell> cells = []
+            context.spreadsheet.dataRows.each { Row row ->
+                Cell c = row.getCell(column)
+                if (c.text) {
+                    cells.add(row.getCell(column))
+                }
+            }
+            cells.groupBy { it.text }.each { String text, List<Cell> cells1 ->
+                context.addProblem(cells1 as Set, Level.INFO, "Comment/Note (${column.headerCell.text}): ${text}", "Comment/Note.")
+            }
+        }
+    }
+}
---
+++ src/test/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/validators/CommentAndNoteValidatorSpec.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.validators
+
+import spock.lang.Specification
+
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidationContext
+import de.dkfz.tbi.util.spreadsheet.validation.Level
+import de.dkfz.tbi.util.spreadsheet.validation.Problem
+
+import static de.dkfz.tbi.TestCase.assertContainSame
+import static de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidationContextFactory.createContext
+
+class CommentAndNoteValidatorSpec extends Specification {
+
+    void 'validate adds expected info'() {
+        given:
+        MetadataValidationContext context = createContext(
+                "ComMenT\tnote\timportant NOTe\tasdfasdf\tCOMMENT asdf\n" +
+                        "a1\ta2\ta3\ta4\ta5\n" +
+                        "a1\ta1\ta3\ta4\t   \n"
+        )
+        Collection<Problem> expectedProblems = [
+                new Problem([context.spreadsheet.dataRows[0].cells[0], context.spreadsheet.dataRows[1].cells[0]] as Set,
+                        Level.INFO, "Comment/Note (ComMenT): a1", "Comment/Note."),
+                new Problem([context.spreadsheet.dataRows[1].cells[1]] as Set, Level.INFO, "Comment/Note (note): a1", "Comment/Note."),
+                new Problem([context.spreadsheet.dataRows[0].cells[1]] as Set, Level.INFO, "Comment/Note (note): a2", "Comment/Note."),
+                new Problem([context.spreadsheet.dataRows[0].cells[2], context.spreadsheet.dataRows[1].cells[2]] as Set,
+                        Level.INFO, "Comment/Note (important NOTe): a3", "Comment/Note."),
+                new Problem([context.spreadsheet.dataRows[0].cells[4]] as Set, Level.INFO, "Comment/Note (COMMENT asdf): a5", "Comment/Note."),
+        ]
+
+        when:
+        new CommentAndNoteValidator().validate(context)
+
+        then:
+        assertContainSame(context.problems, expectedProblems)
+    }
+}
