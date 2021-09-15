8a9e192bd8b39aa0814932e6db67cc066095eca2
Gabriel Kolar <gabriel@kolar.info>
2021-09-15 08:34:21 +0000

    new: otp-1126: add PanCancerFinishJob

---
+++ src/main/groovy/de/dkfz/tbi/otp/workflow/panCancer/PanCancerFinishJob.groovy
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.panCancer
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.dataprocessing.AbstractMergedBamFile
+import de.dkfz.tbi.otp.dataprocessing.AbstractMergedBamFileService
+import de.dkfz.tbi.otp.dataprocessing.RoddyBamFile
+import de.dkfz.tbi.otp.infrastructure.FileService
+import de.dkfz.tbi.otp.utils.Md5SumService
+import de.dkfz.tbi.otp.workflow.jobs.AbstractFinishJob
+import de.dkfz.tbi.otp.workflowExecution.WorkflowStep
+
+import java.nio.file.FileSystem
+import java.nio.file.Files
+import java.nio.file.Path
+
+@Component
+@Slf4j
+@CompileStatic
+class PanCancerFinishJob extends AbstractFinishJob implements PanCancerShared {
+
+    @Autowired
+    FileService fileService
+
+    @Autowired
+    Md5SumService md5SumService
+
+    @Autowired
+    AbstractMergedBamFileService abstractMergedBamFileService
+
+    @Override
+    void updateDomains(WorkflowStep workflowStep) {
+        RoddyBamFile roddyBamFile = getRoddyBamFile(workflowStep)
+        FileSystem fs = getFileSystem(workflowStep)
+        Path md5sumFile = fileService.toPath(roddyBamFile.workMd5sumFile, fs)
+        String md5sum = md5SumService.extractMd5Sum(md5sumFile)
+
+        roddyBamFile.fileOperationStatus = AbstractMergedBamFile.FileOperationStatus.PROCESSED
+        roddyBamFile.workPackage.bamFileInProjectFolder = roddyBamFile
+        roddyBamFile.workPackage.save(flush: true)
+        roddyBamFile.fileSize = Files.size(fileService.toPath(roddyBamFile.workBamFile, fs))
+        roddyBamFile.md5sum = md5sum
+        roddyBamFile.save(flush: true)
+
+        abstractMergedBamFileService.updateSamplePairStatusToNeedProcessing(roddyBamFile)
+    }
+}
---
+++ src/test/groovy/de/dkfz/tbi/otp/workflow/panCancer/PanCancerFinishJobSpec.groovy
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.workflow.panCancer
+
+import grails.testing.gorm.DataTest
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import de.dkfz.tbi.otp.TestConfigService
+import de.dkfz.tbi.otp.dataprocessing.*
+import de.dkfz.tbi.otp.domainFactory.pipelines.IsRoddy
+import de.dkfz.tbi.otp.domainFactory.workflowSystem.WorkflowSystemDomainFactory
+import de.dkfz.tbi.otp.infrastructure.FileService
+import de.dkfz.tbi.otp.job.processing.TestFileSystemService
+import de.dkfz.tbi.otp.ngsdata.FastqImportInstance
+import de.dkfz.tbi.otp.ngsdata.FileType
+import de.dkfz.tbi.otp.utils.CreateFileHelper
+import de.dkfz.tbi.otp.utils.Md5SumService
+import de.dkfz.tbi.otp.workflowExecution.WorkflowStep
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+class PanCancerFinishJobSpec extends Specification implements DataTest, WorkflowSystemDomainFactory, IsRoddy {
+
+    @Override
+    Class[] getDomainClassesToMock() {
+        return [
+                WorkflowStep,
+                MergingWorkPackage,
+                RoddyBamFile,
+                FileType,
+                FastqImportInstance,
+        ]
+    }
+
+    @Rule
+    TemporaryFolder temporaryFolder
+
+    void "test updateDomain method should update roddyBamFile"() {
+        given:
+        new TestConfigService(temporaryFolder.newFolder())
+        WorkflowStep workflowStep = createWorkflowStep()
+        MergingWorkPackage workPackage = createMergingWorkPackage(bamFileInProjectFolder: null)
+        RoddyBamFile roddyBamFile = createBamFile([
+                fileOperationStatus: AbstractMergedBamFile.FileOperationStatus.INPROGRESS,
+                workPackage: workPackage,
+        ])
+
+        Path testWorkBamFile = CreateFileHelper.createFile(roddyBamFile.workBamFile.toPath())
+        String md5Sum = "098f6bcd4621d373cade4e832627b4f6"
+
+        PanCancerFinishJob job = Spy(PanCancerFinishJob) {
+            1 * getRoddyBamFile(workflowStep) >> roddyBamFile
+        }
+
+        job.md5SumService = Mock(Md5SumService) {
+            1 * extractMd5Sum(_) >> md5Sum
+        }
+
+        job.abstractMergedBamFileService = Mock(AbstractMergedBamFileService) {
+            1 * updateSamplePairStatusToNeedProcessing(roddyBamFile)
+        }
+
+        job.fileSystemService = new TestFileSystemService()
+        job.fileService = new FileService()
+
+        when:
+        job.updateDomains(workflowStep)
+
+        then:
+        RoddyBamFile bamFileAfterUpdate = RoddyBamFile.findAll().first()
+        bamFileAfterUpdate.fileOperationStatus == AbstractMergedBamFile.FileOperationStatus.PROCESSED
+        bamFileAfterUpdate.workPackage.bamFileInProjectFolder == bamFileAfterUpdate
+        bamFileAfterUpdate.md5sum == md5Sum
+        bamFileAfterUpdate.fileSize == Files.size(testWorkBamFile)
+        bamFileAfterUpdate.fileSize == testWorkBamFile.toFile().length()
+    }
+}
