873d26996489ed4beca22502376e29ab00db07eb
Andreas Kling <andreas.kling@bioquant.uni-heidelberg.de>
2021-11-17 12:00:00 +0100

    new: otp-1286: Add validator for species

---
+++ src/main/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/fastq/validators/SpeciesValidator.groovy
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.validators
+
+import groovy.transform.Canonical
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidationContext
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidator
+import de.dkfz.tbi.otp.ngsdata.taxonomy.SpeciesCommonName
+import de.dkfz.tbi.otp.ngsdata.taxonomy.SpeciesCommonNameService
+import de.dkfz.tbi.otp.utils.CollectionUtils
+import de.dkfz.tbi.util.spreadsheet.Cell
+import de.dkfz.tbi.util.spreadsheet.validation.*
+
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.*
+
+@Component
+class SpeciesValidator extends ValueTuplesValidator<MetadataValidationContext> implements MetadataValidator {
+
+    @Autowired
+    ValidatorHelperService validatorHelperService
+    @Autowired
+    SpeciesCommonNameService speciesCommonNameService
+    @Autowired
+    SampleIdentifierService sampleIdentifierService
+
+    @Override
+    Collection<String> getDescriptions() {
+        return [
+                "The column ${SPECIES} must exist.",
+                "The field ${SPECIES} must not be empty.",
+                "Each value of the + separated list in field ${SPECIES} has to be known to OTP.",
+                "All entries of a PID must have the same main species.",
+                "If the PID already exists, the main species has to match.",
+                "All entries of a sample must have the same mixed-in species.",
+                "If the sample already exists, the mixed-in species have to match.",
+        ]
+    }
+
+    @Override
+    List<String> getRequiredColumnTitles(MetadataValidationContext context) {
+        return [SPECIES]*.name()
+    }
+
+    @Override
+    List<String> getOptionalColumnTitles(MetadataValidationContext context) {
+        return [PROJECT, SAMPLE_NAME]*.name()
+    }
+
+    @Override
+    void checkMissingOptionalColumn(MetadataValidationContext context, String columnTitle) {
+    }
+
+    @Override
+    void validateValueTuples(MetadataValidationContext context, Collection<ValueTuple> valueTuples) {
+        Set<Cell> emptyCells = [] as Set
+        Map<String, Set<Cell>> unknownCells = [:]
+
+        Collection<ValueTuple> valueTuplesValidSpecies = valueTuples.findAll { ValueTuple valueTuple ->
+            String speciesValue = valueTuple.getValue(SPECIES.name()).trim()
+            if (speciesValue) {
+                List<Boolean> speciesAreKnown =  speciesValue.split(/\+/).collect {
+                    if (!speciesCommonNameService.findByNameOrImportAlias(it.trim())) {
+                        if (unknownCells.containsKey(it)) {
+                            unknownCells[it].addAll(valueTuple.cells)
+                        } else {
+                            unknownCells[it] = new HashSet<Cell>(valueTuple.cells)
+                        }
+                        return false
+                    }
+                    return true
+                }
+                return speciesAreKnown.every()
+            }
+            emptyCells.addAll(valueTuple.cells)
+            return false
+        }
+
+        if (emptyCells) {
+            context.addProblem(emptyCells, LogLevel.ERROR, "The field '${SPECIES}' must not be empty.")
+        }
+        unknownCells.each { String speciesName, Set<Cell> cells ->
+            context.addProblem(cells, LogLevel.ERROR,
+                                "The species '${speciesName}' is not known to OTP.",
+                                "Each value of the + separated list in field '${SPECIES}' has to be known to OTP.")
+        }
+
+        List<Value> values = valueTuplesValidSpecies.collect {
+            List<String> speciesNames = it.getValue(SPECIES.name()).trim().split(/\+/) as List
+            SpeciesCommonName species = speciesCommonNameService.findByNameOrImportAlias(speciesNames.remove(0).trim())
+            Set<SpeciesCommonName> mixedInSpecies = speciesNames.collect { speciesCommonNameService.findByNameOrImportAlias(it.trim()) } as Set
+
+            String sampleType = validatorHelperService.getSampleType(it)
+            String pid = validatorHelperService.getPid(it)
+            Sample sample = validatorHelperService.findExistingSampleForValueTuple(it)
+            Individual individual = CollectionUtils.atMostOneElement(Individual.findAllByPid(pid))
+
+            new Value(
+                    sampleType,
+                    sample,
+                    pid,
+                    individual,
+                    species,
+                    mixedInSpecies,
+                    it,
+            )
+        }
+
+        values.groupBy { it.pid }.each { String pid, List<Value> value ->
+            if (pid && value*.species.unique().size() > 1) {
+                context.addProblem(value.collectMany { it.valueTuple.cells } as Set, LogLevel.ERROR,
+                        "PID '${pid}' has distinct species ${value*.species.collect { "'${it}'" }.join(", ")}.",
+                        "All entries of a PID must have the same main species.")
+            }
+        }
+        values.groupBy { it.individual }.each { Individual individual, List<Value> value ->
+            if (individual && value.species.unique().size() == 1 &&
+                    (value*.species.first() != individual?.species) &&
+                            !(!individual.samples.collectMany { it.seqTracks ?: [] } && !individual.species)) {
+                context.addProblem(value.collectMany { it.valueTuple.cells } as Set, LogLevel.ERROR,
+                        "Species '${value*.species.first()}' for PID '${individual}' doesn't match with existing PID with species '${individual.species}'.",
+                        "If the PID already exists, the main species has to match."
+                )
+            }
+        }
+
+        values.groupBy { [it.pid, it.sampleType] }.each { List<String> sample, List<Value> value ->
+            if (sample && value*.mixedInSpecies.unique().size() > 1) {
+                context.addProblem(value.collectMany { it.valueTuple.cells } as Set, LogLevel.ERROR,
+                        "Sample '${sample.join(" ")}' has distinct mixed-in species ${value*.mixedInSpecies.collect { "'${ it.join(",")}'" }.join(", ")}.",
+                        "All entries of a sample must have the same mixed-in species."
+                )
+            }
+        }
+        values.groupBy { it.sample }.each { Sample sample, List<Value> value ->
+            if (sample && value*.mixedInSpecies.unique().size() == 1 &&
+                    (value*.mixedInSpecies.first() != (sample.mixedInSpecies ?: [] as Set)) &&
+                            !(!sample.seqTracks && !sample.mixedInSpecies)) {
+                String message = ""
+                if (value*.mixedInSpecies.first()) {
+                    message += "Mixed-in species '${value*.mixedInSpecies.first().join(",")}'"
+                } else {
+                    message += "No mixed-in species"
+                }
+                message += " for sample '${sample}' doesn't match with existing sample "
+
+                if (sample.mixedInSpecies) {
+                    message += "with mixed-in species '${(sample.mixedInSpecies ?: []).join(",")}'."
+                } else {
+                    message += "without mixed-in species."
+                }
+                context.addProblem(value.collectMany { it.valueTuple.cells } as Set, LogLevel.ERROR,
+                        message,
+                        "If the sample already exists, the mixed-in species have to match."
+                )
+            }
+        }
+    }
+}
+
+@Canonical
+class Value {
+    String sampleType
+    Sample sample
+    String pid
+    Individual individual
+    SpeciesCommonName species
+    Set<SpeciesCommonName> mixedInSpecies
+    ValueTuple valueTuple
+}
---
+++ src/test/groovy/de/dkfz/tbi/otp/ngsdata/metadatavalidation/fastq/validators/SpeciesValidatorSpec.groovy
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2011-2021 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.validators
+
+import grails.testing.gorm.DataTest
+import spock.lang.Specification
+
+import de.dkfz.tbi.TestCase
+import de.dkfz.tbi.otp.dataprocessing.ProcessingOption
+import de.dkfz.tbi.otp.domainFactory.DomainFactoryCore
+import de.dkfz.tbi.otp.domainFactory.taxonomy.TaxonomyFactory
+import de.dkfz.tbi.otp.ngsdata.*
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.MetadataValidationContextFactory
+import de.dkfz.tbi.otp.ngsdata.metadatavalidation.fastq.MetadataValidationContext
+import de.dkfz.tbi.otp.ngsdata.taxonomy.SpeciesCommonName
+import de.dkfz.tbi.otp.ngsdata.taxonomy.SpeciesCommonNameService
+import de.dkfz.tbi.util.spreadsheet.validation.LogLevel
+import de.dkfz.tbi.util.spreadsheet.validation.Problem
+
+import static de.dkfz.tbi.otp.ngsdata.MetaDataColumn.*
+
+class SpeciesValidatorSpec extends Specification implements DataTest, DomainFactoryCore, TaxonomyFactory {
+
+    @Override
+    Class[] getDomainClassesToMock() {
+        return [
+                ProcessingOption,
+                SampleIdentifier,
+                SpeciesCommonName,
+        ]
+    }
+
+    void "test validate"() {
+        given:
+        SpeciesValidator validator = new SpeciesValidator()
+        validator.speciesCommonNameService = new SpeciesCommonNameService()
+        validator.sampleIdentifierService = new SampleIdentifierService()
+        validator.validatorHelperService = new ValidatorHelperService()
+
+        SpeciesCommonName human = createSpeciesCommonName(name: "human")
+        SpeciesCommonName mouse = createSpeciesCommonName(name: "mouse")
+        SpeciesCommonName dolphin = createSpeciesCommonName(name: "dolphin")
+
+        // correct metadata
+        createSampleIdentifier(name: "sample", sample: createSample(individual: createIndividual(species: human), mixedInSpecies: []))
+        createSampleIdentifier(name: "sampleWithMixedInSpecies", sample: createSample(individual: createIndividual(species: human), mixedInSpecies: [mouse, dolphin]))
+        createSampleIdentifier(name: "existingSampleWithoutData1", sample: createSample(individual: createIndividual(species: null), mixedInSpecies: []))
+        createSampleIdentifier(name: "existingSampleWithoutData2", sample: createSample(individual: createIndividual(species: null), mixedInSpecies: []))
+
+        // incorrect metadata
+        createSampleIdentifier(name: "distinctSpecies", sample: createSample(individual: createIndividual(pid: "1", mockPid: "1", species: mouse)))
+        createSampleIdentifier(name: "existingDifferentSpecies1", sample: createSample(individual: createIndividual(pid: "2", mockPid: "2", species: dolphin)))
+        createSampleIdentifier(name: "existingDifferentSpecies2", sample: createSample(individual: createIndividual(pid: "3", mockPid: "3", species: null), seqTracks: [createSeqTrack()]))
+
+        createSampleIdentifier(name: "distinctMixin", sample: createSample(individual: createIndividual(pid: "4", mockPid: "4", species: human), sampleType: createSampleType(name: "a")))
+        createSampleIdentifier(name: "existingDifferentMixin1", sample: createSample(individual: createIndividual(pid: "5", mockPid: "5", species: human), sampleType: createSampleType(name: "b"), mixedInSpecies: [mouse]))
+        createSampleIdentifier(name: "existingDifferentMixin2", sample: createSample(individual: createIndividual(pid: "6", mockPid: "6", species: human), sampleType: createSampleType(name: "c"), mixedInSpecies: [], seqTracks: [createSeqTrack()]))
+        createSampleIdentifier(name: "existingDifferentMixin3", sample: createSample(individual: createIndividual(pid: "7", mockPid: "7", species: human), sampleType: createSampleType(name: "d"), mixedInSpecies: [human, mouse]))
+
+        MetadataValidationContext context = MetadataValidationContextFactory.createContext("""\
+${SAMPLE_NAME}\t${SPECIES}\t${PROJECT}
+sample\tHUMAN\t
+sample\thuman\t
+sample\thUmAn\t
+sampleWithMixedInSpecies\thuman+mouse+dolphin\t
+sampleWithMixedInSpecies\thuman+dolphin+mouse\t
+existingSampleWithoutData1\tHUMAN\t
+existingSampleWithoutData2\tHUMAN+MOUSE\t
+\t\t
+\tCERBERUS\t
+\tHUMAN+PEGASUS+MEDUSA\t
+distinctSpecies\tHUMAN\t
+distinctSpecies\tMOUSE\t
+existingDifferentSpecies1\tHUMAN\t
+existingDifferentSpecies2\tHUMAN\t
+distinctMixin\tHUMAN+MOUSE\t
+distinctMixin\tHUMAN+DOLPHIN\t
+existingDifferentMixin1\tHUMAN\t
+existingDifferentMixin2\tHUMAN+MOUSE\t
+existingDifferentMixin3\tHUMAN+DOLPHIN+MOUSE\t
+unknownSampleName\tHUMAN\t
+unknownSampleName\tMOUSE\t
+""")
+
+        when:
+        validator.validate(context)
+
+        then:
+        Collection<Problem> expectedProblems = [
+                new Problem(context.spreadsheet.dataRows[7].cells as Set, LogLevel.ERROR,
+                        "The field 'SPECIES' must not be empty.",
+                        "The field 'SPECIES' must not be empty."),
+                new Problem(context.spreadsheet.dataRows[8].cells as Set, LogLevel.ERROR,
+                        "The species 'CERBERUS' is not known to OTP.",
+                        "Each value of the + separated list in field 'SPECIES' has to be known to OTP."),
+                new Problem(context.spreadsheet.dataRows[9].cells as Set, LogLevel.ERROR,
+                        "The species 'PEGASUS' is not known to OTP.",
+                        "Each value of the + separated list in field 'SPECIES' has to be known to OTP."),
+                new Problem(context.spreadsheet.dataRows[9].cells as Set, LogLevel.ERROR,
+                        "The species 'MEDUSA' is not known to OTP.",
+                        "Each value of the + separated list in field 'SPECIES' has to be known to OTP."),
+
+                new Problem(context.spreadsheet.dataRows[10, 11].collectMany { it.cells } as Set, LogLevel.ERROR,
+                        "PID '1' has distinct species 'human', 'mouse'.",
+                        "All entries of a PID must have the same main species."),
+                new Problem(context.spreadsheet.dataRows[12].cells as Set, LogLevel.ERROR,
+                        "Species 'human' for PID '2' doesn't match with existing PID with species 'dolphin'.",
+                        "If the PID already exists, the main species has to match."),
+                new Problem(context.spreadsheet.dataRows[13].cells as Set, LogLevel.ERROR,
+                        "Species 'human' for PID '3' doesn't match with existing PID with species 'null'.",
+                        "If the PID already exists, the main species has to match."),
+
+                new Problem(context.spreadsheet.dataRows[14, 15].collectMany { it.cells } as Set, LogLevel.ERROR,
+                        "Sample '4 a' has distinct mixed-in species 'mouse', 'dolphin'.",
+                        "All entries of a sample must have the same mixed-in species."),
+                new Problem(context.spreadsheet.dataRows[16].cells as Set, LogLevel.ERROR,
+                        "No mixed-in species for sample '5 b' doesn't match with existing sample with mixed-in species 'mouse'.",
+                        "If the sample already exists, the mixed-in species have to match."),
+                new Problem(context.spreadsheet.dataRows[17].cells as Set, LogLevel.ERROR,
+                        "Mixed-in species 'mouse' for sample '6 c' doesn't match with existing sample without mixed-in species.",
+                        "If the sample already exists, the mixed-in species have to match."),
+                new Problem(context.spreadsheet.dataRows[18].cells as Set, LogLevel.ERROR,
+                        "Mixed-in species 'dolphin,mouse' for sample '7 d' doesn't match with existing sample with mixed-in species 'human,mouse'.",
+                        "If the sample already exists, the mixed-in species have to match."),
+        ]
+        TestCase.assertContainSame(context.problems, expectedProblems)
+    }
+}
