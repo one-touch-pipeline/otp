d3b6d672bf132ab4bd0abd170ab0ea5cd70b50e5
merge-request <project12049971_bot@example.com>
2022-02-01 08:15:07 +0000

    chg: otp-1508: update lint dependencies and disable new rules for CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,9 @@ const lintRules = {
 if (process.env.CI) {
   Object.assign(lintRules, {
     strict: 'off',
+    'no-promise-executor-return': 'off',
+    'function-paren-newline': 'off',
+    'function-call-argument-newline': 'off',
     'no-alert': 'off'
   });
 }
