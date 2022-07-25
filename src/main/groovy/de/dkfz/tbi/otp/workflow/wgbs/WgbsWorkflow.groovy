eb5c086e0625bc43d8bb4f48ab5233b4c6bb8b25
Gabriel Kolar <gabriel@kolar.info>
2022-07-25 12:01:30 +0200

    new: otp-1640: create WgbsWorkflow class

--- src/main/groovy/de/dkfz/tbi/otp/workflow/wgbs/WgbsWorkflow.groovy
+++ src/main/groovy/de/dkfz/tbi/otp/workflow/wgbs/WgbsWorkflow.groovy
@@ -21,8 +21,37 @@
  */
 package de.dkfz.tbi.otp.workflow.wgbs
 
-// TODO: implement in otp-1640
-class WgbsWorkflow {
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.springframework.stereotype.Component
+
+import de.dkfz.tbi.otp.workflow.jobs.SetCorrectPermissionJob
+import de.dkfz.tbi.otp.workflow.panCancer.PanCancerCheckQcJob
+import de.dkfz.tbi.otp.workflow.panCancer.PanCancerCleanUpJob
+import de.dkfz.tbi.otp.workflow.panCancer.PanCancerConditionalFailJob
+import de.dkfz.tbi.otp.workflow.panCancer.PanCancerFinishJob
+import de.dkfz.tbi.otp.workflow.panCancer.PanCancerWorkflow
+
+@Component
+@Slf4j
+@CompileStatic
+class WgbsWorkflow extends PanCancerWorkflow {
     static final String WORKFLOW = "WGBS alignment"
     static final String OUTPUT_BAM = "BAM"
+
+    @Override
+    List<String> getJobBeanNames() {
+        return [
+                PanCancerConditionalFailJob.simpleName.uncapitalize(),
+                WgbsPrepareJob.simpleName.uncapitalize(),
+                WgbsExecuteJob.simpleName.uncapitalize(),
+                WgbsValidationJob.simpleName.uncapitalize(),
+                WgbsParseJob.simpleName.uncapitalize(),
+                PanCancerCheckQcJob.simpleName.uncapitalize(),
+                PanCancerCleanUpJob.simpleName.uncapitalize(),
+                WgbsLinkJob.simpleName.uncapitalize(),
+                SetCorrectPermissionJob.simpleName.uncapitalize(),
+                PanCancerFinishJob.simpleName.uncapitalize(),
+        ]
+    }
 }
