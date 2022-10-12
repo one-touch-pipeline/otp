6f4de7e62bd825dab6333d30012f005d2f224784
Andreas Kling <andreas.kling@bioquant.uni-heidelberg.de>
2022-10-12 12:00:00 +0200

    fix: otp-1834: Fix incorrect environment in production

--- src/main/groovy/de/dkfz/tbi/otp/utils/logging/EnvironmentDefiner.groovy
+++ src/main/groovy/de/dkfz/tbi/otp/utils/logging/EnvironmentDefiner.groovy
@@ -22,74 +22,11 @@
 package de.dkfz.tbi.otp.utils.logging
 
 import ch.qos.logback.core.PropertyDefinerBase
-import grails.util.BuildSettings
 import grails.util.Environment
 
 class EnvironmentDefiner extends PropertyDefinerBase {
-    private String key
-
-    void setKey(String key) {
-        this.key = key
-    }
-
     @Override
     String getPropertyValue() {
-        switch (key) {
-            case "isDevelopmentMode":
-                return current == Environment.DEVELOPMENT && BuildSettings.GRAILS_APP_DIR_PRESENT
-            case "isProductionEnvironment":
-                return current == Environment.PRODUCTION
-            case "isWorkflowTestEnvironment":
-                return current.name == "WORKFLOW_TEST"
-        }
-    }
-
-    /**
-     * In Grails 5, {@link Environment#getCurrent()} indirectly creates a log when reading application.yml. This causes an exception when configuring the
-     * log system.
-     * So the following code is copied from {@link Environment#getCurrent()}, with the call to Metadata.getCurrent() removed.
-     */
-    Environment getCurrent() {
-        String envName = environmentInternal
-
-        Environment env
-        if (!isBlank(envName)) {
-            env = Environment.getEnvironment(envName)
-            if (env != null) {
-                return env
-            }
-        }
-        return resolveCurrentEnvironment()
-    }
-
-    private Environment resolveCurrentEnvironment() {
-        String envName = environmentInternal
-
-        if (isBlank(envName)) {
-            return Environment.DEVELOPMENT
-        }
-
-        Environment env = Environment.getEnvironment(envName)
-        if (env == null) {
-            try {
-                env = Environment.valueOf(envName.toUpperCase())
-            }
-            catch (IllegalArgumentException ignored) {
-            }
-        }
-        if (env == null) {
-            env = Environment.CUSTOM
-            env.name = envName
-        }
-        return env
-    }
-
-    private String getEnvironmentInternal() {
-        String envName = System.getProperty(Environment.KEY)
-        return isBlank(envName) ? System.getenv(Environment.ENV_KEY) : envName
-    }
-
-    private boolean isBlank(String value) {
-        return value == null || value.trim().length() == 0
+        return Environment.isDevelopmentEnvironmentAvailable()
     }
 }
