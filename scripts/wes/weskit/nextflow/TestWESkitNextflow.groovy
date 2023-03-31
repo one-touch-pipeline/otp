f67d453b186e1c469300ed5243c89341830a440f
Stefan Borufka <s.borufka@dkfz-heidelberg.de>
2023-03-31 09:41:08 +0200

    chg: otp-2012: configure authentification in weskit swagger
    - also update script to access weskit
      - currently, the truststore missing to do the access

--- scripts/wes/weskit/nextflow/TestWESkitNextflow.groovy
+++ scripts/wes/weskit/nextflow/TestWESkitNextflow.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2020 The OTP authors
+ * Copyright 2011-2023 The OTP authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,22 +20,66 @@
  * SOFTWARE.
  */
 
+
 /**
- * setup wesnake: see README
+ * script to check connection to weskit
+ *
+ * for setup weskit: see https://gitlab.com/one-touch-pipeline/otp-wes-config
  */
 
 import io.swagger.client.wes.api.WorkflowExecutionServiceApi
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.client.HttpStatusCodeException
+import reactor.core.publisher.Mono
+
+/**
+ * access token from keycloak to access weskit
+ */
+String token = ""
+
+/**
+ * weskit host
+ */
+String host = ""
+
+/**
+ * protocol to use, either https or http
+ */
+String protocol = ""
+/**
+ * port to use, usually 443
+ */
+String port = ""
+
+/**
+ * path where the test data are located
+ */
+String TEST_INPUT_EXTERN = ""
+/**
+ * path to use for output of data
+ */
+String TEST_OUTPUT_EXTERN = ""
+
+//-------------------
+//work
+
+assert token
+assert host
+assert protocol
+assert port
+assert TEST_INPUT_EXTERN
+assert TEST_OUTPUT_EXTERN
 
 WorkflowExecutionServiceApi apiInstance = new WorkflowExecutionServiceApi();
-apiInstance.apiClient.basePath = 'http://localhost:9000/ga4gh/wes/v1'
+apiInstance.apiClient.basePath = "${protocol}://${host}:${port}/ga4gh/wes/v1"
+apiInstance.apiClient.accessToken = token
 
 def work = { String message, Closure cl ->
     try {
         println '\n------------------------------------'
         println message
-        println cl()
+        Mono mono = cl()
+        println mono.block()
     } catch (HttpStatusCodeException e) {
         println """
 message: ${e.message}
@@ -55,19 +99,20 @@ body: ${e.responseBodyAsString}
 
 work('serviceinfo') { apiInstance.serviceInfo }
 work('list') { apiInstance.listRuns(10, null) }
-work('run state') { apiInstance.getRunStatus('1') }
-work('run log') { apiInstance.getRunLog('1') }
+work('run state') { apiInstance.getRunStatus('24d5d211-234b-4b24-8d8b-70ae7b1b19b0') }
+work('run log') { apiInstance.getRunLog('24d5d211-234b-4b24-8d8b-70ae7b1b19b0') }
 
 work('post') {
     String workflowParams = """
 {
-}
+    "input" : "'$TEST_INPUT_EXTERN'/run1_gerald_D1VCPACXX_1_R1.sorted.fastq.tar.bz2,'$TEST_INPUT_EXTERN'/run1_gerald_D1VCPACXX_1_R1.sorted.fastq.gz", 
+    "outputDir" : "'$TEST_OUTPUT_EXTERN'" }
 """
-    String workflowType = "snakemake"
-    String workflowTypeVersion = "5.8.2"
-    String tags = ""
+    String workflowType = "NFL"
+    String workflowTypeVersion = "22.10.0"
+    String tags = """ { "run_dir" : "test_${de.dkfz.tbi.util.TimeFormats.DATE_TIME_SECONDS_DASHES.getFormattedDate(new Date())}" } """
     String workflowEngineParameters = ""
-    String workflowUrl = ""
+    String workflowUrl = "nf-seq-qc-1.1.0/main.nf"
     List<byte[]> workflowAttachment = null
 
     apiInstance.runWorkflow(workflowParams, workflowType, workflowTypeVersion, tags, workflowEngineParameters, workflowUrl, workflowAttachment)
