96b2f96880e3489f472696fefad4eddd3fb5e54d
Florian Kaercher <florian.kaercher@bioquant.uni-heidelberg.de>
2024-09-16 08:49:14 +0000

    new: otp-2566: cypress test for processing priority

---
+++ cypress/e2e/workflowSystem/processingPriority.spec.js
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2011-2024 The OTP authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+describe('Check processing priority page', () => {
+  'use strict';
+
+  context('when user is an operator', () => {
+    beforeEach(() => {
+      cy.loginAs('operator');
+      cy.visit('/processingPriority/index');
+    });
+
+    it('should display the processing priority page', () => {
+      cy.get('h3').should('have.text', 'Processing Priority');
+      cy.get('button.btn').contains('Create').should('exist');
+      cy.get('table tbody').should('exist').should('not.be.empty');
+
+      cy.get('button#edit-row').should('exist');
+      cy.get('button#delete-row').should('exist');
+    });
+
+    it('should create a processing priority', () => {
+      cy.get('button.btn').contains('Create').click();
+
+      cy.get('button.btn-primary').contains('Save').should('exist');
+      cy.get('button.btn-secondary').contains('Cancel').should('exist');
+      cy.intercept('/ProcessingPriority/save*').as('createProcessingPriority');
+
+      cy.get('input#pp-name').clear().type('Test1');
+      cy.get('input#pp-priority').clear().type('244');
+      cy.get('input#pp-allowedParallelWorkflowRuns').clear().type('2');
+      cy.get('input#pp-queue').clear().type('queue');
+      cy.get('input#pp-roddyConfigSuffix').clear().type('Test2');
+      cy.get('input#pp-errorMailPrefix').clear().type('Test3');
+      cy.get('button#bt-save').click();
+      cy.wait('@createProcessingPriority')
+        .then((interception) => {
+          expect(interception.response.statusCode).to.eq(200);
+        });
+      cy.get('.otpSuccessToast').should('exist')
+        .and('contain.text', 'Processing Priority Test1 has been saved.');
+
+      cy.get('table tbody tr td:nth-child(1)').contains('Test1').siblings()
+        .should('contain.text', '244')
+        .should('contain.text', '2')
+        .should('contain.text', 'queue')
+        .should('contain.text', 'Test2')
+        .should('contain.text', 'Test3');
+    });
+
+    it('should update a processing priority', () => {
+      cy.get('table tbody tr td:nth-child(1)').contains('Test1').siblings().last()
+        .find('button#edit-row')
+        .click();
+      cy.get('button.btn-primary').contains('Save').should('exist');
+      cy.get('button.btn-secondary').contains('Cancel').should('exist');
+      cy.get('input#pp-name').clear().type('Test4');
+      cy.get('input#pp-priority').clear().type('55');
+      cy.get('input#pp-allowedParallelWorkflowRuns').clear().type('4');
+      cy.get('input#pp-queue').clear().type('queue2');
+      cy.get('input#pp-roddyConfigSuffix').clear().type('Test5');
+      cy.get('input#pp-errorMailPrefix').clear().type('Test6');
+      cy.get('button#bt-save').click();
+
+      cy.get('.otpSuccessToast').should('exist')
+        .and('contain.text', 'Processing Priority Test4 has been saved.');
+      cy.get('table tbody tr td:nth-child(1)')
+        .should('not.contains.text', 'Test1');
+      cy.get('table tbody tr td:nth-child(1)')
+        .contains('Test4').siblings()
+        .should('contain.text', '55')
+        .should('contain.text', '4')
+        .should('contain.text', 'queue2')
+        .should('contain.text', 'Test5')
+        .should('contain.text', 'Test6');
+    });
+
+    it('should not be able to create a processing priority with invalid inputs', () => {
+      cy.get('button.btn').contains('Create').click();
+      cy.get('input#pp-name').clear().type('Test4');
+      cy.get('input#pp-priority').clear().type('55');
+      cy.get('input#pp-allowedParallelWorkflowRuns').clear().type('2');
+      cy.get('input#pp-queue').clear().type('queue');
+      cy.get('input#pp-roddyConfigSuffix').clear().type('Test5');
+      cy.get('input#pp-errorMailPrefix').clear().type('Test6');
+      cy.get('button#bt-save').should('be.disabled');
+    });
+
+    it('should delete a processing priority', () => {
+      cy.get('table tbody tr td:nth-child(1)').contains('Test4').siblings().last()
+        .find('button#delete-row')
+        .click();
+      cy.get('button#bt-delete').click();
+      cy.get('.otpSuccessToast').should('exist')
+        .and('contain.text', 'Processing Priority Test4 has been deleted.');
+      cy.get('table tbody tr td:nth-child(1)')
+        .should('not.contains.text', 'Test4');
+    });
+  });
+
+  context('when user is normal user', () => {
+    beforeEach(() => {
+      cy.loginAs('user');
+    });
+
+    it('should not be able to visit the page', () => {
+      cy.checkAccessDenied('/processingPriority/index', 403);
+    });
+  });
+});
